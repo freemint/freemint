df8c2de5b7b671c3fa91470ec543380c02490c93
fna <fna>
2000-06-15 21:25:01 +0000

    Initial checkin of sfdisk (Linux fdisk ported to MiNT).

---
+++ tools/fdisk/mytypes.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2000 Frank Naumann <fnaumann@freemint.de>
+ * All rights reserved.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * 
+ * 
+ * Started:      2000-05-02
+ * 
+ * Changes:
+ * 
+ * 2000-05-02:
+ * 
+ * - inital version
+ * 
+ */
+
+# ifndef _mytypes_h
+# define _mytypes_h
+
+# include <sys/types.h>
+
+
+typedef unsigned char		uchar;
+
+typedef signed char		__s8;
+typedef signed short		__s16;
+typedef signed long		__s32;
+typedef signed long long	__s64;
+
+typedef unsigned char		__u8;
+typedef unsigned short		__u16;
+typedef unsigned long		__u32;
+typedef unsigned long long	__u64;
+
+
+# endif /* _mytypes_h */
