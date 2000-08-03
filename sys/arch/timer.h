42f8bc906798f44ba6e7bdc6e98667bef9e498fe
Frank Naumann <fnaumann@boerde.de>
2000-08-03 10:42:05 +0000

    New definitions and prototypes for the system timer. This timer is in fact updated by TOS timer at the moment.

---
+++ sys/arch/timer.h
@@ -0,0 +1,43 @@
+/*
+ * This file belongs to FreeMiNT. It's not in the original MiNT 1.12
+ * distribution.
+ * 
+ * Copyright 2000 Frank Naumann <fnaumann@freemint.de>
+ * All rights reserved.
+ * 
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * 
+ * 
+ * last change:	2000-07-17
+ * 
+ * please send suggestions, patches or bug reports to me or
+ * the MiNT mailing list
+ * 
+ */
+
+# ifndef _m68k_timer_h
+# define _m68k_timer_h
+
+# include "mint/mint.h"
+
+
+# define _hz_200		((long *) 0x4baL)
+# define HZ			200
+
+extern volatile long *hz_200;
+# define jiffies    		(*hz_200)
+
+
+# endif /* _m68k_timer_h */
