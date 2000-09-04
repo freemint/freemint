edbecfa67678009623eed2ddb519419ef85b7b78
Frank Naumann <fnaumann@boerde.de>
2000-09-04 11:35:13 +0000

    Source restructurization, moved from sys/xfs/minix to this place.

---
+++ tools/minix/fsck/global.c
@@ -0,0 +1,66 @@
+/* Global Variables
+ */
+
+# include "global.h"
+
+
+char lfname [] = "lost+found";
+
+
+char *drvnam;
+
+char version;		/* Non-zero for V2 */
+
+char modified;
+char preen;
+char no_size;
+
+ilist *inolist;
+
+llist *inums;
+
+llist *zinums;
+
+long maxzone,minzone;	/* Max/Min Allowed Zone Numbers */
+long maxino;		/* Max Inode Number */
+long ioff;		/* Zone offset to inode block */
+
+int incr;		/* Directory increment */
+
+long berr;		/* Bitmap errors */
+
+char badroot;		/* Bad root inode or forced reallocation */
+
+long zonecount;		/* Maximum Zone Count */
+long indcount;		/* Number of indirection blocks */
+char dindcount;		/* Number of double indirection blocks */
+char tindcount;		/* '1' If triple indirection block */
+
+char trunc,done_trunc;	/* Flags for truncation of files */
+
+char quit_trav;
+
+long cino;		/* current inode */
+
+ushort *zbitmap;	/* Zone bitmap */
+ushort *szbitmap;	/* Shadow zone bitmap */
+ushort *ibitmap;	/* Inode Bitmap */
+
+int cdirty;	
+
+super_block *Super;
+
+long ndir;
+long nreg;
+long nfifo;
+long nchr;
+long nblk;
+long nsym;
+long zfree;
+long ifree;
+
+char ally;
+char alln;
+char info;
+
+ushort lfinode;
