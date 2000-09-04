fe31a2e7274b7ce12995c3854bbb4ee8ab4fcff2
Frank Naumann <fnaumann@boerde.de>
2000-09-04 00:42:36 +0000

    New lib, share I/O routines for the tools.

---
+++ tools/IO/mytypes.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2000 Frank Naumann <fnaumann@freemint.de>
+ * All rights reserved.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * 
+ * 
+ * Started:      2000-05-02
+ * 
+ * Changes:
+ * 
+ * 2000-05-02:
+ * 
+ * - inital version
+ * 
+ */
+
+# ifndef _mytypes_h
+# define _mytypes_h
+
+# include <sys/types.h>
+
+typedef unsigned char	uchar;
+
+typedef signed char	__s8;
+typedef int16_t		__s16;
+typedef int32_t		__s32;
+typedef int64_t		__s64;
+
+typedef unsigned char	__u8;
+typedef u_int16_t	__u16;
+typedef u_int32_t	__u32;
+typedef u_int64_t	__u64;
+
+
+# endif /* _mytypes_h */
