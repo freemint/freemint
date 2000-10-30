145e2a7d7f78d91989d92b8ba8a744a042a41002
Frank Naumann <fnaumann@boerde.de>
2000-10-30 13:07:06 +0000

    Splitted off machine dependant signal functions.

--- sys/signal.h
+++ sys/signal.h
@@ -12,25 +12,10 @@
 long	_cdecl	killgroup	(int pgrp, ushort sig, int priv);
 void		post_sig	(PROC *p, ushort sig);
 long	_cdecl	ikill		(int p, ushort sig);
-
 void		check_sigs	(void);
-
 void		raise		(ushort sig);
-void		bombs		(ushort sig);
 void		handle_sig	(ushort sig);
-long	_cdecl	p_sigreturn	(void);
-
 void		stop		(ushort sig);
-void		exception	(ushort sig);
-
-void		sigbus		(void);
-void		sigaddr		(void);
-void		sigill		(void);
-void		sigpriv		(void);
-void		sigfpe		(void);
-void		sigtrap		(void);
-void		haltformat	(void);
-void		haltcpv		(void);
 
 
 # endif /* _signal_h */
