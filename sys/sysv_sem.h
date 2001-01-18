935704ce83d53773ace1c1ccf8b7003b8c997ac7
Frank Naumann <fnaumann@boerde.de>
2001-01-18 13:05:27 +0000

    Started to implement sysV compatible IPC mechanism.

---
+++ sys/sysv_sem.h
@@ -0,0 +1,78 @@
+/*
+ * This file belongs to FreeMiNT. It's not in the original MiNT 1.12
+ * distribution. See the file CHANGES for a detailed log of changes.
+ * 
+ * 
+ * Copyright 2000 Frank Naumann <fnaumann@freemint.de>
+ * All rights reserved.
+ * 
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * 
+ * 
+ * begin:	2000-12-05
+ * last change:	2000-12-05
+ * 
+ * Author:	Frank Naumann <fnaumann@freemint.de>
+ * 
+ * Please send suggestions, patches or bug reports to me or
+ * the MiNT mailing list.
+ * 
+ */
+
+# ifndef _sysv_sem_h
+# define _sysv_sem_h
+
+# include "mint/mint.h"
+# include "mint/sem.h"
+
+
+/*
+ * Kernel implementation stuff
+ */
+#define SEMVMX	32767		/* semaphore maximum value */
+#define SEMAEM	16384		/* adjust on exit max value */
+
+#define MAX_SOPS	5	/* maximum # of sembuf's per semop call */
+
+/*
+ * Permissions
+ */
+#define SEM_A		0200	/* alter permission */
+#define SEM_R		0400	/* read permission */
+
+/*
+ * Undo structure (one per process)
+ */
+struct sem_undo
+{
+	struct	sem_undo *un_next;	/* ptr to next active undo structure */
+	struct	proc *un_proc;		/* owner of this structure */
+	short	un_cnt;			/* # of active entries */
+	struct undo {
+		short	un_adjval;	/* adjust on exit values */
+		short	un_num;		/* semaphore # */
+		int	un_id;		/* semid */
+	} un_ent[1];			/* undo entries */
+};
+
+union __semun;
+
+long sys_semctl (long semid, long semnum, long cmd, union __semun *arg);
+long sys_semget (long key, long nsems, long semflg);
+long sys_semop (long semid, struct sembuf *sops, long nsops);
+long sys_semconfig (long flag);
+
+
+# endif	/* _sysv_sem_h  */
