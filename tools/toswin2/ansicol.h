2e593aa8451ffe7bc94cbc4fb9b0e36f53e2e907
Guido Flohr <guido.flohr@cantanea.com>
2001-04-03 21:53:20 +0000

    - better support for ANSI coloring

---
+++ tools/toswin2/ansicol.h
@@ -0,0 +1,32 @@
+/* Declarations for ANSI color stuff.
+ * Copyright (C) 2001, Guido Flohr <guido@freemint.de>,
+ * all rights reserved.
+ */
+ 
+#ifndef tw_ansicol_h
+# define tw_ansicol_h 1
+
+#define ANSI_BLACK	0
+#define ANSI_RED	1
+#define ANSI_GREEN	2
+#define ANSI_YELLOW	3
+#define ANSI_BLUE	4
+#define ANSI_MAGENTA	5
+#define ANSI_CYAN	6
+#define ANSI_WHITE	7
+
+#define ANSI_DEFAULT_FGCOLOR ANSI_WHITE
+#define ANSI_DEFAULT_BGCOLOR ANSI_BLACK
+
+extern const int ansi2vdi[8];
+extern const int vdi2ansi[8];
+
+void init_ansi_colors (const short* __work_out);
+void set_ansi_fg_color  (TEXTWIN* __textwin, int __color);
+void set_ansi_bg_color  (TEXTWIN* __textwin, int __color);
+void use_ansi_colors    (TEXTWIN* __textwin, unsigned long __flag, 
+			 int* __fgcolor, int* __bgcolor,
+			 int* __texteffects);
+int get_ansi_color (int __color);
+
+#endif
