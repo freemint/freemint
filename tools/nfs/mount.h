866fd2710042f5dc7a218a747c1aa85d4dc196ac
Frank Naumann <fnaumann@boerde.de>
2001-04-09 15:11:42 +0000

    Reworked, removed the rpc lib and use the mintlib rpc calls. Changed to 32bit int too.

--- tools/nfs/mount.h
+++ tools/nfs/mount.h
@@ -12,89 +12,18 @@
  *        specification of the mount protocol
  */
 
-#ifndef MOUNT_H
-#define MOUNT_H
+# ifndef _mount_h
+# define _mount_h
 
-#include <sys/types.h>
-#include "xdr.h"
-
-
-/* request numbers for nfs mount service */
-#define MOUNTPROC_NULL     0
-#define MOUNTPROC_MNT      1
-#define MOUNTPROC_DUMP     2
-#define MOUNTPROC_UMNT     3
-#define MOUNTPROC_UMNTALL  4
-#define MOUNTPROC_EXPORT   5
-
-#define MOUNT_PROGRAM   100005
-#define MOUNT_VERSION   1
-#define MOUNT_MAXPROC   5
+# include <sys/types.h>
+# include <rpc/xdr.h>
 
 
+/* option indicators */
+extern int verbose;
+extern int readonly;
+extern int nosuid;
+extern int grpid;
 
 
-
-#define MNTPATHLEN   1024
-#define MNTNAMLEN     255
-#define MNTFHSIZE      32
-
-
-bool_t xdr_dirpath(xdrs *x, char *s);
-bool_t xdr_name(xdrs *x, char *s);
-
-
-typedef struct fhandle
-{
-	opaque data[MNTFHSIZE];
-} fhandle;
-
-extern bool_t xdr_fhandle(xdrs *x, fhandle *fhp);
-#define xdr_size_fhandle(fhp)  (sizeof(fhandle))
-
-
-typedef struct fhstatus
-{
-	u_long status;
-	union
-	{
-		fhandle directory;   /* if status == 0 */
-	} fhstatus_u;
-} fhstatus;
-
-bool_t xdr_fhstatus(xdrs *x, fhstatus *fhsp);
-long xdr_size_fhstatus(fhstatus *fhsp);
-
-
-typedef struct mountlist
-{
-	char *ml_hostname;
-	char *ml_directory;
-	struct mountlist *ml_next;
-} mountlist;
-
-bool_t xdr_mountlist(xdrs *x, mountlist *mlp);
-long xdr_size_mountlist(mountlist *mlp);
-
-
-typedef struct groups
-{
-	char *gr_name;
-	struct groups *gr_next;
-} groups;
-
-bool_t xdr_groups(xdrs *x, groups *gp);
-long xdr_size_groups(groups *gp);
-
-
-typedef struct exportlist
-{
-	char *ex_filesys;
-	groups *ex_groups;
-	struct exportlist *ex_next;
-} exportlist;
-
-bool_t xdr_exportlist(xdrs *x, exportlist *elp);
-long xdr_size_exportlist(exportlist *elp);
-
-#endif
+# endif
