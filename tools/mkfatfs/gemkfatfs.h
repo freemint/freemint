d156938d769556676275f905b4179c0c7cc7c8ed
Frank Naumann <fnaumann@boerde.de>
2001-04-10 18:42:18 +0000

    Added gemkfatfs from Draco (don't work at the moment).

---
+++ tools/mkfatfs/gemkfatfs.h
@@ -0,0 +1,61 @@
+/* Resource file indices for MKFATFS. */
+
+#define WINDOW           0   /* Form/Dialog-box */
+#define MYHOMEPAGE       2   /* TEXT in tree WINDOW */
+#define DRIVES           3   /* IBOX in tree WINDOW */
+#define DISK_C           4   /* BUTTON in tree WINDOW */
+#define MAKEFS           34  /* BUTTON in tree WINDOW */
+#define EXIT             35  /* BUTTON in tree WINDOW */
+#define FAT12            38  /* BUTTON in tree WINDOW */
+#define FAT16            39  /* BUTTON in tree WINDOW */
+#define FAT32            40  /* BUTTON in tree WINDOW */
+#define SETVOLUMELABEL   41  /* BUTTON in tree WINDOW */
+#define VOLUMELABEL      42  /* FTEXT in tree WINDOW */
+#define SETVOLUMEID      43  /* BUTTON in tree WINDOW */
+#define VOLUMEID         44  /* FTEXT in tree WINDOW */
+#define ONEFAT           46  /* BUTTON in tree WINDOW */
+#define TWOFATS          47  /* BUTTON in tree WINDOW */
+#define THREEFATS        48  /* BUTTON in tree WINDOW */
+#define FOURFATS         49  /* BUTTON in tree WINDOW */
+#define SETROOTENTRIES   50  /* BUTTON in tree WINDOW */
+#define ROOTENTRIES      51  /* FTEXT in tree WINDOW */
+#define SETSPC           52  /* BUTTON in tree WINDOW */
+#define SPC              53  /* BUTTON in tree WINDOW */
+#define CHECKFAT         54  /* BUTTON in tree WINDOW */
+#define BADBLOCKFILENAME 55  /* BUTTON in tree WINDOW */
+#define NOPARTITIONIDCHK 56  /* BUTTON in tree WINDOW */
+#define BOOTMESSAGE      57  /* BUTTON in tree WINDOW */
+
+#define ICON             1   /* Form/Dialog-box */
+
+#define POPUP            2   /* Form/Dialog-box */
+#define POP_PARENT       0   /* BOX in tree POPUP */
+#define SPC2             1   /* STRING in tree POPUP */
+#define SPC4             2   /* STRING in tree POPUP */
+#define SPC8             3   /* STRING in tree POPUP */
+#define SPC16            4   /* STRING in tree POPUP */
+#define SPC32            5   /* STRING in tree POPUP */
+#define SPC64            6   /* STRING in tree POPUP */
+#define SPC128           7   /* STRING in tree POPUP */
+
+#define WARNING          0   /* Alert-string */
+
+#define MKFATFSOK        1   /* Alert-string */
+
+#define MKFATFSKILL      2   /* Alert-string */
+
+#define SIGNALTERM       3   /* Alert-string */
+
+#define NOMKFATFS        4   /* Free String */
+
+#define PNAME            5   /* Free String */
+
+#define WINTITLE         6   /* Free String */
+
+#define FSELMASK         7   /* Free String */
+
+#define ERRMKFATFS       8   /* Free String */
+
+#define FSELTIT1         9   /* Free String */
+
+#define FSELTIT2         10  /* Free String */
