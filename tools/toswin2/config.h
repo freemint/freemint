a221e5e0e9c8c04d51e5c38f69ed103b2d738db2
Guido Flohr <guido.flohr@cantanea.com>
2001-04-25 01:29:55 +0000

    - moved definition of struct wincfg here (from global.h)

--- tools/toswin2/config.h
+++ tools/toswin2/config.h
@@ -1,14 +1,43 @@
 
-#ifndef _tw_config_h_
-#define _tw_config_h_
+#ifndef tw_config_h
+# define tw_config_h
 
+#ifndef tw_global_h
+# include "global.h"
+#endif
+#ifndef tw_window_h
+# include "window.h"
+#endif
+
+typedef struct _wincfg WINCFG;
+struct _wincfg
+{
+	WINCFG	*next;
+	char	progname[80];
+	char	arg[50];
+	char	title[80];
+	int	kind;
+	int	font_id, font_pts;
+	int	col, row, scroll;
+	int	xpos, ypos;
+	int	width, height;		/* 'offene' Gr”že */
+	bool	iconified;
+	bool	wrap;
+	bool	autoclose;
+	int	vt_mode;
+	int	fg_color;
+	int	bg_color;
+	int	vdi_colors;
+	unsigned long	fg_effects;
+	unsigned long	bg_effects;
+	int	char_tab;
+};
 
 /*
  * Variablen
  */
-extern bool	gl_avcycle,
-		gl_shortcut,
-		gl_allogin;
+extern bool	gl_shortcut;
+extern bool	gl_allogin;
 
 extern bool	gl_con_auto,
 		gl_con_output,
