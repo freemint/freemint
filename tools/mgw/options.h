9255b04c7ff972037e5f94dc46cc8db28f2d0a29
Frank Naumann <fnaumann@boerde.de>
2001-06-14 10:23:55 +0000

    Added $Id to the src files, cleaned up the copyright notice.

--- tools/mgw/options.h
+++ tools/mgw/options.h
@@ -1,3 +1,34 @@
+/*
+ * $Id$
+ * 
+ * This file belongs to FreeMiNT. It's not in the original MiNT 1.12
+ * distribution. See the file CHANGES for a detailed log of changes.
+ * 
+ * 
+ * Copyright 1999 Jens Heitmann
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * 
+ * 
+ * Author: Jens Heitmann
+ * Started: 1999-05
+ * 
+ * please send suggestions or bug reports to me or
+ * the MiNT mailing list
+ * 
+ */
 
 # ifndef _options_h
 # define _options_h
