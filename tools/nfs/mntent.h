e01b0620d82e3ab602f6f86501deea78688aa6be
Frank Naumann <fnaumann@boerde.de>
2001-07-03 12:25:29 +0000

    Merged back 1.16 changes, require at least 0.57 MiNTLib now.

--- tools/nfs/mntent.h
+++ tools/nfs/mntent.h
@@ -1,84 +1,84 @@
 /*
- * File: mntent.h
+ * Copyright 1993, 1994 by Ulrich KÅhn. All rights reserved.
  *
+ * THIS PROGRAM COMES WITH ABSOLUTELY NO WARRANTY, NOT
+ * EVEN THE IMPLIED WARRANTIES OF MERCHANTIBILITY OR
+ * FITNESS FOR A PARTICULAR PURPOSE. USE AT YOUR OWN
+ * RISK.
  */
 
-#ifndef MNTENT_H
-#define MNTENT_H
+# ifndef _mntent_h
+# define _mntent_h
 
-#include <stdio.h>
+# include <stdio.h>
 
 
-#define MNTTAB  "/etc/fstab"
-#define MOUNTED "/etc/mtab"
+# define MNTTAB			"/etc/fstab"
+# define MOUNTED		"/etc/mtab"
 
-#define MNTMAXSTR   64
+# define MNTMAXSTR		64
 
 /* file system types */
-#define MNTTYPE_42     "4.2"    /* 4.2 file system */
-#define MNTTYPE_NFS    "nfs"    /* network fs */
-#define MNTTYPE_PC     "pc"     /* DOS (FAT) fs */
-#define MNTTYPE_SWAP   "swap"   /* swap fs */
-#define MNTTYPE_IGNORE "ignore" /* ignore this entry */
-#define MNTTYPE_LO     "lo"     /* loop back fs */
-
-#define MNTTYPE_MNX    "minix"  /* minix fs */
+# define MNTTYPE_42		"4.2"		/* 4.2 file system */
+# define MNTTYPE_NFS		"nfs"		/* network fs */
+# define MNTTYPE_PC		"pc"		/* DOS (FAT) fs */
+# define MNTTYPE_SWAP		"swap"		/* swap fs */
+# define MNTTYPE_IGNORE		"ignore"	/* ignore this entry */
+# define MNTTYPE_LO		"lo"		/* loop back fs */
+# define MNTTYPE_MNX		"minix"		/* minix fs */
 
 /* mount options */
-#define MNTOPT_RO      "ro"      /* read only */
-#define MNTOPT_RW      "rw"      /* read/write */
-#define MNTOPT_GRPID   "grpid"   /* SysV compatible group-id on create */
-#define MNTOPT_REMOUNT "remount" /* change options for previosus mount */
-#define MNTOPT_NOAUTO  "noauto"  /* hide entry for mount -a */
-#define MNTOPT_NOSUB   "nosub"   /* disallow mounts beneath this mount */
+# define MNTOPT_RO		"ro"		/* read only */
+# define MNTOPT_RW		"rw"		/* read/write */
+# define MNTOPT_GRPID		"grpid"		/* SysV compatible group-id on create */
+# define MNTOPT_REMOUNT		"remount"	/* change options for previosus mount */
+# define MNTOPT_NOAUTO		"noauto"	/* hide entry for mount -a */
+# define MNTOPT_NOSUB		"nosub"		/* disallow mounts beneath this mount */
 
 /* 4.2 specific options */
-#define MNTOPT_QUOTA   "quota"   /* with quota */
-#define MNTOPT_NOQUOTA "noquota" /* w/o quota */
+# define MNTOPT_QUOTA		"quota"		/* with quota */
+# define MNTOPT_NOQUOTA		"noquota"	/* w/o quota */
 
 /* NFS specific options */
-#define MNTOPT_SOFT    "soft"    /* soft mount */
-#define MNTOPT_HARD    "hard"    /* hard mount (default) */
-#define MNTOPT_NOSUID  "nosuid"  /* no set uid allowed */
-#define MNTOPT_INTR    "intr"    /* allow interrupts on hard mount */
-#define MNTOPT_SECURE  "secure"  /* use secure RPC */
-#define MNTOPT_NOAC    "noac"    /* do not cache file attributes */
-#define MNTOPT_NOCTO   "nocto"   /* no "close to open" attr consistency */
-#define MNTOPT_PORT    "port"    /* server port no */
-#define MNTOPT_RETRANS "retrans" /* number of request retries */
-#define MNTOPT_RSIZE   "rsize"   /* read size (bytes) */
-#define MNTOPT_WSIZE   "wsize"   /* write size (bytes) */
-#define MNTOPT_TIMEO   "timeo"   /* initial timeout (1/10 sec) */
-#define MNTOPT_ACTIMEO "actimeo" /* attr cache timeout (sec) */
-#define MNTOPT_ACREGMIN "acregmin" /* min ac timeout for reg files (sec) */
-#define MNTOPT_ACREGMAX "acregmax" /* max ac timeout for reg files */
-#define MNTOPT_ACDIRMIN "acdirmin" /* min ac timeout for dirs */
-#define MNTOPT_ACDIRMAX "acdirmax" /* max ac timeout for dirs */
-#define MNTOPT_POSIX   "posix"   /* ask for static pathconf values */
-                                 /* from mountd */
+# define MNTOPT_SOFT		"soft"		/* soft mount */
+# define MNTOPT_HARD		"hard"		/* hard mount (default) */
+# define MNTOPT_NOSUID		"nosuid"	/* no set uid allowed */
+# define MNTOPT_INTR		"intr"		/* allow interrupts on hard mount */
+# define MNTOPT_SECURE		"secure"	/* use secure RPC */
+# define MNTOPT_NOAC		"noac"		/* do not cache file attributes */
+# define MNTOPT_NOCTO		"nocto"		/* no "close to open" attr consistency */
+# define MNTOPT_PORT		"port"		/* server port no */
+# define MNTOPT_RETRANS		"retrans"	/* number of request retries */
+# define MNTOPT_RSIZE		"rsize"		/* read size (bytes) */
+# define MNTOPT_WSIZE		"wsize"		/* write size (bytes) */
+# define MNTOPT_TIMEO		"timeo"		/* initial timeout (1/10 sec) */
+# define MNTOPT_ACTIMEO		"actimeo"	/* attr cache timeout (sec) */
+# define MNTOPT_ACREGMIN	"acregmin"	/* min ac timeout for reg files (sec) */
+# define MNTOPT_ACREGMAX	"acregmax"	/* max ac timeout for reg files */
+# define MNTOPT_ACDIRMIN	"acdirmin"	/* min ac timeout for dirs */
+# define MNTOPT_ACDIRMAX	"acdirmax"	/* max ac timeout for dirs */
+# define MNTOPT_POSIX		"posix"		/* ask for static pathconf values */
+                                 		/* from mountd */
 
 /* Information about the mount entry */
-#define MNTINFO_DEV   "dev"   /* device number of the mounted fs */
-
-
+# define MNTINFO_DEV		"dev"		/* device number of the mounted fs */
 
 struct mntent
 {
-	char *mnt_fsname;   /* name of mounted file system */
-	char *mnt_dir;      /* file system path prefix */
-	char *mnt_type;     /* MNTTYPE_* */
-	char *mnt_opts;     /* MNTOPT_* */
-	int  mnt_freq;      /* dump frequenciy in days */
-	int  mnt_passno;    /* pass number on parallel fsck */
+	char *mnt_fsname;	/* name of mounted file system */
+	char *mnt_dir;		/* file system path prefix */
+	char *mnt_type;		/* MNTTYPE_* */
+	char *mnt_opts;		/* MNTOPT_* */
+	int  mnt_freq;		/* dump frequenciy in days */
+	int  mnt_passno;	/* pass number on parallel fsck */
 };
 
 
+FILE *		setmntent (char *name, char *type);
+struct mntent *	getmntent (FILE *fp);
+int		addmntent (FILE *fp, struct mntent *mnt);
+char *		hasmntopt (struct mntent *mnt, char *opt);
+int		endmntent (FILE *fp);
 
-FILE * setmntent ( char *name, char *type );
-struct mntent * getmntent ( FILE *fp );
-int addmntent ( FILE *fp, struct mntent *mnt );
-char * hasmntopt (struct mntent *mnt, char *opt);
-int endmntent ( FILE *fp );
 
-
-#endif
+# endif
--- tools/nfs/mount_xdr.c
+++ tools/nfs/mount_xdr.c
@@ -15,172 +15,184 @@
 
 #include <string.h>
 #include <sys/types.h>
-#include "xdr.h"
-#include "mount.h"
 
+#include "mount_xdr.h"
 
 
 bool_t
-xdr_dirpath(xdrs *x, char *s)
+xdr_dirpath (XDR *x, char *s)
 {
-	return xdr_string(x, &s, MNTPATHLEN);
+	return xdr_string (x, &s, MNTPATHLEN);
 }
 
 
 bool_t
-xdr_name(xdrs *x, char *s)
+xdr_name (XDR *x, char *s)
 {
-	return xdr_string(x, &s, MNTNAMLEN);
+	return xdr_string (x, &s, MNTNAMLEN);
 }
 
 
 bool_t
-xdr_fhandle(xdrs *x, fhandle *fhp)
+xdr_fhandle (XDR *x, fhandle *fhp)
 {
-	return xdr_fixedopaq(x, fhp->data, MNTFHSIZE);
+	return xdr_opaque (x, fhp->data, MNTFHSIZE);
 }
 
 
 bool_t
-xdr_fhstatus(xdrs *x, fhstatus *fhsp)
+xdr_fhstatus (XDR *x, fhstatus *fhsp)
 {
-	if (!xdr_ulong(x, &fhsp->status))
+	if (!xdr_u_long (x, &fhsp->status))
 		return FALSE;
+	
 	if (0 == fhsp->status)
-		return xdr_fhandle(x, &fhsp->fhstatus_u.directory);
+		return xdr_fhandle (x, &fhsp->fhstatus_u.directory);
+	
 	return TRUE;
 }
 
 long
-xdr_size_fhstatus(fhstatus *fhsp)
+xdr_size_fhstatus (fhstatus *fhsp)
 {
-	long r = sizeof(u_long);
-
+	long r = sizeof (u_long);
+	
 	if (0 == fhsp->status)
-		r += xdr_size_fhandle(&fhsp->fhstatus_u.directory);
+		r += xdr_size_fhandle (&fhsp->fhstatus_u.directory);
+	
 	return r;
 }
 
 
 bool_t
-xdr_mountlist(xdrs *x, mountlist *mlp)
+xdr_mountlist (XDR *x, mountlist *mlp)
 {
-	char *p = (char*)mlp;
-
-	if (XDR_DECODE == x->op)
+	char *p = (char *) mlp;
+	
+	if (XDR_DECODE == x->x_op)
 	{
-		p += sizeof(mountlist);
+		p += sizeof (mountlist);
 		mlp->ml_hostname = p;
 	}
-	if (!xdr_string(x, &mlp->ml_hostname, MNTNAMLEN))
+	
+	if (!xdr_string (x, &mlp->ml_hostname, MNTNAMLEN))
 		return FALSE;
-	if (XDR_DECODE == x->op)
+	
+	if (XDR_DECODE == x->x_op)
 	{
-		p += strlen(mlp->ml_hostname)+1;
+		p += strlen (mlp->ml_hostname) + 1;
 		mlp->ml_directory = p;
 	}
-	if (!xdr_string(x, &mlp->ml_directory, MNTPATHLEN))
+	
+	if (!xdr_string (x, &mlp->ml_directory, MNTPATHLEN))
 		return FALSE;
-	if (XDR_DECODE == x->op)
+	
+	if (XDR_DECODE == x->x_op)
 	{
-		p += strlen(mlp->ml_directory);
-		mlp->ml_next = (mountlist*)(((long)p+1)&(~1L));
+		p += strlen (mlp->ml_directory);
+		mlp->ml_next = (mountlist *)(((long) p + 1) & (~1L));
 	}
-	return xdr_pointer(x, (char**)&mlp->ml_next, 
-	                          sizeof(mountlist), (xdrproc_t)xdr_mountlist);
+	
+	return xdr_pointer (x, (char **) &mlp->ml_next, 
+	                          sizeof (mountlist), (xdrproc_t) xdr_mountlist);
 }
 
 long
-xdr_size_mountlist(mountlist *mlp)
+xdr_size_mountlist (mountlist *mlp)
 {
 	long r = 0;
 
 	while (mlp)
 	{
-		r += 3*sizeof(u_long);
-		r += (strlen(mlp->ml_hostname)+3)&(~3L);
-		r += (strlen(mlp->ml_directory)+3)&(~3L);
+		r += 3 * sizeof (u_long);
+		r += (strlen (mlp->ml_hostname) + 3) & (~3L);
+		r += (strlen (mlp->ml_directory) + 3) & (~3L);
 		mlp = mlp->ml_next;
 	}
 	return r;
 }
 
-
 bool_t
-xdr_groups(xdrs *x, groups *gp)
+xdr_groups(XDR *x, groups *gp)
 {
-	char *p = (char*)gp;
-
-	if (XDR_DECODE == x->op)
+	char *p = (char *) gp;
+	
+	if (XDR_DECODE == x->x_op)
 	{
-		p += sizeof(groups);
+		p += sizeof (groups);
 		gp->gr_name = p;
 	}
-	if (!xdr_string(x, &gp->gr_name, MNTNAMLEN))
+	
+	if (!xdr_string (x, &gp->gr_name, MNTNAMLEN))
 		return FALSE;
-	if (XDR_DECODE == x->op)
+	
+	if (XDR_DECODE == x->x_op)
 	{
-		p += strlen(gp->gr_name)+1;
-		gp->gr_next = (groups*)(((long)p+1)&(~1L));
+		p += strlen (gp->gr_name) + 1;
+		gp->gr_next = (groups *)(((long) p + 1) & (~1L));
 	}
-	return xdr_pointer(x, (char**)&gp->gr_next, 
-	                          sizeof(groups), (xdrproc_t)xdr_groups);
+	
+	return xdr_pointer (x, (char **) &gp->gr_next, 
+	                          sizeof (groups), (xdrproc_t) xdr_groups);
 }
 
 long
-xdr_size_groups(groups *gp)
+xdr_size_groups (groups *gp)
 {
 	long r = 0;
 
 	while (gp)
 	{
-		r += 2*sizeof(u_long);
-		r += (strlen(gp->gr_name)+3)&(~3L);
+		r += 2 * sizeof (u_long);
+		r += (strlen (gp->gr_name) + 3) & (~3L);
 		gp = gp->gr_next;
 	}
 	return r;
 }
 
-
 bool_t
-xdr_exportlist(xdrs *x, exportlist *elp)
+xdr_exportlist (XDR *x, exportlist *elp)
 {
-	char *p = (char*)elp;
-
-	if (XDR_DECODE == x->op)
+	char *p = (char *) elp;
+	
+	if (XDR_DECODE == x->x_op)
 	{
-		p += sizeof(exportlist);
+		p += sizeof (exportlist);
 		elp->ex_filesys = p;
 	}
-	if (!xdr_string(x, &elp->ex_filesys, MNTPATHLEN))
+	
+	if (!xdr_string (x, &elp->ex_filesys, MNTPATHLEN))
 		return FALSE;
-	if (XDR_DECODE == x->op)
+	
+	if (XDR_DECODE == x->x_op)
 	{
 		groups *gp = elp->ex_groups;
-
+		
 		if (gp)
 		{
 			while (gp->gr_next)
 				gp = gp->gr_next;
-			p = gp->gr_name+strlen(gp->gr_name)+1;
+			p = gp->gr_name + strlen (gp->gr_name) + 1;
 		}
-		elp->ex_next = (exportlist*)(((long)p+1)&(~1L));
+		elp->ex_next = (exportlist *)(((long) p + 1) & (~1L));
 	}
-	return xdr_pointer(x, (char**)&elp->ex_next, 
-	                         sizeof(exportlist), (xdrproc_t)xdr_exportlist);
+	
+	return xdr_pointer (x, (char **) &elp->ex_next, 
+	                         sizeof (exportlist), (xdrproc_t) xdr_exportlist);
 }
 
 long
-xdr_size_exportlist(exportlist *elp)
+xdr_size_exportlist (exportlist *elp)
 {
 	long r = 0;
-
+	
 	while (elp)
 	{
-		r += 3*sizeof(u_long);
-		r += (strlen(elp->ex_filesys)+3)&(~3L);
-		r += xdr_size_groups(elp->ex_groups);
+		r += 3 * sizeof (u_long);
+		r += (strlen (elp->ex_filesys) + 3) & (~3L);
+		r += xdr_size_groups (elp->ex_groups);
 		elp = elp->ex_next;
 	}
+	
 	return r;
 }
