842f1d24d531e2421242f6c1ba660f7ec89c94a9
Frank Naumann <fnaumann@boerde.de>
2001-07-03 11:57:52 +0000

    1.16 bugfix, new pl script for crosscompiling.

--- sys/arch/magic/genmagic.pl
+++ sys/arch/magic/genmagic.pl
@@ -1,38 +1,31 @@
-#! /usr/bin/perl
-# genmagic -- dump magics from (crosscompiled...) genmagic executable
-#
+#!/usr/bin/perl
+# 
+# genmagic -- dump magics from (crosscompiled...) genmagic.o object
+# file
+# 
 # This file belongs to FreeMiNT.  It's not in the original MiNT 1.12
 # distribution.
-#
-$f = 'genmagic.ttp';
-$sym = '_magics';
+#                                                                              
+$f = 'genmagic.o';
+$firstsym = 'C_PTRACE';
 $val = 'A28 N';
-#	const char name[28];
-#	long value;
+#       const char name[28];
+#       long value;                                                            
 
-# nm for ST executables (-> binutils)
-# edit
-$nm = '/scratch_hp/fnaumann/m68k-atari-mint/bin/tnm';
-
-($npid = open (NM, "$nm $f|")) || die "nm failed";
-$search = << ;
-while (<NM>) {
-	if (/^$sym\\s+([0-9a-fA-F]+)\\s/) {
-		\$off = hex (\$1) + 0x1c;
-		print STDERR;
-		break;
-	}
+open(M, $f) || die "open failed";
+$offset = 0;
+while (sysread(M, $d, length(pack( $val, 0)))) {
+    ($name, $valx) = unpack( $val, $d );
+    last if ( $name =~ /^$firstsym$/s );
+    $offset = ++$offset;
+    sysseek(M, $offset, SEEK_SET) || die "can't seek";
 }
 
-eval $search;
-close (NM);
-die "not found" if (!defined ($off));
-
-open (F, $f) || die "Can't open";
-seek (F, $off, 0) || die "Can't lseek";
-while (sysread (F, $d, length (pack ($val, 0)))) {
-	($name, $valx) = unpack ($val, $d);
-	last if !$name;
-	print "%define $name $valx\n";
+$name =~ /^$firstsym$/s || die "symbols not found";
+sysseek(M, $offset, 0) || die "can't seek";
+while (sysread(M, $d, length(pack( "A28 N", 0)))) {
+    ($name, $valx) = unpack( "A28 N", $d);
+    last if !$name;
+    print "#define $name $valx\n";
 }
-close F || die "Can't close";
+close M || die "Can't close";
