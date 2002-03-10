a957589fd60895a04938569aafa9f5307efed508
Konrad Kokoszkiewicz <drac030@krap.pl>
2002-03-10 23:21:29 +0000

    Implemented the XBIOS call Bioskeys() Completed the keyboard initialization routines. Switched to the internal keyboard translation tables.

--- sys/keyboard.h
+++ sys/keyboard.h
@@ -2,21 +2,21 @@
  *
  * This file belongs to FreeMiNT. It's not in the original MiNT 1.12
  * distribution. See the file CHANGES for a detailed log of changes.
- * 
+ *
  * This file is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This file is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- * 
+ *
  */
 
 # ifndef _keyboard_h
@@ -94,6 +94,7 @@ short ikbd_scan(ushort scancode);
 
 void init_keybd(void);
 void load_keytbl(void);
+void bioskeys(void);
 
 extern short gl_kbd;
 extern struct cad_def cad[3];
