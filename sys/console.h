7eb77d8b6a38034dde3252d938af2bc4ee4be5dc
Konrad Kokoszkiewicz <drac030@krap.pl>
2003-03-05 21:08:11 +0000

    c_* functions renamed to sys_c_* to be compliant with the new automatically generated syscalls.c module.

--- sys/console.h
+++ sys/console.h
@@ -1,6 +1,6 @@
 /*
  * $Id$
- * 
+ *
  * This file has been modified as part of the FreeMiNT project. See
  * the file Changes.MH for details and dates.
  */
@@ -17,26 +17,26 @@ long file_outstat	(FILEPTR *f);
 long file_getchar	(FILEPTR *f, int mode);
 long file_putchar	(FILEPTR *f, long c, int mode);
 
-long _cdecl c_conin	(void);
-long _cdecl c_conout	(int c);
-long _cdecl c_auxin	(void);
-long _cdecl c_auxout	(int c);
-long _cdecl c_prnout	(int c);
-long _cdecl c_rawio	(int c);
-long _cdecl c_rawcin	(void);
-long _cdecl c_necin	(void);
-long _cdecl c_conws	(const char *str);
-long _cdecl c_conrs	(char *buf);
-long _cdecl c_conis	(void);
-long _cdecl c_conos	(void);
-long _cdecl c_prnos	(void);
-long _cdecl c_auxis	(void);
-long _cdecl c_auxos	(void);
+long _cdecl sys_c_conin		(void);
+long _cdecl sys_c_conout	(int c);
+long _cdecl sys_c_auxin		(void);
+long _cdecl sys_c_auxout	(int c);
+long _cdecl sys_c_prnout	(int c);
+long _cdecl sys_c_rawio		(int c);
+long _cdecl sys_c_rawcin	(void);
+long _cdecl sys_c_necin		(void);
+long _cdecl sys_c_conws		(const char *str);
+long _cdecl sys_c_conrs		(char *buf);
+long _cdecl sys_c_conis		(void);
+long _cdecl sys_c_conos		(void);
+long _cdecl sys_c_prnos		(void);
+long _cdecl sys_c_auxis		(void);
+long _cdecl sys_c_auxos		(void);
 
-long _cdecl f_instat	(int fh);
-long _cdecl f_outstat	(int fh);
-long _cdecl f_getchar	(int fh, int mode);
-long _cdecl f_putchar	(int fh, long c, int mode);
+long _cdecl sys_f_instat	(int fh);
+long _cdecl sys_f_outstat	(int fh);
+long _cdecl sys_f_getchar	(int fh, int mode);
+long _cdecl sys_f_putchar	(int fh, long c, int mode);
 
 
 # endif /* _console_h */
