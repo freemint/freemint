c610ccfee87eb56650ba0056f7c9b9376f01345b
Konrad Kokoszkiewicz <drac030@krap.pl>
2003-03-26 21:37:32 +0000

    Save the complete absolute path in sysdir[], because pathnames like '\multitos\keyboard.tbl' displayed at bootup seem to be confusing for some users (who think it means 'u:\multitos...'). Also, use backslashes in it, when MiNT is already up.

--- sys/global.h
+++ sys/global.h
@@ -53,7 +53,7 @@ extern int screen_boundary;
 extern int flk;
 extern int FalconVideo;
 extern short ste_video;
-extern char *sysdir;
+extern char sysdir[];
 extern short sysdrv;
 
 # define MAXLANG 6	/* languages supported */
