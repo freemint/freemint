4a0240674aacf3c78797f7d0dc0f049402180d04
standa <standa>
2003-03-26 10:12:44 +0000

    Long filenames removal:    get_parent() moved to the my_aes.c    nkcc_gem_to_norm() moved to nkcc.c

--- xaaes/src/my_aes.c
+++ xaaes/src/my_aes.c
@@ -1,6 +1,7 @@
 
-#include "libkern.h"
+#include "global.h"
 #include "my_aes.h"
+#include "xa_types.h"
 
 
 /* Array sizes in aes control block */
@@ -21,6 +22,8 @@ struct aespb
 	long        *addrout;
 };
 
+#if defined(__GNUC_INLINE__) && (__GNUC__ > 2 || __GNUC_MINOR__ > 5)
+
 static inline void
 _aes_trap(struct aespb *aespb)
 {
@@ -34,6 +37,15 @@ _aes_trap(struct aespb *aespb)
 		: "d0", "d1"
 	);
 }
+
+#else
+
+/** perform AES trap */
+extern void aes (struct aespb *pb);
+
+#define _aes_trap(aespb) aes(aespb)
+
+#endif
 
 #define AES_TRAP(aespb, cntrl_0, cntrl_1, cntrl_2, cntrl_3, cntrl_4) \
 	my_aes_control[0] = cntrl_0; \
@@ -50,7 +62,7 @@ short my_gl_apid;
 short my_gl_ap_version;
 
 /*
- * global aes binding params 
+ * global aes binding params
  */
 static short	my_aes_control [AES_CTRLMAX];
 static short	my_aes_global  [AES_GLOBMAX];
@@ -94,7 +106,7 @@ short
 my_appl_exit(void)
 {
 	AES_TRAP(my_aes_params, 19, 0,1,0,0);
-	
+
 	return my_aes_intout[0];
 }
 
@@ -102,11 +114,34 @@ short
 my_graf_handle(short *Wchar, short *Hchar, short *Wbox, short *Hbox)
 {
 	AES_TRAP(my_aes_params, 77, 0,5,0,0);
-	
+
 	*Wchar = my_aes_intout[1];
 	*Hchar = my_aes_intout[2];
 	*Wbox  = my_aes_intout[3];
 	*Hbox  = my_aes_intout[4];
-	
+
 	return my_aes_intout[0];
+}
+
+
+/*
+ * Returns the object number of this object's parent or -1 if it is the root
+ */
+int
+get_parent(OBJECT *t, int object)
+{
+	if (object)
+	{
+		int last;
+
+		do {
+			last = object;
+			object = t[object].ob_next;
+		}
+		while(t[object].ob_tail != last);
+
+		return object;
+	}
+
+	return -1;
 }
--- xaaes/src/nkcc.c
+++ xaaes/src/nkcc.c
@@ -2,6 +2,18 @@
 #include "nkcc.h"
 
 
+unsigned short
+gem_to_norm (short ks, short kr)
+{
+	unsigned short knorm;
+
+	knorm = nkc_tos2n (((long) (kr & 0xff) |	/* ascii Bits 0-7 */
+			    (((long) kr & 0x0000ff00L) << 8L) |	/* scan Bits 16-23 */
+			    ((long) (ks & 0xff) << 24L)));	/* kstate Bits 24-31 */
+	return knorm;
+}
+
+
 int
 nkc_exit(void)
 {
