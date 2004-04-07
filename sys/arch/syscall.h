5a88d75129269b1442f621e6c04f301e597d2bda
fna <fna>
2004-04-07 20:54:22 +0000

    Added #ifdef SYSCALL_REENTRANT around the new stuff, added some more comments and added BIOS/XBIOS #define's for directly jumping to the original vector.

--- sys/arch/syscall.h
+++ sys/arch/syscall.h
@@ -68,55 +68,103 @@ void	_cdecl	do_usrcall	(void);
 
 # ifdef SYSCALL_REENTRANT
 
-extern long trap_1_emu(int fnum, ...);
+long	_cdecl	trap_1_emu(short fnum, ...);
+long	_cdecl	trap_13_emu(short fnum, ...);
+long	_cdecl	trap_14_emu(short fnum, ...);
 
 /* Currently these bindings are only needed in biosfs.c in the rsvf_* functions
  * and in tosfs.c, the old TOS-FS.
  */
 
-# define ROM_Fsetdta(dta)			(void)	trap_1_emu( 26, dta)
-# define ROM_Dfree(buf, d)			(long)	trap_1_emu( 54, buf, d)
-# define ROM_Dcreate(path)			(int)	trap_1_emu( 57, path)
-# define ROM_Ddelete(path)			(long)	trap_1_emu( 58, path)
-# define ROM_Fcreate(fn, mode)			(long)	trap_1_emu( 60, fn, mode)
-# define ROM_Fopen(filename, mode)		(long)	trap_1_emu( 61, filename, mode)
-# define ROM_Fclose(handle)			(long)	trap_1_emu( 62, handle)
-# define ROM_Fread(handle, cnt, buf)		(long)	trap_1_emu( 63, handle, cnt, buf)
-# define ROM_Fwrite(handle, cnt, buf)		(long)	trap_1_emu( 64, handle, cnt, buf)
-# define ROM_Fdelete(fn)			(long)	trap_1_emu( 65, fn)
-# define ROM_Fseek(where, handle, how)		(long)	trap_1_emu( 66, where, handle, how)
-# define ROM_Fattrib(fn, rwflag, attr)		(int)	trap_1_emu( 67, fn, rwflag, attr)
-# define ROM_Fsfirst(filespec, attr)		(long)	trap_1_emu( 78, filespec, attr)
-# define ROM_Fsnext()				(long)	trap_1_emu( 79)
-# define ROM_Frename(zero, old, new)		(int)	trap_1_emu( 86, zero, old, new)
-# define ROM_Fdatime(timeptr, handle, rwflag)	(long)	trap_1_emu( 87, timeptr, handle, rwflag)
-# define ROM_Flock(handle, mode, start, length)	(long)	trap_1_emu( 92, handle, mode, start, length)
-# define ROM_Fcntl(f, arg, cmd)			(long)	trap_1_emu(260, f, arg, cmd)
+# define ROM_Fsetdta(dta)			(void)	trap_1_emu(0x01a, dta)
+# define ROM_Dfree(buf, d)				trap_1_emu(0x036, buf, d)
+# define ROM_Dcreate(path)			(int)	trap_1_emu(0x039, path)
+# define ROM_Ddelete(path)				trap_1_emu(0x03a, path)
+# define ROM_Fcreate(fn, mode)				trap_1_emu(0x03c, fn, mode)
+# define ROM_Fopen(filename, mode)			trap_1_emu(0x03d, filename, mode)
+# define ROM_Fclose(handle)				trap_1_emu(0x03e, handle)
+# define ROM_Fread(handle, cnt, buf)			trap_1_emu(0x03f, handle, cnt, buf)
+# define ROM_Fwrite(handle, cnt, buf)			trap_1_emu(0x040, handle, cnt, buf)
+# define ROM_Fdelete(fn)				trap_1_emu(0x041, fn)
+# define ROM_Fseek(where, handle, how)			trap_1_emu(0x042, where, handle, how)
+# define ROM_Fattrib(fn, rwflag, attr)		(int)	trap_1_emu(0x043, fn, rwflag, attr)
+# define ROM_Fsfirst(filespec, attr)			trap_1_emu(0x04e, filespec, attr)
+# define ROM_Fsnext()					trap_1_emu(0x04f)
+# define ROM_Frename(zero, old, new)		(int)	trap_1_emu(0x056, zero, old, new)
+# define ROM_Fdatime(timeptr, handle, rwflag)		trap_1_emu(0x057, timeptr, handle, rwflag)
+# define ROM_Flock(handle, mode, start, length)		trap_1_emu(0x05c, handle, mode, start, length)
+# define ROM_Fcntl(f, arg, cmd)				trap_1_emu(0x104, f, arg, cmd)
 
-# else
+# define ROM_Bconstat(dev)			(short)	trap_13_emu(0x01,(short)(dev))
+# define ROM_Bconin(dev)				trap_13_emu(0x02,(short)(dev))
+# define ROM_Bconout(dev,c)				trap_13_emu(0x03,(short)(dev),(short)((c) & 0xff))
+# define ROM_Setexc(vnum,vptr)		(void (*)(void))trap_13_emu(0x05,(short)(vnum),(long)(vptr))
+# define ROM_Bcostat(dev)			(short)	trap_13_emu(0x08,(short)(dev))
+# define ROM_Kbshift(data)				trap_13_emu(0x0B,(short)(data))
 
-# include <mintbind.h>
+# define ROM_Initmous(type,param,vptr)		(void)	trap_14_emu(0x00,(short)(type),(long)(param),(long)(vptr))
+# define ROM_Getrez()				(short)	trap_14_emu(0x04)
+# define ROM_Iorec(ioDEV)			(void *)trap_14_emu(0x0E,(short)(ioDEV))
+# define ROM_Rsconf(baud,flow,uc,rs,ts,sc)		trap_14_emu(0x0F,(short)(baud),(short)(flow),\
+								    (short)(uc),(short)(rs),(short)(ts),(short)(sc))
+# define ROM_Keytbl(nrml,shft,caps)		(void *)trap_14_emu(0x10,(long)(nrml),(long)(shft),(long)(caps))
+# define ROM_Cursconf(rate,attr)		(short)	trap_14_emu(0x15,(short)(rate),(short)(attr))
+# define ROM_Settime(time)			(void)	trap_14_emu(0x16,(long)(time))
+# define ROM_Gettime()					trap_14_emu(0x17)
+# define ROM_Offgibit(ormask)			(void)	trap_14_emu(0x1D,(short)(ormask))
+# define ROM_Ongibit(andmask)			(void)	trap_14_emu(0x1E,(short)(andmask))
+# define ROM_Dosound(ptr)			(void)	trap_14_emu(0x20,(long)(ptr))
+# define ROM_Vsync()				(void)	trap_14_emu(0x25)
+# define ROM_Bconmap(dev)				trap_14_emu(0x2c,(short)(dev))
 
-# define ROM_Fsetdta(dta)			Fsetdta(dta)
-# define ROM_Dfree(buf, d)			Dfree(buf, d)
-# define ROM_Dcreate(path)			Dcreate(path)
-# define ROM_Ddelete(path)			Ddelete(path)
-# define ROM_Fcreate(fn, mode)			Fcreate(fn, mode)
-# define ROM_Fopen(filename, mode)		Fopen(filename, mode)
-# define ROM_Fclose(handle)			Fclose(handle)
-# define ROM_Fread(handle, cnt, buf)		Fread(handle, cnt, buf)
-# define ROM_Fwrite(handle, cnt, buf)		Fwrite(handle, cnt, buf)
-# define ROM_Fdelete(fn)			Fdelete(fn)
-# define ROM_Fseek(where, handle, how)		Fseek(where, handle, how)
-# define ROM_Fattrib(fn, rwflag, attr)		Fattrib(fn, rwflag, attr)
-# define ROM_Fsfirst(filespec, attr)		Fsfirst(filespec, attr)
-# define ROM_Fsnext()				Fsnext()
-# define ROM_Frename(zero, old, new)		Frename(zero, old, new)
-# define ROM_Fdatime(timeptr, handle, rwflag)	Fdatime(timeptr, handle, rwflag)
-# define ROM_Flock(handle, mode, start, length)	Flock(handle, mode, start, length)
-# define ROM_Fcntl(f, arg, cmd)			Fcntl(f, arg, cmd)
+# define ROM_VsetScreen(lscrn,pscrn,rez,mode)	(void)	trap_14_emu(0x05,(long)(lscrn),(long)(pscrn),(short)(rez),(short)(mode))
 
-# endif
+# else /* SYSCALL_REENTRANT */
 
+# include <mint/mintbind.h>
+
+# define ROM_Fsetdta(dta)				Fsetdta(dta)
+# define ROM_Dfree(buf, d)				Dfree(buf, d)
+# define ROM_Dcreate(path)				Dcreate(path)
+# define ROM_Ddelete(path)				Ddelete(path)
+# define ROM_Fcreate(fn, mode)				Fcreate(fn, mode)
+# define ROM_Fopen(filename, mode)			Fopen(filename, mode)
+# define ROM_Fclose(handle)				Fclose(handle)
+# define ROM_Fread(handle, cnt, buf)			Fread(handle, cnt, buf)
+# define ROM_Fwrite(handle, cnt, buf)			Fwrite(handle, cnt, buf)
+# define ROM_Fdelete(fn)				Fdelete(fn)
+# define ROM_Fseek(where, handle, how)			Fseek(where, handle, how)
+# define ROM_Fattrib(fn, rwflag, attr)			Fattrib(fn, rwflag, attr)
+# define ROM_Fsfirst(filespec, attr)			Fsfirst(filespec, attr)
+# define ROM_Fsnext()					Fsnext()
+# define ROM_Frename(zero, old, new)			Frename(zero, old, new)
+# define ROM_Fdatime(timeptr, handle, rwflag)		Fdatime(timeptr, handle, rwflag)
+# define ROM_Flock(handle, mode, start, length)		Flock(handle, mode, start, length)
+# define ROM_Fcntl(f, arg, cmd)				Fcntl(f, arg, cmd)
+
+# define ROM_Bconstat(dev)				Bconstat(dev)
+# define ROM_Bconin(dev)				Bconin(dev)
+# define ROM_Bconout(dev,c)				Bconout(dev,c)
+# define ROM_Setexc(vnum,vptr)				Setexc(vnum,vptr)
+# define ROM_Bcostat(dev)				Bcostat(dev)
+# define ROM_Kbshift(data)				Kbshift(data)
+
+# define ROM_Initmous(type,param,vptr)			Initmous(type,param,vptr)
+# define ROM_Getrez()					Getrez()
+# define ROM_Iorec(ioDEV)				Iorec(ioDEV)
+# define ROM_Rsconf(baud,flow,uc,rs,ts,sc)		Rsconf(baud,flow,uc,rs,ts,sc)
+# define ROM_Keytbl(nrml,shft,caps)			Keytbl(nrml,shft,caps)
+# define ROM_Cursconf(rate,attr)			Cursconf(rate,attr)
+# define ROM_Settime(time)				Settime(time)
+# define ROM_Gettime()					Gettime()
+# define ROM_Offgibit(ormask)				Offgibit(ormask)
+# define ROM_Ongibit(andmask)				Ongibit(andmask)
+# define ROM_Dosound(ptr)				Dosound(ptr)
+# define ROM_Vsync()					Vsync()
+# define ROM_Bconmap(dev)				Bconmap(dev)
+
+# define ROM_VsetScreen(lscrn,pscrn,rez,mode)		VsetScreen(lscrn,pscrn,rez,mode)
+
+# endif /* SYSCALL_REENTRANT */
 
 # endif /* _m68k_syscall_h */
