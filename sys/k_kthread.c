9489e870e6e5bd9e4c04d3fc0da92da0217d737d
Frank Naumann <fnaumann@boerde.de>
2004-04-09 13:17:16 +0000

    Setup FATAL error for kthread SYSCALL context, returning from SYSCALL should never happen for kthreads.

--- sys/k_kthread.c
+++ sys/k_kthread.c
@@ -42,32 +42,41 @@
 # include "proc.h"
 
 
+static void
+kthread_fatal(void *arg)
+{
+	FATAL("restoring SYSCALL context from kernel thread???");
+}
+
 long
-kthread_create (void (*func)(void *), void *arg, struct proc **np, const char *fmt, ...)
+kthread_create(void (*func)(void *), void *arg, struct proc **np, const char *fmt, ...)
 {
 	struct proc *p2;
 	long err;
 
-	DEBUG (("kthread_create: 0x%lx", func));
+	DEBUG(("kthread_create: 0x%lx", func));
 
-	p2 = fork_proc1 (rootproc, FORK_SHAREVM|FORK_SHARECWD|FORK_SHAREFILES|FORK_SHARESIGS, &err);
+	p2 = fork_proc1(rootproc, FORK_SHAREVM|FORK_SHARECWD|FORK_SHAREFILES|FORK_SHARESIGS, &err);
 	if (p2)
 	{
 		va_list args;
 		int i;
 
-		va_start (args, fmt);
-		vsprintf (p2->fname, sizeof (p2->fname), fmt, args);
-		vsprintf (p2->cmdlin, sizeof (p2->cmdlin), fmt, args);
-		vsprintf (p2->name, sizeof (p2->name), fmt, args);
-		va_end (args);
+		va_start(args, fmt);
+		vsprintf(p2->fname, sizeof(p2->fname), fmt, args);
+		vsprintf(p2->cmdlin, sizeof(p2->cmdlin), fmt, args);
+		vsprintf(p2->name, sizeof(p2->name), fmt, args);
+		va_end(args);
 
 		/* kernel threads don't have a basepage */
 		p2->base = NULL;
 
 		/* zero the user registers, and set the FPU in a "clear" state */
 		for (i = 0; i < 15; i++)
+		{
 			p2->ctxt[CURRENT].regs[i] = 0;
+			p2->ctxt[SYSCALL].regs[i] = 0;
+		}
 
 		p2->ctxt[CURRENT].sr = 0x2000;	/* kernel threads work in super mode */
 		p2->ctxt[CURRENT].fstate[0] = 0;
@@ -75,26 +84,26 @@ kthread_create (void (*func)(void *), void *arg, struct proc **np, const char *f
 		p2->ctxt[CURRENT].usp = p2->sysstack;
 		p2->ctxt[CURRENT].term_vec = (long) rts;
 
-		p2->ctxt[SYSCALL].sr = 0x2000;	/* kernel threads work in super mode */
+		p2->ctxt[SYSCALL].sr = 0x2000;
 		p2->ctxt[SYSCALL].fstate[0] = 0;
-		p2->ctxt[SYSCALL].pc = (long)func;
+		p2->ctxt[SYSCALL].pc = (long)kthread_fatal;
 		p2->ctxt[SYSCALL].usp = p2->sysstack;
 		p2->ctxt[SYSCALL].term_vec = (long) rts;
 
 		*((long *)(p2->ctxt[CURRENT].usp + 4)) = (long) arg;
 		*((long *)(p2->ctxt[SYSCALL].usp + 4)) = (long) arg;
 
-		DEBUG (("kthread_create: p2 0x%lx", p2));
+		DEBUG(("kthread_create: p2 0x%lx", p2));
 
 		if (np != NULL)
 			*np = p2;
 
-		run_next (p2, 3);
+		run_next(p2, 3);
 
 		return 0;
 	}
 
-	DEBUG (("kthread_create: failed (%li)", err));
+	DEBUG(("kthread_create: failed (%li)", err));
 	return err;
 }
 
@@ -103,15 +112,15 @@ kthread_create (void (*func)(void *), void *arg, struct proc **np, const char *f
  * current context.
  */
 void
-kthread_exit (short code)
+kthread_exit(short code)
 {
 	struct proc *p = curproc;
 
 	if (code != 0)
-		ALERT ("WARNING: thread `%s' (%d) exits with status %d\n",
+		ALERT("WARNING: thread `%s' (%d) exits with status %d\n",
 			p->name, p->pid, code);
 
-	terminate (p, code, ZOMBIE_Q);
+	terminate(p, code, ZOMBIE_Q);
 
 	/* not reached */
 	for (;;) ;
