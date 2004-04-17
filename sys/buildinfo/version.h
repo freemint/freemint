8363e8ae106bc6762ef70089923afc36475dd153
Frank Naumann <fnaumann@boerde.de>
2004-04-17 18:13:33 +0000

    Optimized version macros, distinguish between cvs version, beta release and stable release. cvs version look for \mint\1-16-cur module and config directory.

--- sys/buildinfo/version.h
+++ sys/buildinfo/version.h
@@ -3,40 +3,77 @@
  * the file Changes.MH for details and dates.
  */
 
-# ifndef _mint_version_h
-# define _mint_version_h
+#ifndef _mint_version_h
+#define _mint_version_h
 
-# define MINT_MAJ_VERSION	1
-# define MINT_MIN_VERSION	16
-# define MINT_PATCH_LEVEL	0
+#define THIRD_PARTY		"FreeMiNT"
 
-# define THIRD_PARTY		"FreeMiNT"
-
-# if 1
-# define MINT_BETA		"-ALPHA"
-# define MINT_BETA_IDENT	'a'
+#ifndef THIRD_PARTY
+# ifdef MULTITOS
+#  define MINT_NAME		"MultiTOS"
 # else
-# define MINT_BETA		""
-# define MINT_BETA_IDENT	0
+#  define MINT_NAME		"MiNT"
 # endif
+#else
+# define MINT_NAME		THIRD_PARTY
+#endif
 
-# define MINT_VERS_STRING	str(MINT_MAJ_VERSION) "." \
+/*
+ * main version macros
+ */
+#define MINT_MAJ_VERSION	1
+#define MINT_MIN_VERSION	16
+#define MINT_PATCH_LEVEL	0
+
+/*
+ * source status
+ * 
+ * NOTE: only one of these defines must be nonzero
+ */
+#define MINT_STATUS_CVS		1
+#define MINT_STATUS_BETA	0
+#define MINT_STATUS_RELEASE	0
+
+#if (MINT_STATUS_CVS + MINT_STATUS_BETA + MINT_STATUS_RELEASE) > 1
+# error only one of the MINT_STATUS_* macros can be 1
+#endif
+
+/*
+ * pretty printing macros
+ */
+
+#if MINT_STATUS_CVS
+# define MINT_STATUS		"-ALPHA"
+# define MINT_STATUS_IDENT	'a'
+#endif
+
+#if MINT_STATUS_BETA
+# define MINT_STATUS		"-BETA"
+# define MINT_STATUS_IDENT	'b'
+#endif
+
+#if MINT_STATUS_RELEASE
+# define MINT_STATUS		"-RELEASE"
+# define MINT_STATUS_IDENT	'r'
+#endif
+
+#define MINT_VERS_STRING	str(MINT_MAJ_VERSION) "." \
 				str(MINT_MIN_VERSION) "." \
 				str(MINT_PATCH_LEVEL) \
-				MINT_BETA
+				MINT_STATUS
+
+#if MINT_STATUS_CVS
+
+/* the cvs version always looks for a -cur dir */
+# define MINT_VERS_PATH_STRING	str(MINT_MAJ_VERSION) "-" \
+				str(MINT_MIN_VERSION) "-" \
+				"cur"
+#else
 
 # define MINT_VERS_PATH_STRING	str(MINT_MAJ_VERSION) "-" \
 				str(MINT_MIN_VERSION) "-" \
 				str(MINT_PATCH_LEVEL)
 
-# ifndef THIRD_PARTY
-#  ifdef MULTITOS
-#   define MINT_NAME		"MultiTOS"
-#  else
-#   define MINT_NAME		"MiNT"
-#  endif
-# else
-#  define MINT_NAME		THIRD_PARTY
-# endif
+#endif
 
-# endif /* _mint_version_h */
+#endif /* _mint_version_h */
