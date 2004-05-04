4ff7d06df53b11fff884f811d7a46622a826f702
Frank Naumann <fnaumann@boerde.de>
2004-05-04 12:03:38 +0000

    Cleanup of the new adi stuff.

--- xaaes/src.km/adi.c
+++ xaaes/src.km/adi.c
@@ -1,10 +1,35 @@
+/*
+ * $Id$
+ *
+ * XaAES - XaAES Ain't the AES (c) 1992 - 1998 C.Graham
+ *                                 1999 - 2003 H.Robbers
+ *                                        2004 F.Naumann & O.Skancke
+ *
+ * A multitasking AES replacement for MiNT
+ *
+ * This file is part of XaAES.
+ *
+ * XaAES is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * XaAES is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with XaAES; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #include "adi.h"
 #include "xa_global.h"
 
-static struct moose_data md;
 
-static struct adif *alladifs = 0;
+static struct moose_data md;
+static struct adif *alladifs = NULL;
 
 long
 adi_register(struct adif *a)
@@ -97,4 +122,3 @@ adi_ioctl(short cmd, long arg)
 {
 	return 0;
 }
-			
