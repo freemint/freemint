4b598920caa22906d27715ff320a1a54a09e761e
Frank Naumann <fnaumann@boerde.de>
2004-05-10 21:34:18 +0000

    More work on the progdef callout as signal handler.

--- xaaes/src.km/xa_user_things.s
+++ xaaes/src.km/xa_user_things.s
@@ -23,9 +23,9 @@
 |* along with XaAES; if not, write to the Free Software
 |* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 |*
-	.globl _xa_user_things
+	.text
 
-	.data
+	.globl _xa_user_things
 
 _xa_user_things:
 	.long xa_user_end	- _xa_user_things	| Size of xa user things
@@ -34,12 +34,10 @@ _xa_user_things:
 	.long _retcode		- _xa_user_things	| return code
 	.long _parmblk		- _xa_user_things	| address of local parmblk
 
-	.text
-
 	| This is all that is needed to test this with existing signal
 	| handling scheme I think...
 progdef_callout:
-	movem.l	d0-d7/a0-a6,-(sp)
+|	movem.l	d0-d7/a0-a6,-(sp)
 
 	lea	_userblk(pc),a2		| address of userblk
 	move.l	(a2),d0			| get address of callout
@@ -48,14 +46,14 @@ progdef_callout:
 	pea	_parmblk(pc)
 	jsr	(a0)
 	addq.w	#4,sp
+nofunc:
 	lea	_retcode(pc),a0
-nofunc:	move.l	d0,(a0)
-	movem.l	(sp)+,d0-d7/a0-a6
+	move.l	d0,(a0)
+|	movem.l	(sp)+,d0-d7/a0-a6
 	rts
 
 _userblk: dc.l 0
 _retcode: dc.l 0
 _parmblk: ds.w 60
-
 
 xa_user_end:
