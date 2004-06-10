474bcd56b95b2aa2ca278c7f3d41f5bad7398e14
Odd Skancke <oskancke@getmail.no>
2004-06-10 06:00:15 +0000

    Fixed objc_edit(ED_END) - it returned 0 as position when no objc_edit(ED_INIT) was called before. AtarIRC seems to use the returnvalue causing the cursor to be wrongly placed at start instead of end. Fixed a problem where the menubar was not redrawn when menu_bar(MENU_INSTALL) was called and the callers menubar was already installed.

--- xaaes/src.km/c_mouse.c
+++ xaaes/src.km/c_mouse.c
@@ -8,7 +8,7 @@
  * A multitasking AES replacement for FreeMiNT
  *
  * This file is part of XaAES.
- *
+d *
  * XaAES is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
