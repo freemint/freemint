21a2587776fb3907ae0b9ce0db46024a514e80e3
fna <fna>
2004-07-04 23:18:12 +0000

    New XaAES loader, useable as INIT program or from command line. New km device for controlling kernel modules. Removed old load.prg. You must update the kernel and XaAES (and the rsc files too). Only use the xaloader.prg from now on.

---
+++ xaaes/src.km/xaloader/xaloader.c
@@ -0,0 +1,185 @@
+
+#include <mint/mintbind.h>
+#include <sys/fcntl.h>
+#include <string.h>
+
+/* KM_RUN */
+#include "../../../sys/mint/ioctl.h"
+
+#define DEFAULT "xaaes.km"
+
+static void
+my_strlcpy(char *dst, const char *src, size_t n)
+{
+	size_t org_n = n;
+
+	if (n && --n)
+	{
+		do {
+			if (!(*dst++ = *src++))
+				break;
+		}
+		while (--n);
+
+	}
+
+	if (org_n && n == 0)
+		*dst = '\0';
+}
+
+static void
+my_strlcat(char *dst, const char *src, size_t n)
+{
+	if (!n)
+		return;
+
+	/* find end */
+	while (n-- && *dst)
+		dst++;
+
+	/* copy over */
+	if (n && --n)
+	{
+		do {
+			if (!(*dst++ = *src++))
+				break;
+		}
+		while (--n);
+
+	}
+
+	/* terminate */
+	if (n == 0)
+		*dst = '\0';
+}
+
+/*
+ * - without an argument try to load xaaes.km from sysdir
+ * - with argument:
+ *   - without a path separator try to load this from sysdir
+ *   - with path separator go to this dir and load the module
+ *     from there
+ */
+
+int loader_init(int argc, char **argv, char **env);
+
+int
+loader_init(int argc, char **argv, char **env)
+{
+	char path[384];
+	long fh, r;
+
+	/*
+	 * first make sure we are on FreeMiNT's '/'
+	 */
+	Dsetdrv('u' - 'a');
+	Dsetpath("/");
+
+	/*
+	 * now lookup FreeMiNT's sysdir
+	 */
+	fh = Fopen("/kern/sysdir", O_RDONLY);
+	if (fh < 0)
+	{
+		Cconws("XaAES loader: Fopen(\"/kern/sysdir\") failed!\r\n");
+		goto error;
+	}
+	r = Fread((int)fh, sizeof(path), path);
+	if (r <= 0)
+	{
+		Cconws("XaAES loader: Fread(\"/kern/sysdir\") failed!\r\n");
+		goto error;
+	}
+	if (r >= sizeof(path))
+	{
+		Cconws("XaAES loader: buffer for Fread(\"/kern/sysdir\") to small!\r\n");
+		goto error;
+	}
+	Fclose((int)fh);
+
+	/* null terminate */
+	path[r] = '\0';
+
+	if (argc > 1)
+	{
+		char *s = argv[1], *name = NULL;
+		char c;
+
+		do {
+			c = *s++;
+			if (c == '\\' || c == '/')
+				name = s - 1;
+		}
+		while (c);
+
+		if (name)
+		{
+			*name++ = '\0';
+			my_strlcpy(path, argv[1], sizeof(path));
+
+			r = Dsetpath(path);
+			if (r)
+			{
+				Cconws("XaAES loader: No such directory: \"");
+				Cconws(path);
+				Cconws("\"\r\n");
+				goto error;
+			}
+
+			my_strlcat(path, "/", sizeof(path));
+		}
+		else
+			name = argv[1];
+
+		my_strlcat(path, name, sizeof(path));
+	}
+	else
+	{
+		r = Dsetpath(path);
+		if (r)
+		{
+			Cconws("XaAES loader: No such directory: \"");
+			Cconws(path);
+			Cconws("\"\r\n");
+			goto error;
+		}
+
+		my_strlcat(path, DEFAULT, sizeof(path));
+	}
+
+	fh = Fopen(path, O_RDONLY);
+	if (fh < 0)
+	{
+		Cconws("XaAES loader: No such file: \"");
+		Cconws(path);
+		Cconws("\"\r\n");
+		goto error;
+	}
+	Fclose((int)fh);
+
+	Cconws("Load kernel module: ");
+	Cconws(path);
+	Cconws("\r\n");
+
+	fh = Fopen("/dev/km", O_RDONLY);
+	if (fh < 0)
+	{
+		Cconws("XaAES loader: no /dev/km, please update your kernel!\r\n");
+		goto error;
+	}
+
+	r = Fcntl((int)fh, path, KM_RUN);
+	if (r)
+	{
+		Cconws("XaAES loader: Fcntl(KM_RUN) failed!\r\n");
+		goto error;
+	}
+
+	Fclose((int)fh);
+	return 0;
+
+error:
+	Cconws("press any key to continue ...\r\n");
+	Cconin();
+	return 1;
+}
