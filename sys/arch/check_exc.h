6115a430565bcd53b58e189b378641a0b45f414a
draco <draco>
2004-07-05 06:37:23 +0000

    Updates, mainly structs and code for future usage.

--- sys/arch/check_exc.h
+++ sys/arch/check_exc.h
@@ -39,8 +39,194 @@
 # ifndef _check_exc_h
 # define _check_exc_h
 
-# include "mint/mint.h"
+struct frame_zero
+{
+	ulong data_reg[8];
+	ulong addr_reg[7];
+	ushort sr;
+	ushort *pc;
+	ushort format_word;
+};
 
-extern short safe_super;
+struct privilege_violation_stackframe
+{
+	ulong data_reg[8];
+	ulong addr_reg[7];
+	ushort sr;
+	ushort *pc;
+	ushort format_word;
+};
+
+/* Every 68k processor uses different stack frame format for
+ * access faults. Very funny...
+ */
+
+/* 68020/68030 Special Status Word */
+struct mc68030_ssw
+{
+	unsigned fc:1;		/* Fault on stage C */
+	unsigned fb:1;		/* Fault on stage B */
+	unsigned rc:1;		/* Rerun (1) stage C */
+	unsigned rb:1;		/* Rerun (1) stage B */
+	unsigned :3;		/* CPU internal bits */
+	unsigned df:1;		/* Data fault, rerun (1) data cycle */
+	unsigned rm:1;		/* Read-modify-write on data cycle */
+	unsigned rw:1;		/* Read/write on data cycle (1 = read) */
+	unsigned size:2;	/* Size */
+	unsigned :1;		/* Internal use */
+	unsigned as:3;		/* Address space for data cycle */
+};
+
+/* 68040 Special Status Word */
+struct mc68040_ssw
+{
+	unsigned cp:1;		/* Continuation of FP post-instruction exception pending */
+	unsigned cu:1;		/* Continuation of unimplemented FP instruction exception pending */
+	unsigned ct:1;		/* Continuation of trace exception pending */
+	unsigned cm:1;		/* Continuation of MOVEM instruction execution pending */
+	unsigned ma:1;		/* Misaligned access */
+	unsigned atc:1;		/* ATC fault */
+	unsigned lk:1;		/* Locked transfer (read-modify-write) */
+	unsigned rw:1;		/* Read/write on data cycle (1 = read) */
+	unsigned :1;		/* Undefined */
+	unsigned size:2;	/* Size */
+	unsigned tt:2;		/* Transfer type */
+	unsigned tm:3;		/* Transfer modifier */
+};
+
+/* 68060 Fault Status Long Word */
+struct mc68060_fslw
+{
+	unsigned :4;
+	unsigned ma:1;
+	unsigned :1;
+	unsigned lk:1;
+	unsigned rw:2;
+	unsigned size:2;
+	unsigned tt:2;
+	unsigned tm:2;
+	unsigned io:1;
+	unsigned pbe:1;
+	unsigned sbe:1;
+	unsigned pta:1;
+	unsigned ptb:1;
+	unsigned il:1;
+	unsigned pf:9;
+	unsigned sp:1;
+	unsigned wp:1;
+	unsigned twe:1;
+	unsigned re:1;
+	unsigned we:1;
+	unsigned ttr:1;
+	unsigned bpe:1;
+	unsigned :1;
+	unsigned see:1;
+};
+
+typedef struct mc68030_bus_frame_short MC68030_SHORT_FRAME;
+typedef struct mc68030_bus_frame_long MC68030_LONG_FRAME;
+typedef struct mc68040_bus_frame MC68040_BUS_FRAME;
+typedef struct mc68060_bus_frame MC68060_BUS_FRAME;
+
+/* 68020/68030 short bus cycle fault stack frame, format $a */
+struct mc68030_bus_frame_short
+{
+	ulong data_reg[8];
+	ulong addr_reg[7];
+	ushort sr;
+	ushort *pc;
+	ushort format_word;
+	ushort int_1;
+	struct mc68030_ssw ssw;
+	ushort stage_c;
+	ushort stage_b;
+	void *fault_address;
+	ushort int_2;
+	ushort int_3;
+	ulong data_output_buffer;
+	ushort int_4;
+	ushort int_5;
+};
+
+/* 68020/68030 long bus cycle fault stack frame, format $b */
+struct mc68030_bus_frame_long
+{
+	ulong data_reg[8];
+	ulong addr_reg[7];
+	ushort sr;
+	ushort *pc;
+	ushort format_word;
+	ushort int_1;
+	struct mc68030_ssw ssw;
+	ushort stage_c;
+	ushort stage_b;
+	void *fault_address;
+	ushort int_2;
+	ushort int_3;
+	ulong data_output_buffer;
+	ushort int_4;
+	ushort int_5;
+	ushort int_6;
+	ushort int_7;
+	ushort *stage_b_address;
+	ushort int_8;
+	ushort int_9;
+	ulong data_input_buffer;
+	ushort int_10;
+	ushort int_11;
+	ushort int_12;
+	ushort version;		/* highest 4 bits is version number */
+				/* 18 words of internal information follow */
+};
+
+/* 68040 access error stack frame */
+struct mc68040_bus_frame
+{
+	ulong data_reg[8];
+	ulong addr_reg[7];
+	ushort sr;
+	ushort *pc;
+	ushort format_word;
+	void *effective_address;
+	struct mc68040_ssw ssw;
+	ushort wb3s;			/* pending write-backs, crap */
+	ushort wb2s;
+	ushort wb1s;
+	void *fault_address;
+	void *wb3a;
+	ulong wb3d;
+	void *wb2a;
+	ulong wb2d;
+	void *wb1a;
+	ulong wb1d;
+	ulong pd1;
+	ulong pd2;
+	ulong pd3;
+};
+
+/* 68060 access error stack frame */
+struct mc68060_bus_frame
+{
+	ulong data_reg[8];
+	ulong addr_reg[7];
+	ushort sr;
+	ushort *pc;
+	ushort format_word;
+	void *fault_address;
+	union
+	{
+		ushort *fault_pc;
+		struct mc68060_fslw;
+	} bottom;
+};
+
+struct access
+{
+	long address, size;
+};
+
+long check_bus(struct frame_zero frame);
+long check_priv(struct privilege_violation_stackframe frame);
+
 
 # endif /* _check_exc_h */
