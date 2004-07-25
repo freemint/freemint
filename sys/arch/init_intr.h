fa2d1c1d1ac58cc81b0b5858b07efdfb42719cf9
draco <draco>
2004-07-25 16:54:51 +0000

    init_intr(), restr_intr() and such, moved to the arch/ folder.

---
+++ sys/arch/init_intr.h
@@ -0,0 +1,21 @@
+/*
+ * $Id$
+ * 
+ * This file has been modified as part of the FreeMiNT project. See
+ * the file Changes.MH for details and dates.
+ */
+
+# ifndef _arch_init_intr_h
+# define _arch_init_intr_h
+
+# include "mint/mint.h"
+# include "mint/emu_tos.h"
+
+extern KBDVEC *syskey;
+
+void	new_xbra_install(long *xv, long addr, long _cdecl (*func)());
+
+void	init_intr	(void);
+void	restr_intr	(void);
+
+# endif /* _arch_init_intr_h */
