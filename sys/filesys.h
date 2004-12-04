8dda699a1e62fcc92f3dc0ef7daf3139c001de6a
Frank Naumann <fnaumann@boerde.de>
2004-12-04 14:57:01 +0000

    Changed path2cookie and relapth2cookie to be independant of curproc.

--- sys/filesys.h
+++ sys/filesys.h
@@ -40,31 +40,32 @@ extern long dosdrvs;
 /*
  * exported functions
  */
-void kill_cache (fcookie *dir, char *name);
-long cache_lookup (fcookie *dir, char *name, fcookie *res);
-void cache_init (void);
-void clobber_cookie (fcookie *fc);
-void init_filesys (void);
-char *xfs_name (fcookie *fc);
-void xfs_add (FILESYS *fs);
-void close_filesys (void);
-long _s_ync (void);
-long _cdecl sys_s_ync (void);
-long _cdecl sys_fsync (short fh);
-void _changedrv (ushort drv, const char *function);
+void kill_cache(fcookie *dir, char *name);
+long cache_lookup(fcookie *dir, char *name, fcookie *res);
+void cache_init(void);
+void clobber_cookie(fcookie *fc);
+void init_filesys(void);
+char *xfs_name(fcookie *fc);
+void xfs_add(FILESYS *fs);
+void close_filesys(void);
+long _s_ync(void);
+long _cdecl sys_s_ync(void);
+long _cdecl sys_fsync(short fh);
+void _changedrv(ushort drv, const char *function);
 #define changedrv(drv) _changedrv(drv, FUNCTION);
-long disk_changed (ushort drv);
-long relpath2cookie (fcookie *dir, const char *path, char *lastnm, fcookie *res, int depth);
-long path2cookie (const char *path, char *lastname, fcookie *res);
-void release_cookie (fcookie *fc);
-void dup_cookie (fcookie *new, fcookie *old);
-int _cdecl denyshare (FILEPTR *list, FILEPTR *newfileptr);
-int denyaccess (struct ucred *cred, XATTR *, ushort);
-LOCK *denylock (ushort pid, LOCK *list, LOCK *newlock);
-long dir_access (struct ucred *cred, fcookie *, ushort, ushort *);
-int has_wild (const char *name);
-void copy8_3 (char *dest, const char *src);
-int pat_match (const char *name, const char *template);
-int samefile (fcookie *, fcookie *);
+long disk_changed(ushort drv);
+long relpath2cookie(struct proc *p, fcookie *dir, const char *path, char *lastnm,
+		    fcookie *res, int depth);
+long path2cookie(struct proc *p, const char *path, char *lastname, fcookie *res);
+void release_cookie(fcookie *fc);
+void dup_cookie(fcookie *new, fcookie *old);
+int _cdecl denyshare(FILEPTR *list, FILEPTR *newfileptr);
+int denyaccess(struct ucred *cred, XATTR *, ushort);
+LOCK *denylock(ushort pid, LOCK *list, LOCK *newlock);
+long dir_access(struct ucred *cred, fcookie *, ushort, ushort *);
+int has_wild(const char *name);
+void copy8_3(char *dest, const char *src);
+int pat_match(const char *name, const char *template);
+int samefile(fcookie *, fcookie *);
 
 # endif /* _filesys_h */
