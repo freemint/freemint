260a918a1614f94d4dc5fc87bc6d5a42411cfbaf
Odd Skancke <oskancke@getmail.no>
2005-04-03 13:29:36 +0000

    Moved mouse wheel event handling to correct context, now scroll-lists react to mouse wheel events transparently to the application. Test fileselector and fontselector

--- xaaes/src.km/c_mouse.c
+++ xaaes/src.km/c_mouse.c
@@ -46,6 +46,7 @@
 #include "semaphores.h"
 #include "taskman.h"
 #include "widgets.h"
+#include "obtree.h"
 
 #include "mint/dcntl.h"
 #include "mint/fcntl.h"
@@ -368,5 +369,246 @@ cXA_widget_click(enum locks lock, struct c_event *ce, bool cancel)
 	{
 		DIAG((D_mouse, ce->client, "cXA_widget_click for %s", ce->client->name));
 		widg->click(lock, root_window, widg, &ce->md);
+	}
+}
+
+
+static void
+wheel_arrow(struct xa_window *wind, const struct moose_data *md, XA_WIDGET **wr, short *r, short *r_amnt)
+{
+	XA_WIDGETS which;
+	XA_WIDGET  *widg;
+	short orient;
+	int fac = wind->owner->options.wheel_page;
+	bool rev = wind->owner->options.wheel_reverse ? true : false;
+
+	if (wr)
+		*wr = NULL;
+	if (r)
+		*r = -1;
+
+	if (md->state == cfg.ver_wheel_id)
+	{
+		if (r_amnt)
+			*r_amnt = cfg.ver_wheel_amount;
+
+		if (md->clicks < 0)
+		{
+			which = rev ? XAW_DNLN : XAW_UPLN;
+			orient = rev ? 1 : 0;
+		}
+		else
+		{
+			which = rev ? XAW_UPLN : XAW_DNLN;
+			orient = rev ? 0 : 1;
+		}
+	}
+	else if (md->state == cfg.hor_wheel_id)
+	{
+		if (r_amnt)
+			*r_amnt = cfg.hor_wheel_amount;
+
+		if (md->clicks < 0)
+		{
+			which = rev ? XAW_RTLN : XAW_LFLN;
+			orient = rev ? 3 : 2;
+		}
+		else
+		{
+			which = rev ? XAW_LFLN : XAW_RTLN;
+			orient = rev ? 2 : 3;
+		}
+	}
+	else
+		return;
+
+	if (r)
+		*r = orient;
+
+	if (wind)
+	{
+		if (fac && abs(md->clicks) > abs(fac))
+		{
+			switch (which)
+			{
+				case XAW_UPLN: which = XAW_UPPAGE; break;
+				case XAW_DNLN: which = XAW_DNPAGE; break;
+				case XAW_LFLN: which = XAW_LFPAGE; break;
+				case XAW_RTLN: which = XAW_RTPAGE; break;
+				default: /* make gcc happy */ break;
+			}
+		}
+
+		widg = get_widget(wind, which);
+		if (widg)
+		{
+			if (widg->type && wr)
+				*wr = widg;
+		}
+	}
+}
+/* If md pointer is NULL, send a standard WM_ARROWED (application dont support wheel)
+ * else send extended WM_ARROWED message
+ */
+static void
+whlarrowed(struct xa_window *wind, short WA, short amount, const struct moose_data *md)
+{
+	if (md)
+	{
+		wind->send_message(0, wind, NULL, AMQ_NORM, QMF_CHKDUP,
+				   WM_ARROWED, 0,0, wind->handle,
+				   (amount << 8)|(WA & 7), md->x, md->y, md->kstate);
+	}
+	else
+	{
+		while (amount)
+		{
+			wind->send_message(0, wind, NULL, AMQ_NORM, 0,
+					   WM_ARROWED, 0,0, wind->handle,
+					   WA, 0, 0, 0);
+			amount--;
+		}
+	}
+}
+
+void
+cXA_wheel_event(enum locks lock, struct c_event *ce, bool cancel)
+{
+	struct xa_client *client = ce->client;
+	struct xa_window *wind = ce->ptr1;
+	struct moose_data *md = &ce->md;
+	XA_WIDGET *widg;
+	
+	if (!cancel)
+	{
+		if (client->waiting_for & MU_WHEEL)
+		{
+			struct moose_data *nmd = client->wheel_md;
+			short amount = 0;
+
+			if (!nmd)
+				nmd = kmalloc(sizeof(*nmd));
+			else
+				amount = nmd->clicks;
+			if (nmd)
+			{
+				*nmd = *md;
+				nmd->clicks += amount;
+				client->wheel_md = nmd;
+			}
+		}
+		else if (wind)
+		{
+			bool slist = false;
+			short orient, amount, WA = WA_UPPAGE;
+			
+			wheel_arrow(wind, md, &widg, &orient, &amount);
+			
+			if ((md->kstate & K_ALT))
+				orient ^= 2;
+
+			if (orient & 2)
+				WA = WA_LFPAGE;
+			
+			if (!(md->kstate & (K_RSHIFT|K_LSHIFT)))
+				WA += WA_UPLINE;
+			else
+				amount = 1;	/* No more than one when paging */
+
+			WA += orient & 1;
+
+			if (is_topped(wind))
+			{
+				XA_TREE *wt = get_widget(wind, XAW_TOOLBAR)->stuff;
+				OBJECT *obtree;
+				short obj;
+				
+				if (wt && (obtree = wt->tree))
+				{
+					//display("got toolbar - wt %lx", wt);
+					//display("got object tree %lx", obtree);
+					obj = obj_find(wt, 0, 10, md->x, md->y, NULL);
+					//display("found obj %d, type = %d", obj, obtree[obj].ob_type);
+					if (obj > 0 && (obtree[obj].ob_type & 0xff) == G_SLIST)
+					{
+						struct scroll_info *list = object_get_slist(obtree + obj);
+						
+						//display("object is slist");
+						amount *= (md->clicks < 0 ? -md->clicks : md->clicks);
+						whlarrowed(list->wi, WA, amount, NULL/*md*/);
+						slist = true;
+					}
+				}
+			}
+			 
+			if (!slist && wind->opts & XAWO_WHEEL)
+			{
+				switch (wind->wheel_mode)
+				{
+					case WHL_REALWHEEL:
+					{
+						wind->send_message(lock, wind, NULL, AMQ_NORM, QMF_CHKDUP,
+								   WM_WHEEL, 0,0, wind->handle,
+								   md->x, md->y,
+								   md->kstate,
+								   ((md->state && 0xf) << 12)|((orient & 0xf) << 8)|(md->clicks & 0xff));
+						break;
+					}
+					case WHL_AROWWHEEL:
+					{
+						amount *= (md->clicks < 0 ? -md->clicks : md->clicks);
+						whlarrowed(wind, WA, amount, md);
+						break;
+					}
+					case WHL_SLDRWHEEL:
+					{
+						short w = -1, s = 0;
+
+						if (orient & 2)
+						{
+							if (wind->active_widgets & HSLIDE)
+								w = XAW_HSLIDE, s = WM_HSLID;
+						}
+						else if (wind->active_widgets & VSLIDE)
+							w = XAW_VSLIDE, s = WM_VSLID;
+
+						if (w == -1)
+							whlarrowed(wind, WA, 1, NULL);
+
+						widg = get_widget(wind, w);
+
+						if (widg)
+						{
+							XA_SLIDER_WIDGET *sl = widg->stuff;
+							short unit, wh;
+
+							if (s == WM_VSLID)
+								wh = widg->r.h - sl->r.h;
+							else
+								wh = widg->r.w - sl->r.w;
+					
+							unit = pix_to_sl(2L, wh) - pix_to_sl(1L, wh);
+
+							if (md->clicks < 0)
+								sl->rpos = bound_sl(sl->rpos - (unit * -md->clicks));
+							else
+								sl->rpos = bound_sl(sl->rpos + (unit * md->clicks));
+					
+							{
+								wind->send_message(lock, wind, NULL, AMQ_NORM, QMF_CHKDUP,
+										   s, 0,0, wind->handle,
+										   sl->rpos, 0,0,0);
+							}
+						}
+						break;
+					}
+				}
+			}
+			else if (!slist)
+			{
+				amount *= (md->clicks < 0 ? -md->clicks : md->clicks);
+				whlarrowed(wind, WA, amount, NULL);
+			}
+		}
 	}
 }
