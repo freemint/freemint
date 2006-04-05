b56374c9001e292a36c33671039c178cc37cf18f
Odd Skancke <oskancke@getmail.no>
2006-04-05 19:49:27 +0000

    raised version to 0.998 for new alpha release

--- xaaes/src.km/version.h
+++ xaaes/src.km/version.h
@@ -31,13 +31,13 @@
 
 /* Version numbers */
 #define VER_MAJOR	0
-#define VER_MINOR	997
+#define VER_MINOR	998
 #define ARCH_TARGET	AES_ARCH_M68K
 /*#define DEV_STATUS	(AES_DEVSTATUS_ALPHA|AES_FDEVSTATUS_STABLE)*/
 #define DEV_STATUS	AES_DEVSTATUS_ALPHA
 
 /* This is only used in wind_get(WF_XAAES) which will be removed */
-#define HEX_VERSION	0x0997
+#define HEX_VERSION	0x0998
 
 #define SHORT_NAME		"XaAES"
 #define AES_ID		"   " SHORT_NAME
