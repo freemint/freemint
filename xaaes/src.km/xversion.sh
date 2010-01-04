c5082b268f3ac9858321792005f95bb2ba876508
Alan Hourihane <alanh@fairlite.co.uk>
2010-01-04 23:13:27 +0000

    Add version script

---
+++ xaaes/src.km/xversion.sh
@@ -0,0 +1,16 @@
+#find newest source-file and generate xversion.h with BDATETIME accordingly
+N=xversion.h
+for i in *.c *.h
+do
+	if [ $i -nt $N ]
+	then
+		N=$i
+	fi
+done
+#echo $N
+if [ $N -nt xversion.h ]
+then
+	D=`date -r $N "+%b %e %Y %H:%M"`
+	echo \#define BDATETIME \"$D\" >xversion.h
+fi
+
