a92bc8a691818e8371a8675f73296bdd0fa65eb8
Helmut Karlowski <helmut.karlowski@ish.de>
2010-02-11 17:26:59 +0000

    Use blue instead of cyan for directories (better contrast).

--- xaaes/src.km/xa_fsel.c
+++ xaaes/src.km/xa_fsel.c
@@ -149,7 +149,7 @@ static struct xa_wtxt_inf dir_txt =
 {
  WTXT_NOCLIP,
 /* id  pnts  flags wrm, 		efx 	fgc 		 bgc	 banner x_3dact y_3dact texture */
- {	-1,  -1, 0, 	MD_TRANS, 0, G_LCYAN, G_WHITE, G_WHITE, 0,			0,		 NULL },/* Normal */
+ {	-1,  -1, 0, 	MD_TRANS, 0, G_BLUE, G_WHITE, G_WHITE, 0,			0,		 NULL },/* Normal */
  {	-1,  -1, 0, 	MD_TRANS, 0, G_CYAN,	G_WHITE, G_WHITE, 0,			0,		 NULL },	/* Selected */
  {	-1,  -1, 0, 	MD_TRANS, 0, G_BLACK, G_WHITE, G_WHITE, 0,			0,		 NULL },	/* Highlighted */
 
