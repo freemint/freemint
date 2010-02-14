1afea13c10247d119d6aa1b25eba4ca05373b6a9
Helmut Karlowski <helmut.karlowski@ish.de>
2010-02-14 22:42:15 +0000

    revert previous "optimisation"

--- xaaes/src.km/xversion.sh
+++ xaaes/src.km/xversion.sh
@@ -1,16 +1,16 @@
 #find newest source-file and generate xversion.h with BDATETIME accordingly
-#N=xversion.h
-#for i in *.c *.h
-#do
-#	if [ $i -nt $N ]
-#	then
-#		N=$i
-#	fi
-#done
-#echo $N
+N=xversion.h
+for i in *.c *.h
+do
+	if [ $i -nt $N ]
+	then
+		N=$i
+	fi
+done
+echo $N
 
 #set BDATETIME to date of .
-N=.
+#N=.
 if [ $N -nt xversion.h ]
 then
 	D=`date -r $N "+%b %e %Y %H:%M"`
