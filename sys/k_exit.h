f5aa35306ab8bf22562de5a266af6889d3c54526
Helmut Karlowski <helmut.karlowski@ish.de>
2010-04-09 23:35:59 +0000

    fix compile for gcc2

--- sys/k_exit.h
+++ sys/k_exit.h
@@ -1,31 +1,31 @@
 /*
  * $Id$
- * 
+ *
  * This file belongs to FreeMiNT. It's not in the original MiNT 1.12
  * distribution. See the file CHANGES for a detailed log of changes.
- * 
- * 
+ *
+ *
  * Copyright 2000 Frank Naumann <fnaumann@freemint.de>
  * All rights reserved.
- * 
+ *
  * Please send suggestions, patches or bug reports to me or
  * the MiNT mailing list.
- * 
- * 
+ *
+ *
  * This file is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This file is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- * 
+ *
  */
 
 # ifndef _k_exit_h
@@ -35,7 +35,7 @@
 # include "mint/proc.h"
 
 
-long 		terminate	(struct proc *curproc, short code, short que);
+long 		terminate	(struct proc *cproc, short code, short que);
 long _cdecl	sys_pterm	(short code);
 long		kernel_pterm	(struct proc *p, short code);
 long _cdecl	sys_pterm0	(void);
