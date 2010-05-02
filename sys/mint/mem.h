58b11fb4a04648abc3066b7242c45d0f98bca6f8
Helmut Karlowski <helmut.karlowski@ish.de>
2010-05-02 10:32:15 +0000

    defines for modeflags now in proc.h

--- sys/mint/mem.h
+++ sys/mint/mem.h
@@ -113,11 +113,6 @@ typedef struct fileheader FILEHEAD;
 # define F_SINGLE_TASK	0x0010000		/* XaAES: if set (in p_flags) it's "single-task-mode" */
 # define F_DONT_STOP	0x0020000		/* XaAES: if set do not stop when entering single-task-mode */
 
-/* _memflags */
-# define M_SINGLE_TASK	0x0001		/* XaAES: if set (in _memflags) it's "single-task-mode" */
-# define M_DONT_STOP	0x00002		/* XaAES: if set do not stop when entering single-task-mode */
-/*/ */
-
 /* flags for curproc->memflags (that is, PRGFLAGS) and also Mxalloc mode.  */
 /* (Actually, when users call Mxalloc, they add 0x10 to what you see here) */
 # define F_PROTMODE	0xf0		/* protection mode bits */
