b5138a6ea923cbd92e2dbbc9d18f3522747061b5
Helmut Karlowski <helmut.karlowski@ish.de>
2010-05-17 18:48:15 +0000

    xversion.sh Makefile:
    use ls to find latest source avoiding -nt to be independant
    of the installed shell.

--- xaaes/src.km/xversion.sh
+++ xaaes/src.km/xversion.sh
@@ -1,24 +1,21 @@
-#find newest source-file and generate xversion.h with BDATETIME accordingly
-HFILE=xversion.h
-N=$HFILE
-for i in *.c *.h
-do
-	if [ $i -nt $N ]
-	then
-		N=$i
-		if [ -f $HFILE ]
-		then
-			break
-		fi
-	fi
-done
-echo $i
+#find newest source-file in $2-$N and generate $1 with BDATETIME accordingly
 
-#set BDATETIME to date of .
-#N=.
-if [ $N -nt $HFILE ]
+if [ $# -le 1 ]
 then
-	D=`date -r $N "+%b %e %Y %H:%M"`
-	echo \#define BDATETIME \"$D\" >$HFILE
+	echo "usage: $0 target file ..."
+	exit 1
 fi
 
+TARGET=$1
+shift
+
+if [ $# -gt 1 ]
+then
+	FILES=`ls -t $*`
+	set $FILES
+fi
+
+echo making for: $1
+
+D=`date -r $1 "+%b %e %Y %H:%M"`
+echo \#define BDATETIME \"$D\" >$TARGET
