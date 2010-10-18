c61a28f710c01ac3c84825c4d3c82bb8eafd40f3
Helmut Karlowski <helmut.karlowski@ish.de>
2010-10-18 10:14:22 +0000

    k_mouse.c adi/moose.h:
    use a union for moose_data.iclcks.

--- xaaes/src.km/k_mouse.c
+++ xaaes/src.km/k_mouse.c
@@ -1118,7 +1118,7 @@ button_timeout(struct proc *p, long arg)
 			get_md(&md);
 			DIAGA(("adi_button_event: type=%d, (%d/%d - %d/%d) state=%d, cstate=%d, clks=%d, l_clks=%d, r_clks=%d (%ld)",
 				md.ty, md.x, md.y, md.sx, md.sy, md.state, md.cstate, md.clicks,
-				md.iclicks[0], md.iclicks[1], sizeof(struct moose_data) ));
+				md.iclicks.chars[0], md.iclicks.chars[1], sizeof(struct moose_data) ));
 #if 0
 			display("adi_button_event: type=%d, (%d/%d - %d/%d) state=%d, cstate=%d, clks=%d, l_clks=%d, r_clks=%d (%ld)",
 				md.ty, md.x, md.y, md.sx, md.sy, md.state, md.cstate, md.clicks,
