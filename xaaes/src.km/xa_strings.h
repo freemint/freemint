fce4a154d244704d372bcbd64a35e399d2aab571
Helmut Karlowski <helmut.karlowski@ish.de>
2011-01-07 17:45:11 +0000

    new files for language-support

---
+++ xaaes/src.km/xa_strings.h
@@ -0,0 +1,26 @@
+/*
+ * Output-strings (to be moved into the rsc-file)
+ */
+
+ /* window-titles */
+#define RS_ABOUT  0
+#define RS_TM     1
+#define RS_SYS    2
+#define RS_APPLST 3
+/* system-window*/
+#define RS_ALERTS 4
+#define RS_ENV   5
+#define RS_SYSTEM 6
+
+/* alert-text */
+#define ASK_QUITALL_ALERT   7
+#define ASK_SHUTDOWN_ALERT  8
+#define SNAP_ERR1           9
+#define SNAP_ERR2          10
+#define SNAP_ERR3          11
+#define SDALERT            12
+#define RS_LAUNCH          13
+
+
+extern char *xa_strings[];
+
