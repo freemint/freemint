039bff6ee5a0a98e780032c759402869e430161e
Helmut Karlowski <helmut.karlowski@ish.de>
2011-01-09 18:09:40 +0000

    util.c util.h:
    implement xa_rewind()

--- xaaes/src.km/util.h
+++ xaaes/src.km/util.h
@@ -41,6 +41,7 @@ typedef struct xa_file XA_FILE;
 
 XA_FILE *xa_fopen( char *fn, int rwmd );
 void xa_fclose( XA_FILE *fp );
+long xa_rewind( XA_FILE *fp );
 int xa_writeline( char *buf, long l, XA_FILE *fp );
 char *xa_readline( char *buf, long size, XA_FILE *fp );
 #if 0
