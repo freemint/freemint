a3a79185855ca69bca892d089827ccff96112972
Helmut Karlowski <helmut.karlowski@ish.de>
2011-01-12 22:22:57 +0000

    xa_strings.c xa_strings.h: add some more strings for translation

--- xaaes/src.km/xa_strings.h
+++ xaaes/src.km/xa_strings.h
@@ -11,18 +11,22 @@
 #define RS_ALERTS 4
 #define RS_ENV   5
 #define RS_SYSTEM 6
+#define RS_VIDEO 7
+#define RS_MEMPROT 8
+#define RS_ON 9
+#define RS_OFF 10
 
 /* alert-text */
-#define ASK_QUITALL_ALERT   7
-#define ASK_SHUTDOWN_ALERT  8
-#define SNAP_ERR1           9
-#define SNAP_ERR2          10
-#define SNAP_ERR3          11
-#define SDALERT            12
-#define RS_LAUNCH          13
-#define XA_HELP_FILE       14
-#define AL_SDMASTER        15
-#define AL_KBD             16
+#define ASK_QUITALL_ALERT   11
+#define ASK_SHUTDOWN_ALERT  12
+#define SNAP_ERR1           13
+#define SNAP_ERR2          14
+#define SNAP_ERR3          15
+#define SDALERT            16
+#define RS_LAUNCH          17
+#define XA_HELP_FILE       18
+#define AL_SDMASTER        19
+#define AL_KBD             20
 
 
 
