b20c78ad8ce231a9f110b6c13599dcb3c9fefdb7
Helmut Karlowski <helmut.karlowski@ish.de>
2011-04-30 06:53:00 +0000

    - count mouse-show/hides - always use real functions - new function tellm to return number of mouse show/hides

--- xaaes/src.km/mscall.c
+++ xaaes/src.km/mscall.c
@@ -3,8 +3,9 @@
  */
 #include "xa_global.h"
 
-#if !AGGRESSIVE_INLINING
-void hidem(void){  v_hide_c(C.P_handle);	}
-void showm(void){ v_show_c(C.P_handle, 1);	}
-void forcem(void){ v_show_c(C.P_handle, 0);	}
-#endif
+static int MsCnt = 0;
+void hidem(void){  v_hide_c(C.P_handle);MsCnt--;	}
+void showm(void){ v_show_c(C.P_handle, 1);MsCnt++;	}
+void forcem(void){ v_show_c(C.P_handle, 0);MsCnt=1;	}
+int tellm(void){return MsCnt;}
+
