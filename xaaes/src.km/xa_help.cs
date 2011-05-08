580331f40ae4e392896142af07138771fae68c92
Helmut Karlowski <helmut.karlowski@ish.de>
2011-05-08 09:38:09 +0000

    xa_help.txt xa_help.de xa_help.cs xa_help.es xa_help.fr xa_help.hu xa_help.ru: - break and sort lines - add new features

--- xaaes/src.km/xa_help.cs
+++ xaaes/src.km/xa_help.cs
@@ -1,33 +1,41 @@
 # Umístìte tento soubor do startovacího adresáøe xaaes (C:\mint\1-*-*\xaaes)
 Ctrl-Alt-zkratky:
 
-A             Ukonèit v¹echny programy (seznam výjimek v xaaes.cnf)
+A             Ukonèit v¹echny programy
+              (seznam výjimek v xaaes.cnf)
 B             Otevøít Systémové okno a protokol varování
-D             Otevøít dialogové okno snímku obrazovky (vy¾aduje XAAESNAP.PRG)
+D             Otevøít dialogové okno snímku obrazovky
+              (vy¾aduje XAAESNAP.PRG)
 E             Otevøít kontextovou nabídku okna
 F,L           Otevøít správce úloh
 HELP          Otevøít okno O...
-H             Zastavit systém (nebo spustit $SDMASTER, je-li nastaven)
+H             Zastavit systém (nebo spustit $SDMASTER,
+              je-li nastaven)
 I             (Od-)ikonifikovat souèasné okno
 K             Spustit program
 T,HOME        Pøekreslit obrazovku
-P             Obnovit paletu barev v hloubce 8 bitù nebo ménì
-Q,G           Ukonèit XaAES (nebo spustit $SDMASTER, je-li nastaven)
+P             Obnovit paletu barev v hloubce 8 bitù
+              nebo ménì
+Q,G           Ukonèit XaAES (nebo spustit $SDMASTER,
+              je-li nastaven)
 R             Pokusit se obnovit systém po zaseknutí
 V             Zviditelnit v¹echny programy
 W             Globální pøepínání oken
 X             Skrýt v¹e mimo momentálnì aktivní aplikace
 Y             Skrýt momentálnì aktivní aplikaci
 MEZERA        Otevøít hlavní li¹tu nabídky
-SHIFT+MEZERA  Otevøít nabídku souèasného okna, pokud ji má, jinak hlavní nabídku
+SHIFT+MEZERA  Otevøít nabídku souèasného okna, pokud
+              ji má, jinak hlavní nabídku
 TAB           Pøepínat otevøené aplikace
 ©IPKA VPRAVO  Maximalizovat souèasné okno
----------------------------------------------------------------------------------
+-------------------------------------------------------
 
 Zkratky/pohyb v XaAES oknì pro výbìr souborù:
 
-CTRL+O        Otevøít nabídku okna (pohyb pomocí kurzorových kláves a RETURN)
-CTRL+C        Zkopírovat cestu a jméno oznaèeného souboru (pokud je) do schránky
+CTRL+O        Otevøít nabídku okna (pohyb pomocí
+              kurzorových kláves a RETURN)
+CTRL+C        Zkopírovat cestu a jméno oznaèeného
+              souboru (pokud je) do schránky
 
 * Pole seznamu vybráno:
 
@@ -44,7 +52,9 @@ CTRL+C        Zkopírovat cestu a jméno oznaèeného souboru (pokud je) do schránky
   ©IPKA VLEVO               Rolovat doleva
   ©IPKA NAHORU/DOLÙ         Rolovat nahoru/dolù
 
-Pokud je vybrána polo¾ka v poli seznamu, ©IPKA VPRAVO aktivuje editaèní pole,
-tak¾e výbìr mù¾e být upraven.
----------------------------------------------------------------------------------
-Dal¹í dokumentace: http://wiki.sparemint.org/index.php/XaAES
+Pokud je vybrána polo¾ka v poli seznamu, ©IPKA VPRAVO
+aktivuje editaèní pole, tak¾e výbìr mù¾e být upraven.
+-------------------------------------------------------
+Dal¹í dokumentace:
+
+http://wiki.sparemint.org/index.php/XaAES
