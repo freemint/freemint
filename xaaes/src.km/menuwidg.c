63e4b69407304d184bdd7d94edc6a003306ae5a7
hek <hek>
2011-07-02 08:15:03 +0000

    use widget-rect to find menu-object (was tab->bar)

--- xaaes/src.km/menuwidg.c
+++ xaaes/src.km/menuwidg.c
@@ -1578,11 +1578,12 @@ menu_bar(struct task_administration_block *tab, short item)
 	MENU_TASK *k = &tab->task_data.menu;
 	short title, dx, dy;
 
+
 	dx = k->m.wt->dx;
 	dy = k->m.wt->dy;
 	k->m.wt->dx = k->m.wt->dy = 0;
 	if (item == -1)
-		title = find_menu_object(k->m.wt, k->m.titles, k->rdx, k->rdy, k->x, k->y, &k->bar);
+		title = find_menu_object(k->m.wt, k->m.titles, k->rdx, k->rdy, k->x, k->y, &tab->widg->r);	//&k->bar);
 	else if (item == -2)
 		title = -1;
 	else
