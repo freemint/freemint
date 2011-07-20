2d456a2901e8b1f73b441e5111d591f2935d62cb
Helmut Karlowski <helmut.karlowski@ish.de>
2011-07-20 21:43:22 +0000

    xa_rsrc.c draw_obj.c handler.c: reduce diffs

--- xaaes/src.km/handler.c
+++ xaaes/src.km/handler.c
@@ -624,7 +624,7 @@ XA_handler(void *_pb)
 		}
 #endif
 
-/*	if( client )
+		/*if( client )
 			BLOG((0, "%s[%d] made by %s",	aes_tab[cmd].descr, cmd, client->name));
 		else
 			BLOG((0, "%s[%d] made",	aes_tab[cmd].descr, cmd));
