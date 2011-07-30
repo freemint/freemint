3749ce5a3006a151a980346f58f8cf63c5ee7cac
Helmut Karlowski <helmut.karlowski@ish.de>
2011-07-30 16:22:05 +0000

    xa_help.*: update help-files

--- xaaes/src.km/xa_help.cs
+++ xaaes/src.km/xa_help.cs
@@ -1,6 +1,8 @@
 # Umístìte tento soubor do startovacího adresáøe xaaes (C:\mint\1-*-*\xaaes)
-Ctrl-Alt-zkratky:
+<b>Ctrl-Alt-zkratky:</b>
 
+0
+#$1 [keyboard-switch]
 A             Ukonèit v¹echny programy
               (seznam výjimek v xaaes.cnf)
 B             Otevøít Systémové okno a protokol varování
@@ -12,13 +14,16 @@ HELP          Otevøít okno O...
 H             Zastavit systém (nebo spustit $SDMASTER,
               je-li nastaven)
 I             (Od-)ikonifikovat souèasné okno
+J
 K             Spustit program
-T,HOME        Pøekreslit obrazovku
+M
 P             Obnovit paletu barev v hloubce 8 bitù
               nebo ménì
 Q,G           Ukonèit XaAES (nebo spustit $SDMASTER,
               je-li nastaven)
 R             Pokusit se obnovit systém po zaseknutí
+T,HOME        Pøekreslit obrazovku
+U
 V             Zviditelnit v¹echny programy
 W             Globální pøepínání oken
 X             Skrýt v¹e mimo momentálnì aktivní aplikace
@@ -28,25 +33,34 @@ SHIFT+MEZERA  Otevøít nabídku souèasného okna, pokud
               ji má, jinak hlavní nabídku
 TAB           Pøepínat otevøené aplikace
 ©IPKA VPRAVO  Maximalizovat souèasné okno
--------------------------------------------------------
+<u>                                                      </u>
 
-Zkratky/pohyb v XaAES oknì pro výbìr souborù:
+Zkratky/pohyb v <b>XaAES oknì pro výbìr souborù:</b>
+
+CTRL+B        Adresáøe v reálném èase
+CTRL+T        Stromový pohled
+CTRL+N        Seøadit podle jména
+CTRL+D        Seøadit podle data
+CTRL+S        Seøadit podle velikosti
+CTRL+E        Seøadit podle typu
+CTRL+X        Nesetøídìnì
+CTRL+SPACE
 
 CTRL+O        Otevøít nabídku okna (pohyb pomocí
               kurzorových kláves a RETURN)
 CTRL+C        Zkopírovat cestu a jméno oznaèeného
               souboru (pokud je) do schránky
 
-* Pole seznamu vybráno:
+<u>Pole seznamu vybráno:</u>
 
   ©IPKA VPRAVO              Rozbalit strom
-  ©IPKA VLEVO               Sbalit strom 
+  ©IPKA VLEVO               Sbalit strom
   ©IPKA NAHORU/DOLÙ         Pohyb kurzoru
   PageUp/Down(SHIFT+¹ipky)  Stránkování seznamu souborù
   CLEAR/HOME                Skok na zaèátek seznamu
   SHIFT+CLEAR/HOME          Skok na konec seznamu
 
-* Pole seznamu nevybráno:
+<u>Pole seznamu nevybráno:</u>
 
   ©IPKA VPRAVO              Rolovat doprava
   ©IPKA VLEVO               Rolovat doleva
@@ -54,7 +68,8 @@ CTRL+C        Zkopírovat cestu a jméno oznaèeného
 
 Pokud je vybrána polo¾ka v poli seznamu, ©IPKA VPRAVO
 aktivuje editaèní pole, tak¾e výbìr mù¾e být upraven.
--------------------------------------------------------
+<u>                                                      </u>
+
 Dal¹í dokumentace:
 
 http://wiki.sparemint.org/index.php/XaAES
