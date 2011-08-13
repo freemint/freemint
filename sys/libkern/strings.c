c77e2fd5c4d5e09e53a148f61e09ad673304d62a
Helmut Karlowski <helmut.karlowski@ish.de>
2011-08-13 18:11:05 +0000

    libkern/kernel_module.h libkern/strings.[ch]: export strings from inlined function in kernel_module.h into strings.c

---
+++ sys/libkern/strings.c
@@ -0,0 +1,15 @@
+# include "buildinfo/version.h"
+
+# ifndef str
+# define str(x)		_stringify(x)
+# define _stringify(x)	#x
+# endif
+
+char *kstrings[] = {
+	"Wrong FreeMiNT version!\r\n",
+	"This module is compiled against " MINT_NAME,
+	MINT_VERS_STRING,
+	"\r\n",
+	"Wrong kentry-version (contact your local dealer)!\r\n",
+};
+
---
+++ sys/libkern/strings.h
@@ -0,0 +1,10 @@
+extern char *kstrings[];
+
+enum{
+	Wrong_FreeMiNT = 0,
+	This_module_is_compiled_against,
+	MINT_VERS,
+	KSTR_NL,
+	Wrong_kentry
+};
+
