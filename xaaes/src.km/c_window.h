8820c6feba5fbc034fdf1ca456639c9d2c7cafb3
Helmut Karlowski <helmut.karlowski@ish.de>
2011-09-04 10:08:37 +0000

    c_window.h xa_global.h: implement background-image

--- xaaes/src.km/c_window.h
+++ xaaes/src.km/c_window.h
@@ -56,6 +56,7 @@ void change_window_attribs(enum locks lock,
 			   short noleft,
 			   RECT r, RECT *remember);
 
+void make_window_full_screen(enum locks lock);
 //void wi_remove(struct win_base *b, struct xa_window *w, bool chkfocus);
 //void wi_put_first(struct win_base *b, struct xa_window *w);
 void wi_move_first(struct win_base *b, struct xa_window *w);
