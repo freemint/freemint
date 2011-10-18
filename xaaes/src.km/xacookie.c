4fed2a52ff456690f3a9fb29b75fd84f253514aa
Helmut Karlowski <helmut.karlowski@ish.de>
2011-10-18 17:54:57 +0000

    xacookie.c xacookie.h: - add read/write-mode for cookie - fix prot-flags for m_xalloc - some security-checks

--- xaaes/src.km/xacookie.c
+++ xaaes/src.km/xacookie.c
@@ -2,29 +2,43 @@
 #include "debug.h"
 #include "xacookie.h"
 #include "mint/ssystem.h"
+#include "mint/proc.h"
+#include "mint/mem.h"
 
-int install_cookie( void **addr, void *buff, size_t sz, long magic )
+int install_cookie( void **addr, void *buff, size_t sz, long magic, bool readonly )
 {
 	char mstr[5];
+	short rw;
+
+	if( readonly )
+		rw = F_PROT_PR;
+	else
+		rw = F_PROT_G;
+
+	if( !addr )
+	{
+		BLOG((0,"%s:install_cookie:ERROR:addr=0", get_curproc()->name));
+		return 3;
+	}
 
 	memcpy( mstr, (char*)&magic, 4 );
 	mstr[4] = 0;
 
-	if ( *addr || (*addr = (void*)m_xalloc(sz, 0x40 | 0x8 | 0x3 ) ) )
+	if ( *addr || (*addr = (void*)m_xalloc(sz, rw | 0x3 ) ) )
 	{
 		long r;
 
 		memcpy(*addr, buff, sz );
 		if ( (r=s_system(S_SETCOOKIE, magic, (long)*addr)) != 0)
 		{
+			BLOG((false, "Installing '%s' cookie failed (%ld,addr=%lx)!", mstr, r, *addr));
 			m_free(*addr);
 			*addr= NULL;
-			BLOG((false, "Installing '%s' cookie failed (%ld)!", mstr, r));
 			return 1;
 		}
 		else
 		{
-			BLOG((false, "Installed '%s' cookie in readable memory at %lx", mstr, (long)addr));
+			BLOG((false, "%s:Installed '%s' cookie in readable memory at %lx", get_curproc()->name, mstr, *(long*)addr));
 			return 0;
 		}
 	}
@@ -40,12 +54,17 @@ void delete_cookie( void **addr, long magic, int free)
 	long r;
 	char mstr[5];
 
+	if( !addr )
+	{
+		BLOG((0,"delete_cookie:ERROR:addr=0"));
+		return;
+	}
 	memcpy( mstr, (char*)&magic, 4 );
 	mstr[4] = 0;
 
 	if( (r = s_system(S_DELCOOKIE, magic, 0L) == 0) )
 	{
-		BLOG((false, "Deleted '%s' cookie at %lx (free=%d)", mstr, (long)addr, free));
+		BLOG((false, "Deleted '%s' cookie at %lx (free=%d)", mstr, *(long*)addr, free));
 		if( *addr && free )
 		{
 			m_free(*addr);
@@ -53,6 +72,6 @@ void delete_cookie( void **addr, long magic, int free)
 		}
 	}
 	else
-		BLOG((false, "Deletetion of '%s' cookie at %lx failed (r=%ld,free=%d)!", mstr, (long)addr, r, free));
+		BLOG((false, "Deletetion of '%s' cookie at %lx failed (r=%ld,free=%d)!", mstr, *(long*)addr, r, free));
 }
 
--- xaaes/src.km/xacookie.h
+++ xaaes/src.km/xacookie.h
@@ -1,3 +1,3 @@
-int install_cookie( void **addr, void *buff, size_t sz, long magic );
+int install_cookie( void **addr, void *buff, size_t sz, long magic, bool readonly );
 void delete_cookie( void **addr, long magic, int free);
 
