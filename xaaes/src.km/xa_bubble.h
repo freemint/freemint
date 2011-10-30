9ff4c6f9704dbd3be8701a3c031cd61371d2cd06
Helmut Karlowski <helmut.karlowski@ish.de>
2011-10-30 11:41:12 +0000

    cnf_xaaes.c k_mouse.c widgets.c widgets.h xa_bubble.c xa_bubble.h xa_strings.c xa_strings.h xa_types.h xaaes015.rsl example.cnf: - new feature: describe_widgets   if set not 0 display name/function of a widget when mouse is placed (not moved) twice above a widget. - sightly redesign bubble-layout

--- xaaes/src.km/xa_bubble.h
+++ xaaes/src.km/xa_bubble.h
@@ -21,6 +21,8 @@ typedef enum{
 }BBL_STATUS;
 
 BBL_STATUS xa_bubble( enum locks lock, BBL_MD md, union msg_buf *msg, short destID );
+void bubble_show( char *str );
+void bubble_request( short pid, short whndl, short x, short y );
 void XA_bubble_event(enum locks lock, struct c_event *ce, bool cancel);
 void display_launched( enum locks lock, char *str );
 
