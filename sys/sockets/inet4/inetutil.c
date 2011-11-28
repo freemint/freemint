07ec74ca7a352f97b8f21820a53175ec853d1f79
Alan Hourihane <alanh@fairlite.co.uk>
2011-11-28 09:33:32 +0000

    Fix INET4 for ColdFire. Contributed by Vincent Riviere.

--- sys/sockets/inet4/inetutil.c
+++ sys/sockets/inet4/inetutil.c
@@ -278,26 +278,63 @@ chksum (void *buf, short nwords)
 	__asm__
 	(
 		"clrl	d0		\n\t"
+#ifdef __mcoldfire__
+		"mvzw	%2, d1		\n\t"
+		"lsrl	#1, d1		\n\t"	/* # of longs in buf */
+#else
 		"movew	%2, d1		\n\t"
 		"lsrw	#1, d1		\n\t"	/* # of longs in buf */
+#endif
 		"bcc	l1		\n\t"	/* multiple of 4 ? */
+#ifdef __mcoldfire__
+		"mvz.w	%1@+, d2	\n\t"
+		"addl	d2, %0		\n\t"	/* no, add in extra word */
+		"addxl	d0, %0		\n"
+#else
 		"addw	%1@+, %0	\n\t"	/* no, add in extra word */
 		"addxw	d0, %0		\n"
+#endif
 		"l1:			\n\t"
+#ifdef __mcoldfire__
+		"subql	#1, d1		\n\t"	/* decrement for dbeq */
+#else
 		"subqw	#1, d1		\n\t"	/* decrement for dbeq */
+#endif
 		"bmi	l3		\n"
 		"l2:			\n\t"
 		"addl	%1@+, %0	\n\t"
 		"addxl	d0, %0		\n\t"
+#ifdef __mcoldfire__
+		"subql	#1, d1		\n\t"
+		"bpls	l2		\n"	/* loop over all longs */
+#else
 		"dbra	d1, l2		\n"	/* loop over all longs */
+#endif
 		"l3:			\n\t"
+#ifdef __mcoldfire__
+		"swap	%0		\n\t"	/* convert to short */
+		"mvzw	%0, d1		\n\t"
+		"clr.w	%0		\n\t"
+		"swap	%0		\n\t"
+		"addl	d1, %0		\n\t"
+		"swap	%0		\n\t"
+		"mvzw	%0, d1		\n\t"
+		"clr.w	%0		\n\t"
+		"swap	%0		\n\t"
+		"addl	d1, %0		\n\t"
+#else
 		"movel	%0, d1		\n\t"	/* convert to short */
 		"swap	d1		\n\t"
 		"addw	d1, %0		\n\t"
 		"addxw	d0, %0		\n\t"
+#endif
 		: "=d"(sum), "=a"(buf)
 		: "g"(nwords), "1"(buf), "0"(sum)
+#ifdef __mcoldfire__
+		: "d0", "d1", "d2"
+#else
 		: "d0", "d1"
+#endif
 	);
 	
 	return (short)(~sum & 0xffff);
