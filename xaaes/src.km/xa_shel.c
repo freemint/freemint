52652a561444b94b696542a0c0defd6db0f680fe
Helmut Karlowski <helmut.karlowski@ish.de>
2012-02-12 10:28:01 +0000

    restore cwd for XaAES in launch after process is created.

--- xaaes/src.km/xa_shel.c
+++ xaaes/src.km/xa_shel.c
@@ -644,6 +644,10 @@ launch(enum locks lock, short mode, short wisgr, short wiscr,
 					ret = -ret;
 				}
 			}
+
+			/* restore cwd for XaAES */
+			d_setdrv(toupper(*C.Aes->home_path - 'A') );
+			d_setpath(C.Aes->home_path);
 			break;
 		}
 		case 3:	/* ACC */
