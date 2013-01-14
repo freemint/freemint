d67ce11cb7ddecbe9c8b49b4437048dccf539dca
Helmut Karlowski <helmut.karlowski@ish.de>
2013-01-14 22:00:40 +0000

    Avoid stairs effect on Console (from trunk)

--- xaaes/src.km/k_main.c
+++ xaaes/src.km/k_main.c
@@ -1522,13 +1522,14 @@ void set_tty_mode( short md )
 
 	sg = KBD_dev_sg;
 	sg.sg_flags &= TF_FLAGS;
+
+	sg.sg_flags |= T_CRMOD; /* Avoid stairs effect on Console */
+
 	if( md == RAW )
 		sg.sg_flags |= T_RAW;
 	else
-	{
 		sg.sg_flags &= ~T_RAW;
-		sg.sg_flags |= T_CRMOD;
-	}
+
 	//KERNEL_DEBUG("sg.sg_flags 0x%x", sg.sg_flags);
 
 	r = f_cntl(C.KBD_dev, (long)&sg, TIOCSETN);
