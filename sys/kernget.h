5ffb1ef94ae510dfca9c42766461c94817e61fb4
hek <hek>
2013-04-06 22:50:46 +0000

    kernfs.c kernget.c kernget.h: create /kern/bootlog that contains the path to the bootlog-file.

--- sys/kernget.h
+++ sys/kernget.h
@@ -1,38 +1,38 @@
 /*
  * $Id$
- * 
+ *
  * This file belongs to FreeMiNT. It's not in the original MiNT 1.12
  * distribution. See the file CHANGES for a detailed log of changes.
- * 
- * 
+ *
+ *
  * Copyright 1999, 2000 Guido Flohr <guido@freemint.de>
  * All rights reserved.
- * 
+ *
  * This file is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
  * any later version.
- * 
+ *
  * This file is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- * 
- * 
+ *
+ *
  * Author: Guido Flohr <guido@freemint.de>
  * Started: 1999-10-24
- * 
+ *
  * please send suggestions, patches or bug reports to me or
  * the MiNT mailing list
- * 
- * 
+ *
+ *
  * Kernel system info filesystem, information retrieval routines.
- * 
- */  
+ *
+ */
 
 # ifndef _kernget_h
 # define _kernget_h
@@ -44,6 +44,7 @@
 
 long kern_get_unimplemented	(SIZEBUF **buffer);
 
+long kern_get_bootlog (SIZEBUF **buffer);
 long kern_get_buildinfo		(SIZEBUF **buffer);
 long kern_get_cookiejar		(SIZEBUF **buffer);
 long kern_get_filesystems	(SIZEBUF **buffer);
