f89f06a6d4af413b65342511f7a0a44cd2077825
Helmut Karlowski <helmut.karlowski@ish.de>
2013-10-30 19:10:38 +0000

    bump version from 1.6.6 to 1.7.0

--- xaaes/src.km/version.h
+++ xaaes/src.km/version.h
@@ -33,8 +33,8 @@
  * main version macros
   */
 #define XAAES_MAJ_VERSION  1
-#define XAAES_MIN_VERSION  6
-#define XAAES_PATCH_LEVEL  6
+#define XAAES_MIN_VERSION  7
+#define XAAES_PATCH_LEVEL  0
 
 #define DEV_STATUS	AES_DEVSTATUS_BETA
 //#define DEV_STATUS	AES_DEVSTATUS_RELEASE
