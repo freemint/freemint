784f5f196b239e0ec3877292e539d280078fc162
Alan Hourihane <alanh@fairlite.co.uk>
2013-11-29 13:55:50 +0000

    Fix file unlocking.
    When len == 0, this is special and treated as the maximum length of the file. So when unlocking
    we capture all locks within the start range and unlock.
    
    This fixes samba's smbd tdb issues.

--- sys/xfs/minixfs/minixdev.c
+++ sys/xfs/minixfs/minixdev.c
@@ -629,8 +629,10 @@ m_ioctl (FILEPTR *f, int mode, void *buf)
 				while (lck)
 				{
 					if (lck->l.l_pid == cpid
-						&& lck->l.l_start == t.l.l_start
-						&& lck->l.l_len == t.l.l_len)
+		                                && ((lck->l.l_start == t.l.l_start
+						     && lck->l.l_len == t.l.l_len) ||
+						    (lck->l.l_start >= t.l.l_start
+						     && t.l.l_len == 0)))
 					{
 						*lckptr = lck->next;
 						
