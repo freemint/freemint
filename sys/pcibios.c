91400226a3116892c0554c99513e12e0ef2f6e78
Alan Hourihane <alanh@fairlite.co.uk>
2014-03-22 13:09:57 +0000

    Add PCI-BIOS wrapper. Contributed by David Galvez.

---
+++ sys/pcibios.c
@@ -0,0 +1,424 @@
+/*
+ * Copyright 2014 David Galvez.
+ * Based on SCSIDRV sys_emu implementation by Frank Naumann.
+ *
+ * This file belongs to FreeMiNT. It's not in the original MiNT 1.12
+ * distribution. See the file CHANGES for a detailed log of changes.
+ *
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+#ifdef PCI_BIOS
+# include "pcibios.h"
+# include "global.h"
+
+# include "arch/pcibios_emu.h"
+# include "mint/proc.h"
+# include "libkern/libkern.h"
+
+# include "cookie.h"	/* cookie handling */
+# include "init.h"	/* boot_printf */
+# include "k_prot.h"
+
+# include "proc.h"
+
+static PCIBIOS_MINT pcibios_MiNT = { {
+	0,
+	0,
+	emu_pcibios_Find_pci_device,
+	emu_pcibios_Find_pci_classcode,
+	emu_pcibios_Read_config_byte,
+	emu_pcibios_Read_config_word,
+	emu_pcibios_Read_config_longword,
+	emu_pcibios_Fast_read_config_byte,
+	emu_pcibios_Fast_read_config_word,
+	emu_pcibios_Fast_read_config_longword,
+	emu_pcibios_Write_config_byte,
+	emu_pcibios_Write_config_word,
+	emu_pcibios_Write_config_longword,
+	emu_pcibios_Hook_interrupt,
+	emu_pcibios_Unhook_interrupt,
+	emu_pcibios_Special_cycle,
+	emu_pcibios_Get_routing,
+	emu_pcibios_Set_interrupt,
+	emu_pcibios_Get_resource,
+	emu_pcibios_Get_card_used,
+	emu_pcibios_Set_card_used,
+	emu_pcibios_Read_mem_byte,
+	emu_pcibios_Read_mem_word,
+	emu_pcibios_Read_mem_longword,
+	emu_pcibios_Fast_read_mem_byte,
+	emu_pcibios_Fast_read_mem_word,
+	emu_pcibios_Fast_read_mem_longword,
+	emu_pcibios_Write_mem_byte,
+	emu_pcibios_Write_mem_word,
+	emu_pcibios_Write_mem_longword,
+	emu_pcibios_Read_io_byte,
+	emu_pcibios_Read_io_word,
+	emu_pcibios_Read_io_longword,
+	emu_pcibios_Fast_read_io_byte,
+	emu_pcibios_Fast_read_io_word,
+	emu_pcibios_Fast_read_io_longword,
+	emu_pcibios_Write_io_byte,
+	emu_pcibios_Write_io_word,
+	emu_pcibios_Write_io_longword,
+	emu_pcibios_Get_machine_id,
+	emu_pcibios_Get_pagesize,
+	emu_pcibios_Virt_to_bus,
+	emu_pcibios_Bus_to_virt,
+	emu_pcibios_Virt_to_phys,
+	emu_pcibios_Phys_to_virt,
+} };
+
+
+# ifdef DEBUG_INFO
+# define PCIBIOS_DEBUG(x)	DEBUG (x)
+# else
+# define SCSIDRV_DEBUG(x)
+# endif
+
+
+ushort pcibios_installed = 0;
+
+PCIBIOS *pcibios = NULL;
+void *tab_funcs_pci;
+
+long
+pcibios_init (void)
+{
+	long r;
+	unsigned long t = 0, dummy = 0;
+
+	r = get_toscookie(COOKIE__PCI, &t);
+	pcibios = (PCIBIOS *)t;
+
+	if (!r && pcibios)
+	{
+		pcibios_installed = pcibios->version;
+		/* First function in jump table */
+		tab_funcs_pci = &pcibios->Find_pci_device;
+		pcibios_MiNT.emu_pcibios.version = pcibios_installed;
+		r = set_toscookie (COOKIE__PCI, (long) &(pcibios_MiNT.emu_pcibios));
+	}
+	else
+	{
+		pcibios_installed = 0;
+		pcibios = NULL;
+		return r;
+	}
+
+	/* Some PCI-BIOS functions in FireTOS and CTPCI PCI-BIOS get the
+	 * cookie value every time they're called to calculate the place
+	 * in memory where descriptors are stored, as the cookie value
+	 * is replaced by MiNT the memory addresses calculated are wrong,
+	 * so to avoid this we need to make this hack. Resource and status
+	 * descriptors are store following the PCI-BIOS functions jump table.
+	 */
+	r = get_toscookie(COOKIE__CPU, &t); /* cf68klib in FireTOS emulates 68060 CPU */
+	if (!r && t == 60)
+		if (!get_toscookie(COOKIE__CF_, &dummy) ||	/* Test for FireTOS */
+		     !get_toscookie(COOKIE_CT60, &dummy)) {	/* Test for CT60 (CTPCI) */
+			/* Copy descriptors following the PCI-BIOS structure */
+			memcpy(&(pcibios_MiNT.pci_rsc_desc), pcibios + 1, 
+				PCI_MAX_HANDLE * PCI_MAX_FUNCTION * ((sizeof(PCI_RSC_DESC) *  PCI_MAX_RSC_DESC) + sizeof(PCI_STS_DESC)));
+			r = set_toscookie (COOKIE__PCI, (long) &pcibios_MiNT);
+	}
+
+	return r;
+}
+
+long _cdecl
+sys_pcibios (ushort op,
+	     long a1, long a2, long a3, long a4,
+	     long a5, long a6, long a7)
+{
+	typedef long (*wrap0)();
+	typedef long (*wrap1)(long);
+	typedef long (*wrap2)(long, long);
+	typedef long (*wrap3)(long, long, long);
+
+	/* only superuser can use this interface */
+	if (!suser (get_curproc()->p_cred->ucr))
+		return EPERM;
+
+	if (!pcibios)
+		return ENOSYS;
+
+	switch (op)
+	{
+		/* PCI_BIOS exist */
+		case 0:
+		case 1:
+		{
+			return E_OK;
+		}
+		/* Find_pci_device */
+		case 2:
+		{
+			wrap2 f = (wrap2)Find_pci_device;
+			return (*f)(a1, a2);
+		}
+		/* Find_pci_classcode */
+		case 3:
+		{
+			wrap2 f = (wrap2)Find_pci_classcode;
+			return (*f)(a1, a2);
+		}
+		/* Read_config_byte */
+		case 4:
+		{
+			wrap3 f = (wrap3)Read_config_byte;
+			return (*f)(a1, a2, a3);
+		}
+		/* Read_config_word */
+		case 5:
+		{
+			wrap3 f = (wrap3)Read_config_word;
+			return (*f)(a1, a2, a3);
+		}
+		/* Read_config_longword */
+		case 6:
+		{
+			wrap3 f = (wrap3)Read_config_longword;
+			return (*f)(a1, a2, a3);
+		}
+		/* Fast_read_config_byte */
+		case 7:
+		{
+			wrap2 f = (wrap2)Fast_read_config_byte;
+			return (*f)(a1, a2);
+		}
+		/* Fast_read_config_word */
+		case 8:
+		{
+			wrap2 f = (wrap2)Fast_read_config_word;
+			return (*f)(a1, a2);
+		}
+		/* Fast_read_config_longword */
+		case 9:
+		{
+			wrap2 f = (wrap2)Fast_read_config_longword;
+			return (*f)(a1, a2);
+		}
+		/* Write_config_byte */
+		case 10:
+		{
+			wrap3 f = (wrap3)Write_config_byte;
+			return (*f)(a1, a2, a3);
+		}
+		/* Write_config_word */
+		case 11:
+		{
+			wrap3 f = (wrap3)Write_config_word;
+			return (*f)(a1, a2, a3);
+		}
+		/* Write_config_longword */
+		case 12:
+		{
+			wrap3 f = (wrap3)Write_config_longword;
+			return (*f)(a1, a2, a3);
+		}
+		/* Hook_interrupt */
+		case 13:
+		{
+			wrap3 f = (wrap3)Hook_interrupt;
+			return (*f)(a1, a2, a3);
+		}
+		/* Unhook_interrupt */
+		case 14:
+		{
+			wrap1 f = (wrap1)Unhook_interrupt;
+			return (*f)(a1);
+		}
+		/* Special_cycle */
+		case 15:
+		{
+			wrap2 f = (wrap2)Special_cycle;
+			return (*f)(a1, a2);
+		}
+		/* Get_routing */
+		/* Set_interrupt */
+		case 16:
+		case 17:
+		{
+			return ENOSYS;
+		}
+		/* Get_resource */
+		case 18:
+		{
+			wrap1 f = (wrap1)Get_resource;
+			return (*f)(a1);
+		}
+		/* Get_card_used */
+		case 19:
+		{
+			wrap2 f = (wrap2)Get_card_used;
+			return (*f)(a1, a2);
+		}
+		/* Set_card_used */
+		case 20:
+		{
+			wrap2 f = (wrap2)Set_card_used;
+			return (*f)(a1, a2);
+		}
+		/* Read_mem_byte */
+		case 21:
+		{
+			wrap3 f = (wrap3)Read_mem_byte;
+			return (*f)(a1, a2, a3);
+		}
+		/* Read_mem_word */
+		case 22:
+		{
+			wrap3 f = (wrap3)Read_mem_word;
+			return (*f)(a1, a2, a3);
+		}
+		/* Read_mem_longword */
+		case 23:
+		{
+			wrap3 f = (wrap3)Read_mem_longword;
+			return (*f)(a1, a2, a3);
+		}
+		/* Fast_read_mem_byte */
+		case 24:
+		{
+			wrap2 f = (wrap2)Fast_read_mem_byte;
+			return (*f)(a1, a2);
+		}
+		/* Fast_read_mem_word */
+		case 25:
+		{
+			wrap2 f = (wrap2)Fast_read_mem_word;
+			return (*f)(a1, a2);
+		}
+		/* Fast_read_mem_longword */
+		case 26:
+		{
+			wrap2 f = (wrap2)Fast_read_mem_longword;
+			return (*f)(a1, a2);
+		}
+		/* Write_mem_byte */
+		case 27:
+		{
+			wrap3 f = (wrap3)Write_mem_byte;
+			return (*f)(a1, a2, a3);
+		}
+		/* Write_mem_word */
+		case 28:
+		{
+			wrap3 f = (wrap3)Write_mem_word;
+			return (*f)(a1, a2, a3);
+		}
+		/* Write_mem_longword */
+		case 29:
+		{
+			wrap3 f = (wrap3)Write_mem_longword;
+			return (*f)(a1, a2, a3);
+		}
+		/* Read_io_byte */
+		case 30:
+		{
+			wrap3 f = (wrap3)Read_io_byte;
+			return (*f)(a1, a2, a3);
+		}
+		/* Read_io_word */
+		case 31:
+		{
+			wrap3 f = (wrap3)Read_io_word;
+			return (*f)(a1, a2, a3);
+		}
+		/* Read_io_longword */
+		case 32:
+		{
+			wrap3 f = (wrap3)Read_io_longword;
+			return (*f)(a1, a2, a3);
+		}
+		/* Fast_read_io_byte */
+		case 33:
+		{
+			wrap2 f = (wrap2)Fast_read_io_byte;
+			return (*f)(a1, a2);
+		}
+		/* Fast_read_io_word */
+		case 34:
+		{
+			wrap2 f = (wrap2)Fast_read_io_word;
+			return (*f)(a1, a2);
+		}
+		/* Fast_read_io_longword */
+		case 35:
+		{
+			wrap2 f = (wrap2)Fast_read_io_longword;
+			return (*f)(a1, a2);
+		}
+		/* Write_io_byte */
+		case 36:
+		{
+			wrap3 f = (wrap3)Write_io_byte;
+			return (*f)(a1, a2, a3);
+		}
+		/* Write_io_word */
+		case 37:
+		{
+			wrap3 f = (wrap3)Write_io_word;
+			return (*f)(a1, a2, a3);;
+		}
+		/* Write_io_longword */
+		case 38:
+		{
+			wrap3 f = (wrap3)Write_io_longword;
+			return (*f)(a1, a2, a3);
+		}
+		/* Get_machine_id */
+		case 39:
+		{
+			wrap1 f = (wrap1)Get_machine_id;
+			return (*f)(a1);
+		}
+		/* Get_pagesize */
+		case 40:
+		{
+			wrap0 f = (wrap0)Get_pagesize;
+			return (*f)();
+		}
+		/* Virt_to_bus */
+		case 41:
+		{
+			wrap3 f = (wrap3)Virt_to_bus;
+			return (*f)(a1, a2, a3);
+		}
+		/* Bus_to_virt */
+		case 42:
+		{
+			wrap3 f = (wrap3)Bus_to_virt;
+			return (*f)(a1, a2, a3);
+		}
+		/* Virt_to_phys */
+		case 43:
+		{
+			wrap2 f = (wrap2)Virt_to_phys;
+			return (*f)(a1, a2);
+		}
+		/* Phys_to_virt */
+		case 44:
+		{
+			wrap2 f = (wrap2)Phys_to_virt;
+			return (*f)(a1, a2);
+		}
+	}
+	return EBADARG;
+}
+#endif /* PCI-BIOS */
---
+++ sys/pcibios.h
@@ -0,0 +1,200 @@
+/*
+ * Copyright 2014 David Galvez.
+ * Based on SCSIDRV sys_emu implementation by Frank Naumann.
+ * Some code chunks taken from FireTOS sources:
+ * Didier Mequignon 2005-2007, e-mail: aniplay@wanadoo.fr
+ *
+ * This file belongs to FreeMiNT. It's not in the original MiNT 1.12
+ * distribution. See the file CHANGES for a detailed log of changes.
+ *
+ *
+ * This file is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+/* Header only for PCI-BIOS system call emulation.
+ * IMPORTANT: Drivers must use /mint/pcibios.h
+ */
+
+# ifndef _pcibios_h
+# define _pcibios_h
+
+# include "mint/mint.h"
+
+/* forward definitions
+ */
+
+typedef struct pcibios	PCIBIOS;
+
+/* exported data
+ */
+
+extern ushort pcibios_installed;
+
+
+/* exported functions
+ */
+
+long	pcibios_init		(void);
+
+long	_cdecl sys_pcibios	(ushort op,
+				     long a1, long a2, long a3, long a4,
+				     long a5, long a6, long a7);
+
+/* the struct definitions
+ */
+
+typedef struct				/* structure of address conversion */
+{
+	unsigned long adr;			/* calculated address (CPU<->PCI) */
+	unsigned long len;			/* length of memory range */
+} PCI_CONV_ADR;
+
+/* structure of resource descriptor */
+typedef struct				/* structure of resource descriptor */
+{
+	unsigned short next;			/* length of the following structure */
+	unsigned short flags;			/* type of resource and misc. flags */
+	unsigned long start;			/* start-address of resource */
+	unsigned long length;			/* length of resource */
+	unsigned long offset;			/* offset PCI to phys. CPU Address */
+	unsigned long dmaoffset;		/* offset for DMA-transfers */
+	long		error;			/* internal error code */
+} PCI_RSC_DESC;
+
+/* structure of status descriptor */
+typedef struct
+{
+	unsigned long status;		/* Status PCI */
+	unsigned long callback;	/* Address of Callback Routine */
+	unsigned long handler;	/* Address of Interrupt Handlers */
+	unsigned long parameter;	/* Parameter for Interrupt Handler */
+	unsigned long start_IRQ;	/* Routine Start IRQ */
+} PCI_STS_DESC;
+
+/* PCI_BIOS structure */
+
+struct pcibios {
+	unsigned long subjar;
+	unsigned long version;
+	/* Although we declare this functions as standard gcc functions (cdecl),
+	 * they expect paramenters inside registers (fastcall) unsupported by gcc m68k.
+	 * Caller will take care of parameters passing convention.
+	 */
+	long (*Find_pci_device)	(unsigned long id, unsigned short index);
+	long (*Find_pci_classcode)	(unsigned long class, unsigned short index);
+	long (*Read_config_byte)	(long handle, unsigned short reg, unsigned char *address);
+	long (*Read_config_word)	(long handle, unsigned short reg, unsigned short *address);
+	long (*Read_config_longword)	(long handle, unsigned short reg, unsigned long *address);
+	unsigned char (*Fast_read_config_byte)	(long handle, unsigned short reg);
+	unsigned short (*Fast_read_config_word)	(long handle, unsigned short reg);
+	unsigned long (*Fast_read_config_longword)	(long handle, unsigned short reg);
+	long (*Write_config_byte)	(long handle, unsigned short reg, unsigned short val);
+	long (*Write_config_word)	(long handle, unsigned short reg, unsigned short val);
+	long (*Write_config_longword)	(long handle, unsigned short reg, unsigned long val);
+	long (*Hook_interrupt)	(long handle, unsigned long *routine, unsigned long *parameter);
+	long (*Unhook_interrupt)	(long handle);
+	long (*Special_cycle)	(unsigned short bus, unsigned long data);
+	long (*Get_routing)	(long handle);
+	long (*Set_interrupt)	(long handle);
+	long (*Get_resource)	(long handle);
+	long (*Get_card_used)	(long handle, unsigned long *address);
+	long (*Set_card_used)	(long handle, unsigned long *callback);
+	long (*Read_mem_byte)	(long handle, unsigned long offset, unsigned char *address);
+	long (*Read_mem_word)	(long handle, unsigned long offset, unsigned short *address);
+	long (*Read_mem_longword)	(long handle, unsigned long offset, unsigned long *address);
+	unsigned char (*Fast_read_mem_byte)	(long handle, unsigned long offset);
+	unsigned short (*Fast_read_mem_word)	(long handle, unsigned long offset);
+	unsigned long (*Fast_read_mem_longword)	(long handle, unsigned long offset);
+	long (*Write_mem_byte)	(long handle, unsigned long offset, unsigned short val);
+	long (*Write_mem_word)	(long handle, unsigned long offset, unsigned short val);
+	long (*Write_mem_longword)	(long handle, unsigned long offset, unsigned long val);
+	long (*Read_io_byte)	(long handle, unsigned long offset, unsigned char *address);
+	long (*Read_io_word)	(long handle, unsigned long offset, unsigned short *address);
+	long (*Read_io_longword)	(long handle, unsigned long offset, unsigned long *address);
+	unsigned char (*Fast_read_io_byte)	(long handle, unsigned long offset);
+	unsigned short (*Fast_read_io_word)	(long handle, unsigned long offset);
+	unsigned long (*Fast_read_io_longword)	(long handle, unsigned long offset);
+	long (*Write_io_byte)	(long handle, unsigned long offset, unsigned short val);
+	long (*Write_io_word)	(long handle, unsigned long offset, unsigned short val);
+	long (*Write_io_longword)	(long handle, unsigned long offset, unsigned long val);
+	long (*Get_machine_id)	(void);
+	long (*Get_pagesize)	(void);
+	long (*Virt_to_bus)	(long handle, unsigned long address, PCI_CONV_ADR *pointer);
+	long (*Bus_to_virt)	(long handle, unsigned long address, PCI_CONV_ADR *pointer);
+	long (*Virt_to_phys)	(unsigned long address, PCI_CONV_ADR *pointer);
+	long (*Phys_to_virt)	(unsigned long address, PCI_CONV_ADR *pointer);
+	long reserved[2];
+};
+
+typedef struct pcibios_mint {
+	PCIBIOS emu_pcibios;
+	/* Only for FireTOS and CTPCI */
+#define PCI_MAX_HANDLE		5 	/* 4 slots on the CTPCI + host bridge PLX9054 */
+#define PCI_MAX_FUNCTION	4	/* 4 functions per PCI slot */
+#define PCI_MAX_RSC_DESC	6	/* 6 resource descriptors */
+	PCI_RSC_DESC pci_rsc_desc[PCI_MAX_HANDLE][PCI_MAX_FUNCTION][PCI_MAX_RSC_DESC];
+	PCI_STS_DESC pci_sts_desc[PCI_MAX_HANDLE][PCI_MAX_FUNCTION];
+} PCIBIOS_MINT;
+
+
+/* PCI bios calls from _PCI cookie
+ */
+
+long Find_pci_device(unsigned long id, unsigned short index);
+long Find_pci_classcode(unsigned long class, unsigned short index);
+long Read_config_byte(long handle, unsigned short reg, unsigned char *address);
+long Read_config_word(long handle, unsigned short reg, unsigned short *address);
+long Read_config_longword(long handle, unsigned short reg, unsigned long *address);
+unsigned char Fast_read_config_byte(long handle, unsigned short reg);
+unsigned short Fast_read_config_word(long handle, unsigned short reg);
+unsigned long Fast_read_config_longword(long handle, unsigned short reg);
+long Write_config_byte(long handle, unsigned short reg, unsigned short val);
+long Write_config_word(long handle, unsigned short reg, unsigned short val);
+long Write_config_longword(long handle, unsigned short reg, unsigned long val);
+long Hook_interrupt(long handle, unsigned long *routine, unsigned long *parameter);
+long Unhook_interrupt(long handle);
+long Special_cycle(unsigned short bus, unsigned long data);
+long Get_routing(long handle);
+long Set_interrupt(long handle);
+long Get_resource(long handle);
+long Get_card_used(long handle, unsigned long *address);
+long Set_card_used(long handle, unsigned long *callback);
+long Read_mem_byte(long handle, unsigned long offset, unsigned char *address);
+long Read_mem_word(long handle, unsigned long offset, unsigned short *address);
+long Read_mem_longword(long handle, unsigned long offset, unsigned long *address);
+unsigned char Fast_read_mem_byte(long handle, unsigned long offset);
+unsigned short Fast_read_mem_word(long handle, unsigned long offset);
+unsigned long Fast_read_mem_longword(long handle, unsigned long offset);
+long Write_mem_byte(long handle, unsigned long offset, unsigned short val);
+long Write_mem_word(long handle, unsigned long offset, unsigned short val);
+long Write_mem_longword(long handle, unsigned long offset, unsigned long val);
+long Read_io_byte(long handle, unsigned long offset, unsigned char *address);
+long Read_io_word(long handle, unsigned long offset, unsigned short *address);
+long Read_io_longword(long handle, unsigned long offset, unsigned long *address);
+unsigned char Fast_read_io_byte(long handle, unsigned long offset);
+unsigned short Fast_read_io_word(long handle, unsigned long offset);
+unsigned long Fast_read_io_longword(long handle, unsigned long offset);
+long Write_io_byte(long handle, unsigned long offset, unsigned short val);
+long Write_io_word(long handle, unsigned long offset, unsigned short val);
+long Write_io_longword(long handle, unsigned long offset, unsigned long val);
+long Get_machine_id(void);
+long Get_pagesize(void);
+long Virt_to_bus(long handle, unsigned long address, PCI_CONV_ADR *pointer);
+long Bus_to_virt(long handle, unsigned long address, PCI_CONV_ADR *pointer);
+long Virt_to_phys(unsigned long address, PCI_CONV_ADR *pointer);
+long Phys_to_virt(unsigned long address, PCI_CONV_ADR *pointer);
+
+# endif /* _pcibios_h */
