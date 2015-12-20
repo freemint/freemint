fbf7aea5720a56fd21543a5343e4e52175af5fe6
Helmut Karlowski <helmut.karlowski@ish.de>
2015-12-20 16:02:46 +0000

    export debug_fp.

--- sys/debug.h
+++ sys/debug.h
@@ -1,14 +1,14 @@
 /*
  * $Id$
- * 
+ *
  * This file has been modified as part of the FreeMiNT project. See
  * the file Changes.MH for details and dates.
- * 
- * 
+ *
+ *
  * Copyright 1991,1992 Eric R. Smith.
  * Copyright 1992,1993,1994 Atari Corporation.
  * All rights reserved.
- * 
+ *
  */
 
 # ifndef _debug_h
@@ -16,5 +16,6 @@
 
 # include "mint/mint.h"
 
+extern FILEPTR *debug_fp;
 
 # endif /* _debug_h */
