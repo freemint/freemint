24646474c39299b6017413645caec1f00d080e5e
Alan Hourihane <alanh@fairlite.co.uk>
2016-03-03 17:09:46 +0000

    Fixes for support of GCC 4.7

--- sys/libkern/md5.c
+++ sys/libkern/md5.c
@@ -160,8 +160,14 @@ MD5Final(unsigned char digest[16], struct MD5Context *ctx)
 	byteReverse((unsigned long *) ctx->in, 14);
 	
 	/* Append length in bits and transform */
-	((__u32 *) ctx->in)[14] = ctx->bits[0];
-	((__u32 *) ctx->in)[15] = ctx->bits[1];
+	{
+	   __u32 *ptr;
+
+	   ptr = (__u32 *)&ctx->in[56];
+	   *ptr = ctx->bits[0];
+	   ptr = (__u32 *)&ctx->in[60];
+	   *ptr = ctx->bits[1];
+        }
 	
 	MD5Transform(ctx->buf, (__u32 *) ctx->in);
 	byteReverse(ctx->buf, 4);
