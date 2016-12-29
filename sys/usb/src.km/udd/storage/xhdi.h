c256db3d43c8f1c8833eec4d9f1c3674de78f713
alanh <alanh>
2016-12-29 13:42:12 +0000

    Move definition to appropriate place.

--- sys/usb/src.km/udd/storage/xhdi.h
+++ sys/usb/src.km/udd/storage/xhdi.h
@@ -22,8 +22,6 @@
 #include "../../../../pun.h"		/* for PUN_INFO */
 /* AHDI */
 
-#define PUN_USB           0x20 /* USB */
-
 /* BIOS parameter block */
 
 struct  bpb
@@ -52,6 +50,7 @@ struct usb_pun_info
 # define PUN_UNIT	0x7			/* Unit number */
 # define PUN_SCSI	0x8			/* 1=SCSI 0=ACSI */
 # define PUN_IDE	0x10			/* Falcon IDE */
+# define PUN_USB        0x20			/* USB */
 # define PUN_REMOVABLE	0x40			/* Removable media */
 # define PUN_VALID	0x80			/* zero if valid */
 	long	partition_start [MAX_LOGICAL_DRIVE];
