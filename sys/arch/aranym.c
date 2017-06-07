4f8c4bf7b0a854cd99b5872c352df0c2905c3dd7
Thorsten Otto <thotto@users.sourceforge.net>
2017-06-07 04:31:14 +0200

    Move nf_call global variable to aranym.c as it is used by several modules

--- sys/arch/aranym.c
+++ sys/arch/aranym.c
@@ -46,6 +46,14 @@ static unsigned long nf_call_instr = 0x73014e75UL;
 static struct nf_ops _nf_ops = { (void *)&nf_get_id_instr, (void *)&nf_call_instr }; 
 static struct nf_ops *nf_ops = NULL; 
 
+/*
+ * nf_ops->call function reference.
+ */
+#if defined WITH_HOSTFS || defined WITH_ARANYMFS
+long __CDECL (*nf_call)(long id, ...) = 0L;
+#endif
+
+
 struct nf_ops *
 nf_init(void)
 {
