1361b1157812153f11a3a33a45cfdcfdb4ba40d2
Thorsten Otto <admin@tho-otto.de>
2017-06-09 02:31:58 +0200

    Pass process id to denylock()

--- sys/kerinfo.c
+++ sys/kerinfo.c
@@ -69,12 +69,6 @@ static void * _cdecl m_dmabuf_alloc(ulong size, short cm)
 { return _dmabuf_alloc (size, cm, "xfs/xdd"); }
 #endif
 
-static LOCK * _cdecl
-denylock_curproc(LOCK *list, LOCK *newlock)
-{
-	return denylock(get_curproc()->pid, list, newlock);
-}
-
 static long _cdecl
 old_kthread_create(void _cdecl (*func)(void *), void *arg,
 		   struct proc **np, const char *fmt, ...)
@@ -112,7 +106,7 @@ struct kerinfo kernelinfo =
 	ksprintf_old,
 	ms_time, unixtime, dostime,
 	nap, sleep, wake, (void _cdecl (*)(long)) wakeselect,
-	denyshare, denylock_curproc,
+	denyshare, denylock,
 	addtimeout_curproc, canceltimeout,
 	addroottimeout, cancelroottimeout,
 	ikill, iwake,
