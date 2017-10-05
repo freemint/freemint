e2f350f8d67c879b3a192c88afc7aef61a6d57ab
Thorsten Otto <admin@tho-otto.de>
2017-10-05 16:43:10 +0200

    mkfatfs: fix handling of drive numbers >= 26

--- tools/mkfatfs/mkfatfs.c
+++ tools/mkfatfs/mkfatfs.c
@@ -356,6 +356,7 @@ static char *	vol	= NULL;	/* Volume name */
 static time_t	cr_time	= 0;	/* Creation time */
 
 static ushort	drv	= 0;
+static ushort	drv_char = '?';
 static ushort	major	= 0;
 static ushort	minor	= 0;
 static ulong	start	= 0;
@@ -448,7 +449,7 @@ cdiv (long a, long b)
 static void
 fatal_ (const char *fmt_string)
 {
-	printf (fmt_string, program, 'A' + DRV);
+	printf (fmt_string, program, drv_char);
 	
 	exit (1);
 }
@@ -677,7 +678,7 @@ get_geometry (void)
 {
 	long r;
 	
-	r = XHInqDev2 (DRV, &MAJOR, &MINOR, &START, NULL, &PSECS, ID);
+	r = XHInqDev2 (drv, &MAJOR, &MINOR, &START, NULL, &PSECS, ID);
 	if (r == 0)
 	{
 		r = XHInqTarget2 (MAJOR, MINOR, &PSECSIZE, NULL, NULL, 0);
@@ -690,7 +691,7 @@ get_geometry (void)
 	
 	ID [3] = '\0';
 	
-	printf ("Physical informations about partition %c:\n", 'A' + DRV);
+	printf ("Physical informations about partition %c:\n", drv_char);
 	printf ("----------------------------------------\n");
 	printf ("XHDI major number    : %d\n", MAJOR);
 	printf ("XHDI minor number    : %d\n", MINOR);
@@ -1007,7 +1008,7 @@ setup_tables (void)
 	
 	if (VERBOSE)
 	{
-		printf ("Logical informations about partition %c:\n", 'A' + DRV);
+		printf ("Logical informations about partition %c:\n", drv_char);
 		printf ("---------------------------------------\n");
 		printf ("Media descriptor    : 0x%02x\n", (unsigned int)(BOOT.media));
 		printf ("logical sector size : %ld\n", SECSIZE);
@@ -1323,12 +1324,12 @@ verify_user (void)
 	
 	if (ONLY_BS == YES)
 	{
-		printf ("WARNING: THIS WILL OVERWRITE YOUR BOOTSECTOR ON %c:\n", 'A' + DRV);
+		printf ("WARNING: THIS WILL OVERWRITE YOUR BOOTSECTOR ON %c:\n", drv_char);
 		printf ("Are you ABSOLUTELY SURE you want to do this? (y/n) ");
 	}
 	else
 	{
-		printf ("WARNING: THIS WILL TOTALLY DESTROY ANY DATA ON %c:\n", 'A' + DRV);
+		printf ("WARNING: THIS WILL TOTALLY DESTROY ANY DATA ON %c:\n", drv_char);
 		printf ("Are you ABSOLUTELY SURE you want to do this? (y/n) ");
 	}
 	scanf ("%c", &c);
@@ -1490,12 +1491,18 @@ main (int argc, char **argv)
 		usage ();
 	}
 	
-	DRV = (ushort) (toupper (c) - 'A');
-	if (DRV > 31)
+	drv_char = toupper (c);
+	if (drv_char >= 'A' && drv_char <= 'Z')
+	{
+		drv = drv_char - 'A';
+	} else if (drv_char >= '1' && drv_char <= '6')
+	{
+		drv = (drv_char - '1') + 26;
+	} else
 	{
 		fatal ("invalid drive");
 	}
-	
+
 	/*
 	 * Auto and specified bad sector handling are mutually
 	 * exclusive of each other!
@@ -1506,7 +1513,7 @@ main (int argc, char **argv)
 	}
 	
 	/* lock the drv */
-	if (Dlock (1, DRV))
+	if (Dlock (1, drv))
 	{
 		fatal ("Can't lock %c:");
 	}
@@ -1536,10 +1543,10 @@ main (int argc, char **argv)
 		write_tables ();
 	}
 	
-	(void) Dlock (0, DRV);
+	(void) Dlock (0, drv);
 	
 	/* Terminate with no errors! */
-	exit (0);
+	return EXIT_SUCCESS;
 }
 
 /* END main */
