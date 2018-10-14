266617821a601f9f5a4133d3f36dc80d56201a4e
Thorsten Otto <admin@tho-otto.de>
2018-10-15 00:18:59 +0200

    COPS: do not print error message about missing WDIALOG during boot when running as ACC; it will be shown when the accessory is activated

--- tools/cops/adaptrsc.h
+++ tools/cops/adaptrsc.h
@@ -27,17 +27,18 @@
 #include <gem.h>
 #include "global.h"
 
-#define	GAI_WDLG	0x0001	/* wdlg_xx()-Funktionen vorhanden */
-#define	GAI_LBOX	0x0002	/* lbox_xx()-Funktionen vorhanden */
-#define	GAI_FNTS	0x0004	/* fnts_xx()-Funktionen vorhanden */
-#define	GAI_FSEL	0x0008	/* neue Dateiauswahl vorhanden */
+#define	GAI_WDLG	0x0001	/* wdlg_xx()-functions available */
+#define	GAI_LBOX	0x0002	/* lbox_xx()-functions available */
+#define	GAI_FNTS	0x0004	/* fnts_xx()-functions available */
+#define	GAI_FSEL	0x0008	/* new file selector (fslx_xx) available */
+#define	GAI_PDLG	0x0010	/* pdlg_xx()-functions available */
 
-#define	GAI_MAGIC	0x0100	/* MagiC-AES vorhanden */
-#define	GAI_INFO	0x0200	/* appl_getinfo() vorhanden */
-#define	GAI_3D		0x0400	/* 3D-Look vorhanden */
-#define	GAI_CICN	0x0800	/* Color-Icons vorhanden */
-#define	GAI_APTERM	0x1000	/* AP_TERM wird unterstÅtzt */
-#define	GAI_GSHORTCUT	0x2000	/* Objekttyp G_SHORTCUT wird unterstÅtzt */
+#define	GAI_MAGIC	0x0100	/* MagiC-AES present */
+#define	GAI_INFO	0x0200	/* appl_getinfo() supported */
+#define	GAI_3D		0x0400	/* 3D-Look supported */
+#define	GAI_CICN	0x0800	/* Color-Icons supported */
+#define	GAI_APTERM	0x1000	/* AP_TERM supported */
+#define	GAI_GSHORTCUT	0x2000	/* object type G_SHORTCUT supported */
 
 _WORD get_aes_info(_WORD *font_id, _WORD *font_height, _WORD *hor_3d, _WORD *ver_3d);
 void adapt3d_rsrc(OBJECT *objs, unsigned short no_objs, _WORD hor_3d, _WORD ver_3d);
