d6f279f990fed51d3526ca73317f1f90e3bb1cdb
David Gálvez <dgalvez75@gmail.com>
2018-12-28 17:39:42 +0100

    USB mass storage: Cosmetic

--- sys/usb/src.km/udd/storage/usb_storage.h
+++ sys/usb/src.km/udd/storage/usb_storage.h
@@ -6,11 +6,11 @@ typedef long (*trans_reset)(struct us_data *data);
  *     For this struct we should take care and cast to long the unsigned 
  *     char elements before calling some of the MACROs defined in usb.h
  *     If we don't do that, when compiling with mshort, some values are 
- *	   transformed wrongly during the operations done in those MACROs
+ *     transformed wrongly during the operations done in those MACROs
  */
 struct us_data
 {
-	struct usb_device *pusb_dev;		 /* this usb_device */
+	struct usb_device *pusb_dev;		/* this usb_device */
 	unsigned long	flags;			/* from filter initially */
 	unsigned char	ifnum;			/* interface number */
 	unsigned char	ep_in;			/* in endpoint */
@@ -23,7 +23,7 @@ struct us_data
 	long		action;			/* what to do */
 	long		ip_wanted;		/* needed */
 	long		*irq_handle;		/* for USB int requests */
-	unsigned long	irqpipe;	 	/* pipe for release_irq */
+	unsigned long	irqpipe;		/* pipe for release_irq */
 	unsigned char	irqmaxp;		/* max packed for irq Pipe */
 	unsigned char	irqinterval;		/* Intervall for IRQ Pipe */
 	ccb		*srb;			/* current srb */
