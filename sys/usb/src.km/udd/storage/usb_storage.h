6290e4b97ed8c52edf59b4cf11934db12cff8368
David Gálvez <dgalvez75@gmail.com>
2019-01-18 11:28:26 +0100

    USB mass storage: Add comment

--- sys/usb/src.km/udd/storage/usb_storage.h
+++ sys/usb/src.km/udd/storage/usb_storage.h
@@ -36,7 +36,7 @@ struct us_data
 #define USB_MAX_STOR_DEV 	8	/* Total number of USB mass storage devices */
 
 struct mass_storage_dev {
-	unsigned char	target;
+	unsigned char	target;					/* USB device ID */
 	struct us_data usb_stor;
 	block_dev_desc_t *usb_dev_desc[MAX_LUN_NUM_PER_DEV];	/* LUNs structs */
 };
