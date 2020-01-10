00d8bcefc397e8aec507d1af6401b018ffe320f0
David Galvez <dgalvez75@gmail.com>
2020-01-10 16:10:31 +0100

    XaAES: Align comment (cosmmetic)

--- xaaes/src.km/xa_fsel.c
+++ xaaes/src.km/xa_fsel.c
@@ -1,7 +1,7 @@
 /*
  * XaAES - XaAES Ain't the AES (c) 1992 - 1998 C.Graham
- *																 1999 - 2003 H.Robbers
- *																				2004 F.Naumann & O.Skancke
+ *                                 1999 - 2003 H.Robbers
+ *                                        2004 F.Naumann & O.Skancke
  *
  * A multitasking AES replacement for FreeMiNT
  *
