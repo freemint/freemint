dc109fda26a26f3ad7421a6d9406ee5bd2623534
Thorsten Otto <admin@tho-otto.de>
2020-02-18 10:47:25 +0100

    xaaes: rework & fix translation of menus
    This was previously done by an utterly complex state-machine
    while looping over all objects of a resource. This could even lead
    to false positives in normal dialogs, because it ignored the menu
    structure and just looked at the object types.

--- xaaes/src.km/xa_rsc_lang.h
+++ xaaes/src.km/xa_rsc_lang.h
@@ -1,13 +1,6 @@
-#define MAX_TITLES 32
-
-struct rsc_scan
-{
-	short n_titles, title, lastbox, mwidth, keep_w, change_lo[MAX_TITLES], shift[MAX_TITLES];
-	char *titles[MAX_TITLES];
-};
-
-short rsc_trans_rw(struct xa_client *client, XA_FILE *rfp, char **txt, short *chgl);
-void rsc_lang_translate_object(struct xa_client *client, XA_FILE *rfp, OBJECT *obj, struct rsc_scan *scan);
+short rsc_trans_rw(struct xa_client *client, XA_FILE *rfp, char **txt, bool *strip);
+void rsc_lang_translate_object(struct xa_client *client, XA_FILE *rfp, OBJECT *obj, bool *strip);
+void rsc_lang_fix_menu(struct xa_client *client, OBJECT *menu, XA_FILE *rfp);
 
 XA_FILE *rsc_lang_file_open(const char *name);
 XA_FILE *rsc_lang_file_create(const char *name);
