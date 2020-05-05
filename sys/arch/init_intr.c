freemint/freemint

commit 625241ae26f8a726eccb557a824d61159067faed

Revert changes done for AKP support done by commit...
0317cb716b169cec9c07abf93352ad9ed15e0a28
with that commit the execution of the KBDVEC handler was removed
(Milan target being an exception). Software compatible with TOS 2.00
or newer expects that this vector handler to be executed. An example
is the USB keyboard driver in FireTOS and CT60 TOS 2.0x which complains
that MiNT's AKP support is not compatible with TOS > 2.00.

sys/arch/init_intr.c
@@ -94,17 +94,7 @@ init_intr (void)
 	oldkey = *syskey;
 
 # ifndef NO_AKP_KEYBOARD
-	/* Hook the keyboard interrupt to call ikbd_scan() on keyboard data */
-	if (machine == machine_milan || machine == machine_unknown)
-	{
-		/* Assume that TOS >= 2.0. In this case, there is an undocumented
-		 * vector just before the KBDVEC structure. This vector is called
-		 * by the TOS ikbdsys routine to process keyboard-only data.
-		 * It is exactly what we need to hook. */
-		long *kbdvec = ((long *)syskey)-1;
-		new_xbra_install (&oldkeys, (long)kbdvec, newkeys);
-	}
-	else
+	if (tosvers < 0x0200) /* TOS versions without the KBDVEC vector */
 	{
 		/* We need to hook the ikbdsys vector. Our handler will have to deal
 		 * with ACIA registers, and to call the appropriate KBDVEC vectors
@@ -117,6 +107,15 @@ init_intr (void)
 		/* Also hook the whole ACIA interrupt (why??) */
 		new_xbra_install(&old_acia, 0x0118L, new_acia);
 		spl(savesr);
+	}
+	else /* Hook the keyboard interrupt to call ikbd_scan() on keyboard data */
+	{
+		/* Assume that TOS >= 2.0. In this case, there is an undocumented
+		 * vector just before the KBDVEC structure. This vector is called
+		 * by the TOS ikbdsys routine to process keyboard-only data.
+		 * It is exactly what we need to hook. */
+		long *kbdvec = ((long *)syskey)-1;
+		new_xbra_install (&oldkeys, (long)kbdvec, newkeys);
 	}
 # endif
 	old_term = (long) TRAP_Setexc (0x102, -1UL);
@@ -241,14 +240,14 @@ restr_intr (void)
 	*syskey = oldkey;	/* restore keyboard vectors */
 
 # ifndef NO_AKP_KEYBOARD
-	if (machine == machine_milan || machine == machine_unknown)
+	if (tosvers < 0x0200)
 	{
-		long *kbdvec = ((long *)syskey)-1;
-		*kbdvec = (long) oldkeys;
+		*((long *) 0x0118L) = old_acia;
 	}
 	else
 	{
-		*((long *) 0x0118L) = old_acia;
+		long *kbdvec = ((long *)syskey)-1;
+		*kbdvec = (long) oldkeys;
 	}
 # endif
 

sys/arch/acia.S
@@ -46,8 +46,8 @@ _new_acia:
 	movem.l	d0-d3/a0-a3,-(sp)
 #endif
 again:	move.l	_syskey,a3
-//	move.l	VMIDISYS(a3),a0
-//	jsr	(a0)
+	move.l	VMIDISYS(a3),a0
+	jsr	(a0)
 	move.l	VIKBDSYS(a3),a0
 	jsr	(a0)
 #ifdef __mcoldfire__
@@ -117,20 +117,8 @@ no_error:
 	bne.s	mid_multiple
 	cmp.b	#0xf6,d0		// $f6-$ff are multibyte packets
 	bcc.s	not_keyboard
-#ifdef __mcoldfire__
-	and.l	#0xff,d0
-#else
-	and.w	#0xff,d0
-#endif
-	move.l	_keyrec,-(sp)
-	move.w	d0,-(sp)
-	jsr	_ikbd_scan
-	addq.l	#6,sp
-	rts
-
-//	jmp	_newkeys
-//	move.l	KBDVEC(a3),a1
-//	jmp	(a1)
+	move.l	KBDVEC(a3),a1
+	jmp	(a1)
 
 not_keyboard:
 #ifdef __mcoldfire__

commit b7cf33b4cd9a41bfc15c4290adfca3de446fb1bf

Fix regression in keyboard handling
Introduced with commit 625241ae26f8a726eccb557a824d61159067faed,
affects keyboard handling when the kernel runs over TOS < 2.00.
KBDVEC hooked by the kernel was not called in that case.

sys/arch/init_intr.c
@@ -108,15 +108,16 @@ init_intr (void)
 		new_xbra_install(&old_acia, 0x0118L, new_acia);
 		spl(savesr);
 	}
-	else /* Hook the keyboard interrupt to call ikbd_scan() on keyboard data */
-	{
-		/* Assume that TOS >= 2.0. In this case, there is an undocumented
-		 * vector just before the KBDVEC structure. This vector is called
-		 * by the TOS ikbdsys routine to process keyboard-only data.
-		 * It is exactly what we need to hook. */
-		long *kbdvec = ((long *)syskey)-1;
-		new_xbra_install (&oldkeys, (long)kbdvec, newkeys);
-	}
+	/* Hook the keyboard interrupt to call ikbd_scan() on keyboard data.
+	 * There is an undocumented vector just before the KBDVEC structure.
+	 * This vector is called by the TOS ikbdsys routine to process
+	 * keyboard-only data. It is exactly what we need to hook.
+	 * TOS < 2.00 doesn't know about this vector but the new ikdsys
+	 * hadler hooked above if we're running over TOS < 2.00 will call it.
+	 */
+	long *kbdvec = ((long *)syskey)-1;
+	new_xbra_install (&oldkeys, (long)kbdvec, newkeys);
+
 # endif
 	old_term = (long) TRAP_Setexc (0x102, -1UL);
 