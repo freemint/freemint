freemint/freemint

commit 2dfde1b15202fca5f4c6ceeacce5cec6087b17d0

Revert commit. This change is available on helmut-enhancements.

sys/filesys.c
@@ -666,7 +666,7 @@ disk_changed (ushort d)
 		 * Note that ENODEV must be tested for drives A-C, or else
 		 * booting may not work properly.
 		 */
-		if (d >= 2 && r == ENODEV)
+		if (d > 2 && r == ENODEV)
 			return 0;	/* assume no change */
 		else
 			return r;

sys/init.c
@@ -332,7 +332,6 @@ init (void)
 
 		usp = get_usp();
 		ssp = get_ssp();
-		ssp = 0;
 
 		DEBUG(("Kernel BASE: 0x%08lx", _base));
 		DEBUG(("Kernel TEXT: 0x%08lx (SIZE: %ld bytes)", _base->p_tbase, _base->p_tlen));
@@ -1005,8 +1004,6 @@ do_exec_os (long basepage)
 	TRAP_Pterm ((int)r);
 }
 
-int sys_err= 0;
-
 void
 mint_thread(void *arg)
 {
@@ -1074,8 +1071,7 @@ mint_thread(void *arg)
 	 */
 	{
 		unsigned short i;
-		char cwd[PATH_MAX];
-		strcpy( cwd, "X:");
+		char cwd[PATH_MAX] = "X:";
 
 		for (i = 0; i < NUM_DRIVES; i++)
 		{
@@ -1286,10 +1282,7 @@ mint_thread(void *arg)
 	}
 # ifndef DEBUG_INFO
 	else
-	{
-		sys_err |= 0x80;
 		sys_s_hutdown(SHUT_HALT);		/* Everything failed. Halt. */
-	}
 
 	/* If init program exited, reboot the system.
 	 * Never go back to TOS.
@@ -1300,7 +1293,6 @@ mint_thread(void *arg)
 	/* With debug kernels, always halt
 	 */
 	FORCE("init:sys_s_hutdown:HALT");
-	sys_err |= 0x80;
 	(void) sys_s_hutdown(SHUT_HALT);
 # endif
 

sys/mint/debug.h
@@ -1,6 +1,6 @@
 /*
  * $Id$
- *
+ * 
  * This file has been modified as part of the FreeMiNT project. See
  * the file Changes.MH for details and dates.
  */
@@ -32,7 +32,6 @@ extern int debug_logging;
 # define TRACE_LEVEL	3
 # define LOW_LEVEL	4
 
-# define DBG FORCE
 
 # ifndef DEBUG_INFO
 
@@ -40,20 +39,15 @@ extern int debug_logging;
 # define TRACE(x)
 # define DEBUG(x)
 
-# define DBG_FORCE(x)
-
 # define FUNCTION __FUNCTION__
 
-# else /* DEBUG_INFO */
+# else
 
 # define TRACELOW(s)	Tracelow s
 
 # define TRACE(s)	Trace s
 
-# define DEBUG(s)	Debug s
-
-/* can be removed by sed */
-# define DBG_FORCE(x) FORCE x
+# define DEBUG(s)	Debug s 
 
 # ifndef str
 # define str(x)		_stringify(x)

sys/tosfs.c
@@ -83,7 +83,7 @@ long gemdos_version;
  */
 #define NEWWAY
 
-#if 1
+#if 0
 #define COOKIE_DB(x) DEBUG(x)
 #else
 #define COOKIE_DB(x)
@@ -1122,12 +1122,7 @@ again:
 		namelen -= (int) sizeof(long);
 		if (namelen <= 0)
 			return EBADARG;
-#ifdef M68000
-		memcpy( name, &index, 4);
-		index++;
-#else
 		*((long *)name) = index++;
-#endif
 		name += sizeof(long);
 	}
 	if (strlen(DIR_NAME(dirh)) < namelen) {

commit 75601465b6b999791cc0706e204171ad9022cfa7

Make OLDTOSFS work with Hatari's GEMDOS drive emulation
With this change, the Hatari-specific kernel MINTHAT.PRG, which
is compiled with OLDTOSFS, can be used to boot FreeMiNT from
a drive C: that uses Hatari's GEMDOS HD emulation.

Note: This was broken since commit
2dfde1b15202fca5f4c6ceeacce5cec6087b17d0.

sys/filesys.c
@@ -678,7 +678,16 @@ disk_changed (ushort d)
 		 * Note that ENODEV must be tested for drives A-C, or else
 		 * booting may not work properly.
 		 */
+#ifdef OLDTOSFS
+		/* The kernel for the Hatari emulator (minthat.prg) is compiled
+		 * with OLDTOSFS to enable access to hard drives emulated only on
+		 * GEMDOS level. However, in that case there is no BIOS device
+		 * for drive C:, either, and we must ignore ENODEV.
+		 */
+		if (d >= 2 && r == ENODEV)
+#else
 		if (d > 2 && r == ENODEV)
+#endif
 			return 0;	/* assume no change */
 		else
 			return r;