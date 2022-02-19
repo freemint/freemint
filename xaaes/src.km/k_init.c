freemint/freemint

commit 7df69a09409099d9f15895e4e332ac7d51dcaf33

Huge job changing window-widget layoutcode plus making things very much more configurable. This work is not done so be careful. These changes will not look good unless one makes the icons in the widgetfile of choise transparent, and remove borders from within the icons

xaaes/src.km/k_init.c
@@ -60,6 +60,24 @@
 #include "cookie.h"
 
 
+static short
+vcheckmode(short mode)
+{
+	__asm__ volatile
+	(
+		"movem.l	d1-d7/a0-a6,-(sp)\n\t"	\
+		"move.w		%0,-(sp)\n\t"
+		"move.w		#0x5f,-(sp)\n\t"
+		"trap		#14\n\t"
+		"addq.w		#4,sp\n\t"
+//		"move.w		d0,%0\n\t"
+		"movem.l	(sp)+,d1-d7/a0-a6\n\t"
+		:
+		: "d"(mode), "r"(mode)
+		: "memory"
+	);
+	return mode;
+}
 /*
  * check if file exist in Aes_home_path
  */
@@ -136,7 +154,6 @@ k_init(void)
 
 	work_in[10] = 2;
 
-	
 	if (cfg.auto_program)
 	{
 		short mode = 1;
@@ -150,11 +167,19 @@ k_init(void)
 		{
 			if (vdo == 0x00030000L)
 			{
+
 				DIAGS(("Falcon video: mode %d(%x)", cfg.videomode, cfg.videomode));
 
 				/* Ronald Andersson:
 				 * This should be the method for falcon!
 				 */
+				cfg.videomode = vcheckmode(cfg.videomode);
+				if ((cfg.videomode & (1 << 4)) &&	/* VGA_FALCON */
+				    (cfg.videomode & 7) == 4)		/* is 16bit */
+				{
+					cfg.videomode &= ~(1 << 3);		/* Set 320 pixels */
+				}
+				
 				work_out[45] = cfg.videomode;
 				mode = 5;
 			}
@@ -175,12 +200,14 @@ k_init(void)
 		{
 			DIAGS(("Default screenmode"));
 		}
-
 		DIAGS(("Screenmode is: %d", mode));
+		display("Screenmode is: %d", mode);
+		
 		work_in[0] = mode;
 
 		v_opnwk(work_in, &(C.P_handle), work_out);
 		DIAGS(("Physical work station opened: %d", C.P_handle));
+		display("Physical work station opened: %d", C.P_handle);
 
 		if (C.P_handle == 0)
 		{
@@ -208,6 +235,7 @@ k_init(void)
 	/* Open us a virtual workstation for XaAES to play with */
 	C.vh = C.P_handle;
 	v_opnvwk(work_in, &C.vh, work_out);
+	display("AES virtual vdi handle = %d", C.vh);
 
 	if (C.vh == 0)
 	{

xaaes/src.km/about.c
@@ -93,7 +93,7 @@ about_form_exit(struct xa_client *client,
 		case ABOUT_OK:
 		{
 			object_deselect(obtree + ABOUT_OK);
-			display_toolbar(lock, about_window, ABOUT_OK);
+			redraw_toolbar(lock, about_window, ABOUT_OK);
 			close_window(lock, about_window);
 			delete_window(lock, about_window);
 			break;
@@ -161,7 +161,7 @@ open_about(enum locks lock)
 		(obtree + ABOUT_INFOSTR)->ob_spec.free_string = info_string;
 #endif
 
-		wt = set_toolbar_widget(lock, dialog_window, dialog_window->owner, obtree, -1, WIDG_NOTEXT, NULL);
+		wt = set_toolbar_widget(lock, dialog_window, dialog_window->owner, obtree, -1, WIP_NOTEXT, NULL);
 		wt->exit_form = about_form_exit;
 		list = object_get_slist(obtree + ABOUT_LIST);
 

xaaes/src.km/c_window.c
@@ -435,7 +435,9 @@ void
 send_untop(enum locks lock, struct xa_window *wind)
 {
 	struct xa_client *client = wind->owner;
-
+	
+	wind->colours = wind->untop_cols;
+	
 	if (wind->send_message && !client->fmd.wind)
 		wind->send_message(lock, wind, NULL, AMQ_NORM, QMF_CHKDUP,
 				   WM_UNTOPPED, 0, 0, wind->handle,
@@ -448,10 +450,15 @@ send_ontop(enum locks lock)
 	struct xa_window *top = window_list;
 	struct xa_client *client = top->owner;
 
-	if (top->send_message && !client->fmd.wind)
-		top->send_message(lock, top, NULL, AMQ_NORM, QMF_CHKDUP,
-				  WM_ONTOP, 0, 0, top->handle,
-				  0, 0, 0, 0);
+	if (!client->fmd.wind)
+	{
+		top->colours = top->ontop_cols;
+		
+		if (top->send_message)
+			top->send_message(lock, top, NULL, AMQ_NORM, QMF_CHKDUP,
+					  WM_ONTOP, 0, 0, top->handle,
+					  0, 0, 0, 0);
+	}
 }
 
 void
@@ -600,6 +607,62 @@ uniconify_window(enum locks lock, struct xa_window *wind, RECT *r)
 {
 	move_window(lock, wind, true, ~XAWS_ICONIFIED, r->x, r->y, r->w, r->h);
 }
+
+struct xa_window_colours def_otop_wc =
+{
+ G_LBLACK, /* window frame color */
+/* flags	wrmode		color		Interior	style	tl color	br color */
+ { 0,		MD_REPLACE,	G_LWHITE,	FIS_SOLID,	0,	G_WHITE,	G_LBLACK },	/* window areas not covered by a widget/ unused widgets*/
+ { WCOL_DRAW3D|WCOL_ACT3D|WCOL_DRAWBKG,		MD_REPLACE,	G_LWHITE,	FIS_SOLID,	0,	G_WHITE,	G_BLACK },	/* Slider */
+ { WCOL_DRAW3D|WCOL_ACT3D|WCOL_DRAWBKG,		MD_REPLACE,	G_LBLACK,	FIS_SOLID,	0,	G_WHITE,	G_LBLACK },	/* Slide */
+ { WCOL_DRAW3D|WCOL_ACT3D|WCOL_DRAWBKG,		MD_REPLACE,	G_LWHITE,	FIS_SOLID,	0,	G_WHITE,	G_LBLACK },	/* Title */
+/* flags	                     fontID	    size	   Effect     forground       background	*/
+/*								                  col	          col		*/
+ { WTXT_DRAW3D|WTXT_ACT3D,	        1,            10,	        0,	G_BLACK,	G_WHITE },		/* Title text info */
+ { WCOL_DRAW3D|WCOL_DRAWBKG,		MD_REPLACE,	G_WHITE,	FIS_SOLID,	0,	G_LBLACK,	G_BLACK},	/* Info */
+/* flags	                    fontID	    size	   Effect     forground	       background	*/
+/*								                   col	          col		*/
+ { 0,		      		        1,	      9,		0,	G_BLACK,	G_WHITE },		/* Info text info */
+ { WCOL_DRAW3D|WCOL_ACT3D|WCOL_DRAWBKG, MD_REPLACE,	G_LWHITE,	FIS_SOLID,	0,	G_WHITE,	G_LBLACK },	/* closer */
+ { WCOL_DRAW3D|WCOL_ACT3D|WCOL_DRAWBKG, MD_REPLACE,	G_LWHITE,	FIS_SOLID,	0,	G_WHITE,	G_LBLACK },	/* hider */
+ { WCOL_DRAW3D|WCOL_ACT3D|WCOL_DRAWBKG, MD_REPLACE,	G_LWHITE,	FIS_SOLID,	0,	G_WHITE,	G_LBLACK },	/* iconify */
+ { WCOL_DRAW3D|WCOL_ACT3D|WCOL_DRAWBKG, MD_REPLACE,	G_LWHITE,	FIS_SOLID,	0,	G_WHITE,	G_LBLACK },	/* fuller */
+ { WCOL_DRAW3D|WCOL_ACT3D|WCOL_DRAWBKG, MD_REPLACE,	G_LWHITE,	FIS_SOLID,	0,	G_WHITE,	G_LBLACK },	/* Sizer */
+ { WCOL_DRAW3D|WCOL_ACT3D|WCOL_DRAWBKG, MD_REPLACE,	G_LWHITE,	FIS_SOLID,	0,	G_WHITE,	G_LBLACK },	/* UParrow */
+ { WCOL_DRAW3D|WCOL_ACT3D|WCOL_DRAWBKG, MD_REPLACE,	G_LWHITE,	FIS_SOLID,	0,	G_WHITE,	G_LBLACK },	/* DNarrow */
+ { WCOL_DRAW3D|WCOL_ACT3D|WCOL_DRAWBKG, MD_REPLACE,	G_LWHITE,	FIS_SOLID,	0,	G_WHITE,	G_LBLACK },	/* LFarrow */
+ { WCOL_DRAW3D|WCOL_ACT3D|WCOL_DRAWBKG, MD_REPLACE,	G_LWHITE,	FIS_SOLID,	0,	G_WHITE,	G_LBLACK },	/* RTarrow */
+};
+
+struct xa_window_colours def_utop_wc =
+{
+ G_BLACK,	/* window frame color */
+/* flags	wrmode		color		Interior	style	tl color	br color */
+ { 0,		MD_REPLACE,	G_LWHITE,	FIS_SOLID,	0,	G_WHITE,	G_LBLACK },	/* window areas not covered by a widget/ unused widgets*/
+ { WCOL_DRAW3D|WCOL_DRAWBKG,		MD_REPLACE,	G_LBLACK,	FIS_SOLID,	0,	G_LWHITE,	G_BLACK },	/* Slider */
+ { WCOL_DRAW3D|WCOL_DRAWBKG,		MD_REPLACE,	G_LBLACK,	FIS_SOLID,	7,	G_LWHITE,	G_BLACK },	/* Slide */
+ 
+ { WCOL_DRAWBKG,		MD_REPLACE,	G_LBLACK,	FIS_SOLID,	4,	G_LWHITE,	G_BLACK },	/* Title */
+/* flags	  fontID	    size	   Effect     forground	background	*/
+/*								  col	    col		*/
+ {	0,	       1,	       10,	        0,	G_LWHITE,	G_BLACK },		/* Title text info */
+ 
+ { WCOL_DRAWBKG,		MD_REPLACE,	G_LBLACK,	FIS_SOLID,	4,	G_BLACK,	G_LWHITE },	/* Info */
+/* flags	  fontID	    size	   Effect     forground	background	*/
+/*								  col	    col		*/
+ {	0,	       1,	       9,	   ITALIC,	G_LWHITE,	G_BLACK },		/* Info text info */
+ 
+ { WCOL_DRAWBKG,		MD_REPLACE,	G_LBLACK,	FIS_SOLID,	0,	G_LWHITE,	G_BLACK },	/* closer */
+ { WCOL_DRAWBKG,		MD_REPLACE,	G_LBLACK,	FIS_SOLID,	0,	G_LWHITE,	G_BLACK },	/* hider */
+ { WCOL_DRAWBKG,		MD_REPLACE,	G_LBLACK,	FIS_SOLID,	0,	G_LWHITE,	G_BLACK },	/* iconify */
+ { WCOL_DRAWBKG,		MD_REPLACE,	G_LBLACK,	FIS_SOLID,	0,	G_LWHITE,	G_BLACK },	/* fuller */
+ { WCOL_DRAWBKG,		MD_REPLACE,	G_LBLACK,	FIS_SOLID,	0,	G_LWHITE,	G_BLACK },	/* Sizer */
+ { WCOL_DRAWBKG,		MD_REPLACE,	G_LBLACK,	FIS_SOLID,	0,	G_LWHITE,	G_BLACK },	/* UParrow */
+ { WCOL_DRAWBKG,		MD_REPLACE,	G_LBLACK,	FIS_SOLID,	0,	G_LWHITE,	G_BLACK },	/* DNarrow */
+ { WCOL_DRAWBKG,		MD_REPLACE,	G_LBLACK,	FIS_SOLID,	0,	G_LWHITE,	G_BLACK },	/* LFarrow */
+ { WCOL_DRAWBKG,		MD_REPLACE,	G_LBLACK,	FIS_SOLID,	0,	G_LWHITE,	G_BLACK },	/* RTarrow */
+};
+	
 /*
  * Create a window
  *
@@ -641,7 +704,7 @@ create_window(
 	}
 #endif
 
-	w = kmalloc(sizeof(*w));
+	w = kmalloc(sizeof(*w) + (sizeof(struct xa_window_colours) << 1));
 	if (!w)
 		/* Unable to allocate memory for window? */
 		return NULL;
@@ -692,7 +755,13 @@ create_window(
 		w->opts = opts;
 	}
 
-	w->vdi_handle = C.vh;	
+	w->vdi_handle = C.vh;
+	w->ontop_cols = (struct xa_window_colours *)((long)w + sizeof(*w));
+	w->untop_cols = (struct xa_window_colours *)((long)w->ontop_cols + sizeof(struct xa_window_colours));
+	*w->ontop_cols = def_otop_wc;
+	*w->untop_cols = def_utop_wc;
+	w->colours = w->ontop_cols;
+
 	w->wheel_mode = client->options.wheel_mode;
 	w->frame = frame;
 	w->thinwork = thinwork;
@@ -1064,7 +1133,7 @@ do_rootwind_msg(
 }
 
 void
-draw_window(enum locks lock, struct xa_window *wind)
+draw_window(enum locks lock, struct xa_window *wind, RECT *clip)
 {
 	
 	DIAG((D_wind, wind->owner, "draw_window %d for %s to %d/%d,%d/%d",
@@ -1077,7 +1146,9 @@ draw_window(enum locks lock, struct xa_window *wind)
 			wind->handle, w_owner(wind)));
 		return;
 	}
-
+	
+	set_clip(clip);
+	
 	l_color(G_BLACK);
 	hidem();
 
@@ -1090,8 +1161,8 @@ draw_window(enum locks lock, struct xa_window *wind)
 
 		/* Display the window backdrop (borders only, GEM style) */
 
-		cl.w-=SHADOW_OFFSET;
-		cl.h-=SHADOW_OFFSET;
+		cl.w -= SHADOW_OFFSET;
+		cl.h -= SHADOW_OFFSET;
 #if 0
 		if (wind->active_widgets & TOOLBAR)
 		{
@@ -1121,7 +1192,7 @@ draw_window(enum locks lock, struct xa_window *wind)
 			tcl = cl;
 			if (wind->frame > 0)
 			{
-				l_color(9);
+				l_color(wind->colours->frame_col);
 				gbox(0, &cl);
 				tcl.x++;
 				tcl.y++;
@@ -1133,17 +1204,17 @@ draw_window(enum locks lock, struct xa_window *wind)
 			pnt[1] = tcl.y;
 			pnt[2] = tcl.x + tcl.w - 1;
 			pnt[3] = wa.y - 1;
-			if_bar(pnt); 			/* top */
+		//	if_bar(pnt); 			/* top */
 			pnt[1] = wa.y + wa.h;
 			pnt[3] = tcl.y + tcl.h - 1;
-			if_bar(pnt);			/* bottom */
+		//	if_bar(pnt);			/* bottom */
 			pnt[0] = tcl.x;
 			pnt[1] = tcl.y;
 			pnt[2] = wa.x - 1;
-			if_bar(pnt);			/* left */
+		//	if_bar(pnt);			/* left */
 			pnt[0] = wa.x + wa.w;
 			pnt[2] = tcl.x + tcl.w - 1;
-			if_bar(pnt);			/* right */
+		//	if_bar(pnt);			/* right */
 
 			/* Display the work area */
 
@@ -1163,8 +1234,8 @@ draw_window(enum locks lock, struct xa_window *wind)
 				else
 				{
 					RECT nw = wa;
-					nw.w++;
-					nw.h++;
+					//nw.w++;
+					//nw.h++;
 					br_hook(2, &nw, screen.dial_colours.shadow_col);
 					tl_hook(2, &nw, screen.dial_colours.lit_col);
 					br_hook(1, &nw, screen.dial_colours.lit_col);
@@ -1210,19 +1281,31 @@ draw_window(enum locks lock, struct xa_window *wind)
 		int f;
 		short status = wind->window_status;
 		XA_WIDGET *widg;
+		RECT r;
 
 		for (f = 0; f < XA_MAX_WIDGETS; f++)
 		{
 			widg = get_widget(wind, f);
 
-			if ((widg->properties & WIDG_NOTEXT) || (f == XAW_MENU && wind == root_window))
+			if ((widg->loc.properties & WIP_NOTEXT) || (f == XAW_MENU && wind == root_window))
 				continue;
 
 			if (!(status & widg->loc.statusmask) && widg->display)
 			{
 				DIAG((D_wind, wind->owner, "draw_window %d: display widget %d (func=%lx)",
 					wind->handle, f, widg->display));
-				widg->display(lock, wind, widg);
+				
+				if (widg->loc.properties & WIP_WACLIP)
+				{
+					if (xa_rect_clip(clip, &wind->wa, &r))
+					{
+						set_clip(&r);
+						widg->display(lock, wind, widg);
+						set_clip(clip);
+					}
+				}
+				else
+					widg->display(lock, wind, widg);
 			}
 		}
 	}
@@ -1292,6 +1375,8 @@ after_top(enum locks lock, bool untop)
 	/* Refresh the previous top window as being 'non-topped' */
 	if (below && below != root_window)
 	{
+		below->colours = below->untop_cols;
+
 		send_iredraw(lock, below, 0, NULL);
 
 		if (untop)
@@ -1699,7 +1784,10 @@ delete_window1(enum locks lock, struct xa_window *wind)
 	}
 
 	clear_wind_rectlist(wind);
-
+	
+	if (wind->widg_rows)
+		kfree(wind->widg_rows);
+	
 	kfree(wind);
 }
 
@@ -1789,14 +1877,14 @@ display_window(enum locks lock, int which, struct xa_window *wind, RECT *clip)
 				{
 					if (xa_rect_clip(clip, &rl->r, &d))
 					{
-						set_clip(&d);
-						draw_window(lock, wind);
+						//set_clip(&d);
+						draw_window(lock, wind, &d);
 					}
 				}
 				else
 				{
-					set_clip(&rl->r);
-					draw_window(lock, wind);
+					//set_clip(&rl->r);
+					draw_window(lock, wind, &rl->r);
 				}
 				rl = rl->next;
 			}

xaaes/src.km/c_window.h
@@ -69,7 +69,7 @@ void	display_window(enum locks lock, int which, struct xa_window *w, RECT *clip)
 
 DoWinMesag	do_rootwind_msg;
 
-void	draw_window(enum locks lock, struct xa_window *wind);
+void	draw_window(enum locks lock, struct xa_window *wind, RECT *clip);
 void	update_all_windows(enum locks lock, struct xa_window *wl);
 void	update_windows_below(enum locks lock, const RECT *old, RECT *new, struct xa_window *wl, struct xa_window *wend);
 void	redraw_client_windows(enum locks lock, struct xa_client *client);

xaaes/src.km/draw_obj.c
@@ -311,22 +311,30 @@ void t_effect(short m)
 	}
 }
 
-void t_font(short p, short f)
+void
+t_font(short p, short f)
 {
 	static short pm = -1;
 	static short fm = -1;
 	short temp;
 
-	if (pm != p)
-	{
-		pm = p;
-		vst_point(C.vh, pm, &temp, &temp, &temp, &temp);
-	}
 
 	if (fm != f)
 	{
 		fm = f;
-		vst_font(C.vh, fm);
+		vst_font(C.vh, f);
+		f = 0;
+	}
+	else
+		f = 1;
+
+	/*
+	 * Ozk: Hmm... have to reset point size when changing fonts!!??
+	 */
+	if (!f || pm != p)
+	{
+		pm = p;
+		vst_point(C.vh, p, &temp, &temp, &temp, &temp);
 	}
 }
 
@@ -676,7 +684,7 @@ void cramped_name(const void *s, char *t, short w)
 	{
 		if (w < 12)		/* 8.3 */
 		{
-			strcpy(t, q+d); /* only the last ch's */
+			strcpy(t, q + d); /* only the last ch's */
 		}
 		else
 		{
@@ -705,6 +713,48 @@ const char *clipped_name(const void *s, char *t, short w)
 	return s;
 }
 
+const char *
+prop_clipped_name(const char *s, char *d, int w)
+{
+	int swidth = 0;
+	short cw, tmp;
+	char *dst = d;
+
+	while (*s)
+	{
+		vqt_width(C.vh, *s, &cw, &tmp, &tmp);
+		if (cw != -1)
+		{
+			swidth += cw;
+
+			if (swidth >= w)
+				break;
+			*dst++ = *s++;
+		}
+	}
+	*dst = '\0';
+	return d;	
+}
+
+const char *
+gdprop_clipped_name(const char *s, char *d, int width)
+{
+	long w = (long)width << 16;
+	long ax, ay, swidth = 0;
+	char *dst = d;
+
+	while (*s)
+	{
+		vqt_advance32(C.vh, *s, &ax, &ay);
+		swidth += ax;
+		if (swidth > w)
+			break;
+		*dst++ = *s++;
+	}
+	*dst = '\0';
+	return d;
+}
+		
 /* HR: 1 (good) set of routines for screen saving */
 inline long calc_back(const RECT *r, short planes)
 {
@@ -1130,6 +1180,7 @@ set_text(OBJECT *ob,
 	}
 	case TE_STANDARD:		/* Use the standard system font (probably 10 point) */
 	{
+		t_font(10, 1);
 		cur.w = screen.c_max_w;
 		cur.h = screen.c_max_h;
 		break;
@@ -1320,7 +1371,7 @@ set_objcursor(struct widget_tree *wt)
 	wt->e.cr.x -= wt->tree->ob_x;
 	wt->e.cr.y -= wt->tree->ob_y;
 
-	t_font(screen.standard_font_point, screen.standard_font_id);
+	//t_font(screen.standard_font_point, screen.standard_font_id);
 }
 
 #if SELECT_COLOR
@@ -1407,12 +1458,13 @@ d_g_box(enum locks lock, struct widget_tree *wt, const RECT *clip)
 		p->wt		= wt;
 		p->index	= wt->current;
 		p->r		= wt->r;
-		
+		p->clip		= *clip;
+	#if 0
 		p->clip.x	= clip->x;
 		p->clip.y	= clip->y;
 		p->clip.w	= clip->w - clip->x + 1;
 		p->clip.h	= clip->h - clip->y + 1;
-		
+	#endif
 		if (xa_rect_clip(&wt->r, &p->clip, &p->clip))
 		{
 			p->clip.w = p->clip.x + p->clip.w - 1;
@@ -1516,11 +1568,13 @@ d_g_boxchar(enum locks lock, struct widget_tree *wt, const RECT *clip)
 			gr.y += PUSH3D_DISTANCE;
 		}
 		wr_mode(colours.textmode ? MD_REPLACE : MD_TRANS);
+		t_font(10, 1);
 		ob_text(wt, &gr, &r, NULL, temp_text, 0, -1);
 	}
 	else
 	{
 		gbar(0, &r);
+		t_font(10, 1);
 		ob_text(wt, &gr, &r, &colours, temp_text, ob->ob_state, -1);
 
 		if (selected)
@@ -1565,9 +1619,11 @@ d_g_boxtext(enum locks lock, struct widget_tree *wt, const RECT *clip)
 			gr.y += PUSH3D_DISTANCE;
 		}
 		ob_text(wt, &gr, &r, &colours, temp_text, 0, -1);
-	} else
+	}
+	else
 	{
 		gbar(0, &r);
+		//t_font(10, 1);
 		ob_text(wt, &gr, &r, &colours, temp_text, ob->ob_state, -1);
 
 		if (selected)
@@ -1581,7 +1637,7 @@ d_g_boxtext(enum locks lock, struct widget_tree *wt, const RECT *clip)
 		}
 	}
 
-	t_font(screen.standard_font_point, screen.standard_font_id);
+	//t_font(screen.standard_font_point, screen.standard_font_id);
 	done(OS_SELECTED);
 }
 void
@@ -1607,12 +1663,13 @@ d_g_fboxtext(enum locks lock, struct widget_tree *wt, const RECT *clip)
 			gr.x += PUSH3D_DISTANCE;
 			gr.y += PUSH3D_DISTANCE;
 		}
-
+		//t_font(10, 1);
 		ob_text(wt, &gr, &r, &colours, temp_text, 0, -1);
 	}
 	else
 	{
-		gbar(0, &r);				
+		gbar(0, &r);
+		//t_font(10, 1);			
 		ob_text(wt, &gr, &r, &colours, temp_text, ob->ob_state, -1);
 
 		if (selected)
@@ -1640,7 +1697,7 @@ d_g_fboxtext(enum locks lock, struct widget_tree *wt, const RECT *clip)
 	}
 #endif
 
-	t_font(screen.standard_font_point, screen.standard_font_id);
+	//t_font(screen.standard_font_point, screen.standard_font_id);
 	done(OS_SELECTED);
 }
 
@@ -1680,12 +1737,14 @@ d_g_button(enum locks lock, struct widget_tree *wt, const RECT *clip)
 				chiseled_gbox(0, &rr);
 			gr.x = r.x + screen.c_max_w;
 			gr.y = r.y;
+			t_font(10, 1);
 			t_extent(text, &gr.w, &gr.h);
 			ob_text(wt, &gr, NULL, &colours, text, 0, -1);
 		}
 		else
 		{
 			XA_TREE b;
+
 			b.owner = wt->owner; //C.Aes;
 			b.tree = get_widgets();
 			display_object(	lock, &b, clip,
@@ -1696,6 +1755,7 @@ d_g_button(enum locks lock, struct widget_tree *wt, const RECT *clip)
 			gr.x += ICON_W;
 			gr.x += screen.c_max_w;
 			wr_mode(MD_TRANS);
+			t_font(10, 1);
 			ob_text(wt, &gr, &r, NULL, text, 0, und & 0x7f);
 		}
 	}
@@ -1709,7 +1769,7 @@ d_g_button(enum locks lock, struct widget_tree *wt, const RECT *clip)
 		 * real values that can be used for other stuff (like shortcut
 		 * underlines :-)
 		 */
-
+		t_font(10, 1);
 		t_extent(text, &tw, &th);
 		gr.y += (r.h - th) / 2;
 		gr.x += (r.w - tw) / 2;
@@ -1795,7 +1855,7 @@ icon_characters(ICONBLK *iconblk, short state, short obx, short oby, short icx, 
 	if (state & OS_SELECTED)
 		f_color(G_WHITE);
 
-	t_font(screen.standard_font_point, screen.standard_font_id);
+	//t_font(screen.standard_font_point, screen.standard_font_id);
 	t_effect(0);
 	wr_mode( MD_TRANS);
 }
@@ -2036,9 +2096,10 @@ d_g_text(enum locks lock, struct widget_tree *wt, const RECT *clip)
 		done(OS_SELECTED);
 	}
 
+	//t_font(10, 1);
 	ob_text(wt, &gr, &r, &colours, temp_text, ob->ob_state, -1);
 
-	t_font(screen.standard_font_point, screen.standard_font_id);
+	//t_font(screen.standard_font_point, screen.standard_font_id);
 }
 void
 d_g_ftext(enum locks lock, struct widget_tree *wt, const RECT *clip)
@@ -2074,7 +2135,7 @@ d_g_ftext(enum locks lock, struct widget_tree *wt, const RECT *clip)
 	}
 #endif
 
-	t_font(screen.standard_font_point, screen.standard_font_id);
+	//t_font(screen.standard_font_point, screen.standard_font_id);
 }
 
 #define userblk(ut) (*(USERBLK **)(ut->userblk_pp))
@@ -2087,6 +2148,7 @@ d_g_progdef(enum locks lock, struct widget_tree *wt, const RECT *clip)
 	struct xa_client *client = lookup_extension(NULL, XAAES_MAGIC);
 	OBJECT *ob = wt->tree + wt->current;
 	PARMBLK *p;
+	short r[4];
 
 #if GENERATE_DIAGS
 	struct proc *curproc = get_curproc();
@@ -2112,11 +2174,13 @@ d_g_progdef(enum locks lock, struct widget_tree *wt, const RECT *clip)
 
 	*(RECT *)&(p->pb_x) = wt->r;
 
+	*(RECT *)&(p->pb_xc) = *clip;
+#if 0
 	p->pb_xc = clip->x; //C.global_clip[0];
 	p->pb_yc = clip->y; //C.global_clip[1];
 	p->pb_wc = clip->w - clip->x + 1; //C.global_clip[2] - C.global_clip[0] + 1;
 	p->pb_hc = clip->h - clip->y + 1; //C.global_clip[3] - C.global_clip[1] + 1;
-
+#endif
 	userblk(client->ut) = object_get_spec(ob)->userblk;
 	p->pb_parm = userblk(client->ut)->ub_parm;
 
@@ -2174,7 +2238,10 @@ d_g_progdef(enum locks lock, struct widget_tree *wt, const RECT *clip)
 	 * Ozk: Since it is possible that taskswitches happens during the callout of
 	 *	progdef's, we need to restore the clip-rect used by this 'thread'
 	 */
-	vs_clip(C.vh, 1, (short *)clip);
+	*(RECT *)&r = *clip;
+	r[2] += (r[0] - 1);
+	r[3] += (r[1] - 1);
+	vs_clip(C.vh, 1, (short *)&r);
 	
 	if (*wt->state_mask & OS_DISABLED)
 	{
@@ -2207,6 +2274,8 @@ display_list_element(enum locks lock, struct xa_client *client, const RECT *clip
 	XA_TREE tr = nil_tree;
 	short xt = x + ICON_W;
 
+	t_font(10, 1);
+	
 	if (this)
 	{
 		f_color(sel ? G_BLACK : G_WHITE);
@@ -2234,8 +2303,9 @@ display_list_element(enum locks lock, struct xa_client *client, const RECT *clip
 				this->icon->ob_state &= ~OS_SELECTED;
 
 			tr.tree = this->icon;
-			tr.owner = client; //C.Aes;
+			tr.owner = client;
 			display_object(lock, &tr, clip, 0, x, y, 12);
+			t_font(10, 1);
 		}
 	}
 	else /* filler line */
@@ -2271,7 +2341,7 @@ d_g_slist(enum locks lock, struct widget_tree *wt, const RECT *clip)
 	this = list->top;
 
 	t_color(G_BLACK);
-
+	
 	if (list->state == 0)
 	{
 		get_widget(w, XAW_TITLE)->stuff = list->title;
@@ -2287,7 +2357,11 @@ d_g_slist(enum locks lock, struct widget_tree *wt, const RECT *clip)
 			tl_hook(2,&r,screen.dial_colours.lit_col);
 		}
 #endif
-		draw_window(list->lock, w);
+		draw_window(list->lock, w, clip);
+
+		wr_mode(MD_TRANS);
+		
+		t_font(10, 1);
 		for (; y <= maxy; y += screen.c_max_h)
 		{
 			/* can handle nil this */
@@ -2319,6 +2393,7 @@ d_g_slist(enum locks lock, struct widget_tree *wt, const RECT *clip)
 		}
 		/* scroll list windows are not on top, but are visible! */
 		display_vslide(list->lock, w, get_widget(w, XAW_VSLIDE));
+		wr_mode(MD_TRANS);
 		display_list_element(lock, wt->owner, clip, this, list->left, wa.x, y, wa.w, this == list->cur);
 		list->state = 0;
 	}
@@ -2369,7 +2444,10 @@ d_g_string(enum locks lock, struct widget_tree *wt, const RECT *clip)
 			done(OS_DISABLED);
 		}
 		else
+		{
+			t_font(10, 1);
 			g_text(wt, r, &wt->r, text, ob->ob_state);
+		}
 
 		if (tit)
 			line(r.x, r.y + r.h, r.x + r.w -1, r.y + r.h, G_BLACK);
@@ -2391,7 +2469,10 @@ d_g_title(enum locks lock, struct widget_tree *wt, const RECT *clip)
 
 	/* most AES's allow null string */
 	if (text)
+	{
+		t_font(10, 1);
 		g_text(wt, r, &wt->r, text, ob->ob_state);
+	}
 
 	if (ob->ob_state & OS_SELECTED && wt->menu_line)
 		/* very special!!! */
@@ -2465,9 +2546,9 @@ display_object(enum locks lock, XA_TREE *wt, const RECT *clip, short item, short
 	o.w = r.w - o.w;
 	o.h = r.h - o.h;
 	
-	if (   o.x		> clip->w //C.global_clip[2]	/* x + w */
+	if (   o.x		> (clip->x + clip->w - 1) //C.global_clip[2]	/* x + w */
 	    || o.x + o.w - 1	< clip->x //C.global_clip[0]	/* x     */
-	    || o.y		> clip->h //C.global_clip[3]	/* y + h */
+	    || o.y		> (clip->y + clip->h - 1) //C.global_clip[3]	/* y + h */
 	    || o.y + o.h - 1	< clip->y) //C.global_clip[1])	/* y     */
 		return;
 
@@ -2598,6 +2679,9 @@ draw_object_tree(enum locks lock, XA_TREE *wt, OBJECT *tree, short item, short d
 	bool curson = (wt->e.c_state & (OB_CURS_ENABLED | OB_CURS_DRAWN)) == (OB_CURS_ENABLED | OB_CURS_DRAWN) ? true : false;
 	RECT clip = *(RECT *)&C.global_clip;
 
+	clip.w -= (clip.x - 1);
+	clip.h -= (clip.y - 1);
+	
 	IFDIAG(short *cl = C.global_clip;)
 
 	if (wt == NULL)

xaaes/src.km/draw_obj.h
@@ -89,6 +89,8 @@ void form_copy(const RECT *fr, const RECT *to);
 void strip_name(char *to, const char *fro);
 void cramped_name(const void *s, char *t, short w);
 const char *clipped_name(const void *s, char *t, short w);
+const char *prop_clipped_name(const char *s, char *d, int w);
+const char *gdprop_clipped_name(const char *s, char *d, int w);
 
 //int thickness(OBJECT *ob);
 //bool is_menu(OBJECT *tree);

xaaes/src.km/form.c
@@ -161,7 +161,7 @@ Setup_form_do(struct xa_client *client,
 		DIAG((D_form, client, "Setup_form_do: wind %d for %s", client->fmd.wind->handle, client->name));
 		wind = client->fmd.wind;
 		calc_fmd_wind(client, obtree, kind, (RECT *)&client->fmd.r);
-		wt = set_toolbar_widget(lock, wind, client, obtree, edobj, WIDG_NOTEXT, NULL);
+		wt = set_toolbar_widget(lock, wind, client, obtree, edobj, WIP_NOTEXT, NULL);
 		wt->zen = false;
 		move_window(lock, wind, true, -1, client->fmd.r.x, client->fmd.r.y, client->fmd.r.w, client->fmd.r.h);
 	}
@@ -197,7 +197,7 @@ Setup_form_do(struct xa_client *client,
 		if ((wind = create_fmd_wind(lock, client, kind, client->fmd.state ? created_for_FMD_START : created_for_FORM_DO, &client->fmd.r)))
 		{
 			client->fmd.wind = wind;
-			wt = set_toolbar_widget(lock, wind, client, obtree, edobj, WIDG_NOTEXT, NULL);
+			wt = set_toolbar_widget(lock, wind, client, obtree, edobj, WIP_NOTEXT, NULL);
 			wt->zen = false;
 		}
 		else

xaaes/src.km/menuwidg.c
@@ -1864,6 +1864,7 @@ Display_menu_widg(enum locks lock, struct xa_window *wind, struct xa_widget *wid
 	{
 		obtree->ob_x = widg->ar.x; //wt->rdx;
 		obtree->ob_y = widg->ar.y; //wt->rdy;
+		obtree->ob_width = obtree[obtree[0].ob_head].ob_width = widg->ar.w;
 		draw_object_tree(0, wt, NULL, 0, MAX_DEPTH, NULL);
 		write_menu_line((RECT*)&obtree->ob_x);	/* HR: not in standard menu's object tree */
 	}
@@ -2211,7 +2212,7 @@ fix_menu(struct xa_client *client, OBJECT *root, bool do_desk)
 	int titles, menus, tbar, s_ob, t_ob;
 
 	DIAG((D_menu, NULL, "fixing menu 0x%lx", root));
-
+	
 	tbar = root[0].ob_head;
 	titles = root[tbar].ob_head;
 	menus = root[0].ob_tail;

commit 7255b267facd4036f36b2a58de8ad0e72228ec3f

Prevent XaAES from changing video modes. Contributed by Miro Kropacek.
The bug was introduced by commit 7df69a09.

xaaes/src.km/k_init.c
@@ -638,23 +638,10 @@ k_init(unsigned long vm)
 #ifndef ST_ONLY
 			else if (vdo == 0x00030000L)
 			{
-				short nvmode;
-
-				BLOG((false, "Falcon video: mode %d(%x)", cfg.videomode, cfg.videomode));
-
-				/* Ronald Andersson:
-				 * This should be the method for falcon!
-				 */
-				nvmode = vcheckmode(cfg.videomode);
-				if ((nvmode & (1 << 4)) &&	/* VGA_FALCON */
-				    (nvmode & 7) == 4)		/* is 16bit */
-				{
-					nvmode &= ~(1 << 3);		/* Set 320 pixels */
-				}
-				
-				work_out[45] = nvmode;
+				work_out[45] = vcheckmode(cfg.videomode);
 				mode = 5;
-// 				display("Falcon video: mode %x, %x", cfg.videomode, nvmode);
+				BLOG((false, "Falcon video: videomode %d(%x),mode=%d,nvmode=%x", cfg.videomode, cfg.videomode, mode, work_out[45]));
+// 				display("Falcon video: mode %x, %x", cfg.videomode, work_out[45]);
 			}
 			else
 #endif