freemint/freemint

commit 7c9ba347f2e1e9b44b5c19bee1222dffa6668093

xaaes: remove some unneeded ifdef GENERATE_DIAGS

xaaes/src.km/c_window.c
@@ -365,7 +365,7 @@ inside_minmax(RECT *r, struct xa_window *wind)
 }
 
 #if GENERATE_DIAGS
-static char *stacks[] =
+static const char *const stacks[] =
 {
 	"HUH!!",
 	"Open windows",
@@ -1237,7 +1237,6 @@ create_window(
 	r = R;
 
 
-#if GENERATE_DIAGS
 	if (max)
 	{
 		DIAG((D_wind, client, "create_window for %s: r:%d,%d/%d,%d  max:%d,%d/%d,%d",
@@ -1248,8 +1247,6 @@ create_window(
 		DIAG((D_wind, client, "create_window for %s: r:%d,%d/%d,%d  no max",
 			c_owner(client), r.x,r.y,r.w,r.h));
 	}
-
-#endif
 
 	w = kmalloc(sizeof(*w));
 	if (!w)	/* Unable to allocate memory for window? */
@@ -2380,12 +2377,10 @@ move_window(int lock, struct xa_window *wind, bool blit, WINDOW_STATUS newstate,
 					wind->ro = wind->rc;
 				}
 			}
-#if GENERATE_DIAGS
-			if ((newstate & XAWS_SHADED))
+			if (newstate & XAWS_SHADED)
 			{
 				DIAGS(("move_window: set shaded"));
 			}
-#endif
 			if (newstate == XAWS_FULLED)
 			{
 				blit = false;
@@ -2397,12 +2392,10 @@ move_window(int lock, struct xa_window *wind, bool blit, WINDOW_STATUS newstate,
 	}
 	else
 	{
-#if GENERATE_DIAGS
-		if ((wind->window_status & XAWS_SHADED))
+		if (wind->window_status & XAWS_SHADED)
 		{
 			DIAGS(("move_window: win is shaded"));
 		}
-#endif
 		wind->pr = wind->rc;
 	}
 
@@ -2715,15 +2708,14 @@ delete_window1(int lock, struct xa_window *wind)
 static void
 CE_delete_window(int lock, struct c_event *ce, short cancel)
 {
-// 	if (!strnicmp(ce->client->proc_name, "gfa_xref", 8))
-// 		display("CE_del_wind %lx", ce->ptr1);
-
 	/*
 	 * Since the window in this event already have been removed from
 	 * all stacks, we perform  this deletion even when cancel flag is set
 	 */
-	//if (!cancel)
+#if 0
+	if (!cancel)
 		delete_window1(lock, ce->ptr1);
+#endif
 }
 
 void _cdecl

xaaes/src.km/app_man.c
@@ -132,12 +132,10 @@ reset_focus(struct xa_window **new_focus, short flags)
 					if (get_desktop()->owner != topcl) {
 						DIAGA(("reset_focus: Only root_wind present, app ontop have no wind- no focused"));
 						fw = NULL;
-					}
-#if GENERATE_DIAGS
-					else {
+					} else
+					{
 						DIAGA(("reset_focus: Suggest root_window to get focus"));
 					}
-#endif
 					break;
 				} else if (fw->owner == topcl && !(fw->window_status & XAWS_NOFOCUS)) {
 					DIAGA(("reset_focus: suggest %d of %s as new focus", fw->handle, fw->owner->name));
@@ -212,14 +210,12 @@ setnew_focus(struct xa_window *wind, struct xa_window *unfocus, bool topowner, b
 					if (S.focus == wind) {
 						DIAGA(("setnew_focus: taking NOFOCUS window out of focus!"));
 						S.focus = NULL;
-					}
-#if GENERATE_DIAGS
-					else if (S.focus) {
+					} else if (S.focus)
+					{
 						DIAGA(("setnew_focus: Keep focus on %d of %s", S.focus->handle, S.focus->owner->name));
 					} else {
 						DIAGA(("setnew_focus: Keeping no focused window state"));
 					}
-#endif
 				}
 				return;
 			}
@@ -385,14 +381,12 @@ find_focus(bool withlocks, bool *waiting, struct xa_client **locked_client, stru
 						*keywind = top;
 					else if (S.focus && S.focus->owner == client)
 						*keywind = S.focus;
-#if GENERATE_DIAGS
 					if (*keywind) {
 						DIAGA(("find_focus: keywind handle %d, type %s",
 							(*keywind)->handle, (*keywind)->nolist ? "nolist":"normal"));
 					} else {
 						DIAGA(("find_focus: no window with focus"));
 					}
-#endif
 				}
 				DIAGA(("-= 3 =-"));
 				return client;
@@ -1063,13 +1057,14 @@ next_wind(int lock)
 		wind = wind->prev;
 	}
 
-#if GENERATE_DIAGS
 	if (wind)
+	{
 		DIAG((D_appl, NULL, "  --  return %d, owner %s",
 			wind->handle, wind->owner->name));
-	else
+	} else
+	{
 		DIAG((D_appl, NULL, " -- no next window"));
-#endif
+	}
 	return wind;
 }
 #endif

xaaes/src.km/c_mouse.c
@@ -279,11 +279,10 @@ cXA_open_menu(int lock, struct c_event *ce, short cancel)
 				}
 				widg->m.click(lock, root_window, widg, &ce->md);
 			}
-		}
-#if GENERATE_DIAGS
-		else
+		} else
+		{
 			DIAG((D_mouse, ce->client, "cXA_open_menu skipped for %s - menu changed before cevent.", ce->client->name));
-#endif
+		}
 	}
 	C.ce_open_menu = NULL;
 }

xaaes/src.km/cnf_xaaes.c
@@ -685,12 +685,10 @@ pCB_app_options(char *line)
 			{
 				get_argument(s + 19, &a);
 				opts->standard_font_point = a;
-			}
-
-#if GENERATE_DIAGS
-			else
+			} else
+			{
 				DIAGS(("pCB_app_options: unknown keyword %s", s));
-#endif
+			}
 		}
 	}
 }
@@ -757,14 +755,13 @@ isolate_strarg(char **str)
 			}
 			e++;
 		}
-#if GENERATE_DIAGS
 		if (ret)
 		{
 			DIAGS((" -- got quoted string = '%s', start of next = %lx", ret, (unsigned long)e));
-		}
-		else
+		} else
+		{
 			DIAGS((" -- some error"));
-#endif
+		}
 	}
 	else
 	{
@@ -782,14 +779,13 @@ isolate_strarg(char **str)
 				*e++ = 0;
 		}
 
-#if GENERATE_DIAGS
 		if (ret)
 		{
 			DIAGS((" -- got unquoted string = '%s', start of next = %lx", ret, (unsigned long)e));
-		}
-		else
+		} else
+		{
 			DIAGS((" -- some unquote error"));
-#endif
+		}
 	}
 
 	if (ret)
@@ -1000,8 +996,7 @@ pCB_run(const char *path, const char *line, struct parsinf *inf)
 #endif
 
 #if GENERATE_DIAGS
-static void
-diags_opts(struct options *o)
+static void diags_opts(struct options *o)
 {
 	DIAGS(("        windowner  = %s", o->windowner ? "true" : "false"));
 	DIAGS(("        nohide     = %s", o->nohide    ? "true" : "false"));
@@ -1015,8 +1010,10 @@ diags_opts(struct options *o)
 	DIAGS(("        nolive     = %s", o->nolive    ? "true" : "false"));
 	DIAGS(("        winframe   = %d", o->thinframe));
 }
-
+#else
+#define diags_opts(o)
 #endif
+
 void
 load_config(void *path )
 {

xaaes/src.km/debug.c
@@ -390,7 +390,7 @@ const char *D_fl = "%s,%d: ";
 const char *D_flu = "%ld,%d - %s,%d: ";
 
 void
-show_bits(unsigned short data, char *prf, char *t[], char *x)
+show_bits(unsigned short data, const char *prf, const char *const t[], char *x)
 {
 	int i = 0;
 	*x = '\0';
@@ -457,12 +457,6 @@ diags(const char *fmt, ...)
 	char buf[512];
 	va_list args;
 	long l;
-#if 1
-	struct proc *p = get_curproc();
-
-	if ((strnicmp(p->name, "thing", 5)))
-		return;
-#endif
 
 	va_start(args, fmt);
 	l = vsprintf(buf, sizeof(buf), fmt, args);
@@ -505,12 +499,7 @@ diag(enum debug_item item, struct xa_client *client, const char *t, ...)
 {
 	enum debug_item *point = client ? client->options.point : D.point;
 	short b, x, y;
-#if 1
-	struct proc *p = get_curproc();
 
-	if ((strnicmp(p->name, "thing", 5)))
-		return;
-#endif
 	if (D.debug_level == 4
 	    || (    (D.debug_level >= 2 && point[item])
 	        && !(D.debug_level == 3 && client == NULL)))

xaaes/src.km/debug.h
@@ -161,7 +161,7 @@ struct xa_client;
 struct xa_window;
 struct widget_tree;
 
-void show_bits(unsigned short, char *prf, char *t[], char *x);
+void show_bits(unsigned short, const char *prf, const char *const t[], char *x);
 const char *w_owner(struct xa_window *w);
 const char *c_owner(struct xa_client *c);
 const char *t_owner(struct widget_tree *t);
@@ -188,7 +188,6 @@ void diag(enum debug_item item, struct xa_client *client, const char *t, ...) __
 #endif
 
 #else /* GENERATE_DIAGS */
-// void show_bits(unsigned short, char *prf, char *t[], char *x);
 
 #define MAX_NAMED_DIAG 0
 #define DIAGS(x)

xaaes/src.km/draw_obj.c
@@ -57,7 +57,7 @@ static struct widget_tree nil_wt;
 
 
 #if GENERATE_DIAGS
-static char *pstates[] =
+static const char *const pstates[] =
 {
 	"SEL",
 	"CROSS",
@@ -77,7 +77,7 @@ static char *pstates[] =
 	"15"
 };
 #if 0
-static char *pflags[] =
+static const char *const pflags[] =
 {
 	"S",
 	"DEF",
@@ -98,7 +98,7 @@ static char *pflags[] =
 };
 #endif
 #if 0
-static char *ob_types[] =
+static const char *const ob_types[] =
 {
 	"box",
 	"text",
@@ -123,6 +123,7 @@ static char *ob_types[] =
 	"40"
 };
 #endif
+
 #if 0
 static char *
 object_txt(OBJECT *tree, short t)			/* HR: I want to know the culprit in a glance */
@@ -153,7 +154,7 @@ object_txt(OBJECT *tree, short t)			/* HR: I want to know the culprit in a glanc
 	return nother;
 }
 
-static char *
+static const char *
 object_type(OBJECT *tree, short t)
 {
 	static char other[80];
@@ -175,7 +176,7 @@ object_type(OBJECT *tree, short t)
 #endif
 #else
 #if 0
-static char *ob_types[] =
+static const char *const ob_types[] =
 {
 	"box",
 	"text",
@@ -734,9 +735,6 @@ display_object(int lock, XA_TREE *wt, struct xa_vdi_settings *v, struct xa_aes_o
 	/* Get display routine for this type of object from jump table */
 	if (t <= G_UNKNOWN)
 		drawer = wt->objcr_api->drawers[t];
-
-//	if (t >= G_SWBUTTON && t <= G_SHORTCUT)
-//		display("display %s(%d) for %s", ob_types[t], t, wt->owner->name);
 
 	if (!drawer)
 	{

xaaes/src.km/form.c
@@ -1245,17 +1245,12 @@ Click_form_do(int lock,
 				(*wt->exit_form)(client, wind, wt, &fr);
 			}
 		}
-	}
-#if GENERATE_DIAGS
-	else
+	} else
 	{
 		DIAGS(("Click_form_do: NO OBTREE!!"));
-// 		display("Click_form_do: NO OBTREE!!");
 	}
-#endif
 
 	DIAGS(("Click_form_do: return"));
-// 	display("Click_form_do: return");
 	return false;
 }
 

commit 53f15d54a330f2c1acdaa94b167319b2598b7931

Fix a typo which prevents releasing window handles
Introduced by 7c9ba347.

Fixes #239.

xaaes/src.km/c_window.c
@@ -2713,8 +2713,8 @@ CE_delete_window(int lock, struct c_event *ce, short cancel)
 	 */
 #if 0
 	if (!cancel)
-		delete_window1(lock, ce->ptr1);
 #endif
+		delete_window1(lock, ce->ptr1);
 }
 
 void _cdecl