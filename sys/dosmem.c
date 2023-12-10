freemint/freemint

commit 29bbebc7a64c682006f281fa52973e809f3892e4

Use in_vdi
This fixes #22.

sys/dosmem.c
@@ -24,9 +24,11 @@
 # include "arch/mprot.h"
 # include "arch/detect.h"
 
+# include "cookie.h"	/* get_cookie */
 # include "k_prot.h"
+# include "keyboard.h"	/* emutos */
 # include "memory.h"
-# include "util.h" /* pid2proc */
+# include "util.h"		/* pid2proc */
 
 # include "proc.h"
 
@@ -175,18 +177,22 @@ sys_m_xalloc (long size, int mode)
 
 	TRACE(("Mxalloc(%ld,%x)",size,mode));
 
-	/* AKP: Hack here: if the calling process' PC is in ROM, then this
-	 * is a Malloc call made by VDI's v_opnvwk routine.  So we change
-	 * mode to include "super accessible."  This is temporary, until
-	 * VDI catches up with multitasking TOS.
+#ifndef M68000
+	/* Hack here: if this is a Malloc call made by TOS 3/4 VDI's v_opnvwk routine,
+	 * we change mode to include "super accessible."
 	 */
-	if (((mode & F_PROTMODE) == 0)
-		&& (get_curproc()->ctxt[SYSCALL].pc > 0x00e00000L)
-		&& (get_curproc()->ctxt[SYSCALL].pc < 0x00efffffL))
+	if (((mode & F_PROTMODE) == 0) && get_curproc()->in_vdi)
 	{
-		mode |= (F_PROT_PR) | F_KEEP;
-		TRACE(("m_xalloc: VDI special (call from ROM)"));
+		if (!emutos
+			&& get_cookie(NULL, COOKIE_NVDI, NULL) == EERROR
+			&& get_cookie(NULL, COOKIE_fVDI, NULL) == EERROR
+			&& get_cookie(NULL, COOKIE_NOVA, NULL) == EERROR)
+		{
+			mode |= (F_PROT_PR) | F_KEEP;
+			DEBUG(("m_xalloc: VDI special (call from TOS 3/4)"));
+		}
 	}
+#endif
 
 	/* If the mode argument comes in a zero, then set it to the default
 	 * value from prgflags.  Otherwise subtract one from it to bring it

xaaes/src.km/k_init.c
@@ -432,6 +432,19 @@ setup_xa_module_api(void)
 }
 
 /*
+ * Workaround for buggy TOS3/4 VDI. We have to set the flag explicitly
+ * because we already are in kernel so enter_vdi() wouldn't be called.
+ * See sys_m_xalloc() in dosmem.c for further explanation.
+ */
+static void v_opnvwk_patched(short work_in[], VdiHdl *handle, short work_out[])
+{
+	struct proc *p = get_curproc();
+	p->in_vdi = 1;
+	v_opnvwk(work_in, handle, work_out);
+	p->in_vdi = 0;
+}
+
+/*
  * check if file exist in Aes_home_path
  */
 static char * _cdecl
@@ -524,6 +537,7 @@ calc_average_fontsize(struct xa_vdi_settings *v, short *maxw, short *maxh, short
 
 	vqt_fontinfo(v->handle, &i, &i, dist, &i, temp);
 }
+
 int
 k_init(unsigned short dev, unsigned short mc)
 {
@@ -577,7 +591,7 @@ k_init(unsigned short dev, unsigned short mc)
 		memset( work_out, 0, sizeof(work_out) );
 		set_wrkin(work_in, 1);
 		BLOG((0,"1st v_opnvwk (P_handle=%d)", C.P_handle ));
-		v_opnvwk(work_in, &v->handle, work_out);
+		v_opnvwk_patched(work_in, &v->handle, work_out);
 		BLOG((0,"->%d, wh=%d/%d %d colors", v->handle, work_out[0], work_out[1], work_out[13]));
 		if( !(work_out[0] && work_out[1] && work_out[13]) )
 		{
@@ -723,7 +737,7 @@ k_init(unsigned short dev, unsigned short mc)
 		v->handle = C.P_handle;
 
 		set_wrkin(work_in, 1);
-		v_opnvwk(work_in, &v->handle, work_out);
+		v_opnvwk_patched(work_in, &v->handle, work_out);
 	}	/* /if ( v->handle <= 0 ) */
 
 	if (v->handle == 0)

commit 5ddf2bc3579b3d3f0627f58b6fbc696853bbf071

Bring back the check for PC in TOS ROM
Commit 29bbebc7 mistakenly assumed that any Mxalloc() call coming from VDI would be solely in the context of the caller. However that is not true, at least Milan and CTPCI VDI allocate private memory from the TOS area and need the original workaround to work correctly.

Hopefully fixes issue #22 for good.

sys/dosmem.c
@@ -178,19 +178,27 @@ sys_m_xalloc (long size, int mode)
 	TRACE(("Mxalloc(%ld,%x)",size,mode));
 
 #ifndef M68000
-	/* Hack here: if this is a Malloc call made by TOS 3/4 VDI's v_opnvwk routine,
-	 * we change mode to include "super accessible."
+	/* AKP: Hack here: if the calling process' PC is in ROM, then this
+	 * is a Malloc call made by VDI's v_opnvwk routine.  So we change
+	 * mode to include "super accessible."  This is temporary, until
+	 * VDI catches up with multitasking TOS.
+	 *
+	 * Unfortunately, the check for PC doesn't work in all cases anymore; it has
+	 * an effect on Milan/CTPCI VDI which are still bugged but if v_opnvwk() is
+	 * called from an application, ctxt contains its PC, not ROM's so further
+	 * checking is necessary.
 	 */
-	if (((mode & F_PROTMODE) == 0) && get_curproc()->in_vdi)
+	if ((mode & F_PROTMODE) == 0
+		&& !emutos
+		&& ((get_curproc()->ctxt[SYSCALL].pc > 0x00e00000L
+			 && get_curproc()->ctxt[SYSCALL].pc < 0x00efffffL)
+			|| (get_curproc()->in_vdi
+				&& get_cookie(NULL, COOKIE_NVDI, NULL) == EERROR
+				&& get_cookie(NULL, COOKIE_fVDI, NULL) == EERROR
+				&& get_cookie(NULL, COOKIE_NOVA, NULL) == EERROR)))
 	{
-		if (!emutos
-			&& get_cookie(NULL, COOKIE_NVDI, NULL) == EERROR
-			&& get_cookie(NULL, COOKIE_fVDI, NULL) == EERROR
-			&& get_cookie(NULL, COOKIE_NOVA, NULL) == EERROR)
-		{
-			mode |= (F_PROT_PR) | F_KEEP;
-			DEBUG(("m_xalloc: VDI special (call from TOS 3/4)"));
-		}
+		mode |= (F_PROT_PR) | F_KEEP;
+		DEBUG(("m_xalloc: VDI special (call from TOS 3/4)"));
 	}
 #endif
 