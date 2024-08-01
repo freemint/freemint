64cf0c6aa32e21b3fbc3af48fe80856b783ab334
Thorsten Otto <admin@tho-otto.de>
2024-08-01 14:14:40 +0200

    Simplify the override of cpu targets in util.sh
    This should be more robust in case the source file is changed

--- .scripts/before_script.sh
+++ .scripts/before_script.sh
@@ -25,27 +25,27 @@ then
 fi
 
 # limit building to selected CPU targets
-sed -i -e "s/xaloadtargets = 000 02060 030 040 060 col/xaloadtargets = ${TEMP_CPU_TARGET}/;" ./xaaes/src.km/xaloader/XALOADDEFS
-sed -i -e "s/xaaestargets = deb 000 sto 030 040 060 col 02060/xaaestargets = ${XAAES_TARGETS}/;" ./xaaes/src.km/XAAESDEFS
-sed -i -e "s/moosetargets = 02060 030 040 060 deb 000 col/moosetargets = ${TEMP_CPU_TARGET}/;" ./xaaes/src.km/adi/whlmoose/MOOSEDEFS
-sed -i -e "s/kerneltargets = 000 020 030 040 060 deb mil ara hat hat030 col/kerneltargets = ${KERNEL_TARGETS}/;" ./sys/KERNELDEFS
-sed -i -e "s/ext2targets = 02060 030 040 060 deb 000 col/ext2targets = ${TEMP_CPU_TARGET}/;" ./sys/xfs/ext2fs/EXT2DEFS
-sed -i -e "s/nfstargets = 02060 030 040 060 deb 000 col/nfstargets = ${TEMP_CPU_TARGET}/;" ./sys/xfs/nfs/NFSDEFS
-sed -i -e "s/minixtargets = 02060 030 040 060 deb 000 col/minixtargets = ${TEMP_CPU_TARGET}/;" ./sys/xfs/minixfs/MINIXDEFS
-sed -i -e "s/xconout2targets = 02060 030 040 060 deb 000 col/xconout2targets = ${TEMP_CPU_TARGET}/;" ./sys/xdd/xconout2/XCONOUT2DEFS
-sed -i -e "s/lptargets = 02060 030 040 060 deb 000 col/lptargets = ${TEMP_CPU_TARGET}/;" ./sys/xdd/lp/LPDEFS
-sed -i -e "s/usbloadtargets = 000 02060 030 040 060 col/usbloadtargets = ${TEMP_CPU_TARGET}/;" ./sys/usb/src.km/loader/USBLOADDEFS
-sed -i -e "s/ethtargets = 02060 030 040 060 deb 000 col prg/ethtargets = ${TEMP_CPU_TARGET}${PRG_TARGET}/;" ./sys/usb/src.km/udd/eth/ETHDEFS
-sed -i -e "s/keyboardtargets = 02060 030 040 060 deb 000 col prg/keyboardtargets = ${TEMP_CPU_TARGET}${PRG_TARGET}/;" ./sys/usb/src.km/udd/hid/keyboard/KEYBOARDDEFS
-sed -i -e "s/mousetargets = 02060 030 040 060 deb 000 col prg/mousetargets = ${TEMP_CPU_TARGET}${PRG_TARGET}/;" ./sys/usb/src.km/udd/hid/mouse/MOUSEDEFS
-sed -i -e "s/printertargets = 02060 030 040 060 deb 000 col prg/printertargets = ${TEMP_CPU_TARGET}${PRG_TARGET}/;" ./sys/usb/src.km/udd/printer/PRINTERDEFS
-sed -i -e "s/storagetargets = 02060 030 040 060 deb 000 col prg/storagetargets = ${TEMP_CPU_TARGET}${PRG_TARGET}/;" ./sys/usb/src.km/udd/storage/STORAGEDEFS
-sed -i -e "s/usbtargets = 02060 030 040 060 deb 000 col prg plm/usbtargets = ${USB_TARGETS}/;" ./sys/usb/src.km/USBDEFS
-sed -i -e "s/ehcitargets = 02060 030 040 060 deb 000 col #prg/ehcitargets = ${TEMP_CPU_TARGET}/;" ./sys/usb/src.km/ucd/ehci/EHCIDEFS
-sed -i -e "s/netusbeetargets = 02060 030 040 060 deb 000 prg prg_000 #col/netusbeetargets = ${NETUSBEE_TARGETS}/;" ./sys/usb/src.km/ucd/netusbee/NETUSBEEDEFS
-sed -i -e "s/unicorntargets = 02060 030 040 060 deb 000 col prg/unicorntargets = ${TEMP_CPU_TARGET}${PRG_TARGET}/;" ./sys/usb/src.km/ucd/unicorn/UNICORNDEFS
-sed -i -e "s/vttusbtargets = 030 deb 000 prg p30 pst mst/vttusbtargets = ${VTTUSB_TARGETS}/;" ./sys/usb/src.km/ucd/vttusb/VTTUSBDEFS
-sed -i -e "s/inet4targets = 02060 030 040 060 deb 000 col/inet4targets = ${TEMP_CPU_TARGET}/;" ./sys/sockets/INET4DEFS
+echo "xaloadtargets = ${TEMP_CPU_TARGET}" >> ./xaaes/src.km/xaloader/XALOADDEFS
+echo "xaaestargets = ${XAAES_TARGETS}" >> ./xaaes/src.km/XAAESDEFS
+echo "moosetargets = ${TEMP_CPU_TARGET}" >> ./xaaes/src.km/adi/whlmoose/MOOSEDEFS
+echo "kerneltargets = ${KERNEL_TARGETS}" >> ./sys/KERNELDEFS
+echo "ext2targets = ${TEMP_CPU_TARGET}" >> ./sys/xfs/ext2fs/EXT2DEFS
+echo "nfstargets = ${TEMP_CPU_TARGET}" >> ./sys/xfs/nfs/NFSDEFS
+echo "minixtargets = ${TEMP_CPU_TARGET}" >> ./sys/xfs/minixfs/MINIXDEFS
+echo "xconout2targets = ${TEMP_CPU_TARGET}" >> ./sys/xdd/xconout2/XCONOUT2DEFS
+echo "lptargets = ${TEMP_CPU_TARGET}" >> ./sys/xdd/lp/LPDEFS
+echo "usbloadtargets = ${TEMP_CPU_TARGET}" >> ./sys/usb/src.km/loader/USBLOADDEFS
+echo "ethtargets = ${TEMP_CPU_TARGET}${PRG_TARGET}" >> ./sys/usb/src.km/udd/eth/ETHDEFS
+echo "keyboardtargets = ${TEMP_CPU_TARGET}${PRG_TARGET}" >> ./sys/usb/src.km/udd/hid/keyboard/KEYBOARDDEFS
+echo "mousetargets = ${TEMP_CPU_TARGET}${PRG_TARGET}" >> ./sys/usb/src.km/udd/hid/mouse/MOUSEDEFS
+echo "printertargets = ${TEMP_CPU_TARGET}${PRG_TARGET}" >> ./sys/usb/src.km/udd/printer/PRINTERDEFS
+echo "storagetargets = ${TEMP_CPU_TARGET}${PRG_TARGET}" >> ./sys/usb/src.km/udd/storage/STORAGEDEFS
+echo "usbtargets = ${USB_TARGETS}" >> ./sys/usb/src.km/USBDEFS
+echo "ehcitargets = ${TEMP_CPU_TARGET}" >> ./sys/usb/src.km/ucd/ehci/EHCIDEFS
+echo "netusbeetargets = ${NETUSBEE_TARGETS}" >> ./sys/usb/src.km/ucd/netusbee/NETUSBEEDEFS
+echo "unicorntargets = ${TEMP_CPU_TARGET}${PRG_TARGET}" >> ./sys/usb/src.km/ucd/unicorn/UNICORNDEFS
+echo "vttusbtargets = ${VTTUSB_TARGETS}" >> ./sys/usb/src.km/ucd/vttusb/VTTUSBDEFS
+echo "inet4targets = ${TEMP_CPU_TARGET}" >> ./sys/sockets/INET4DEFS
 
 for f in tools/IO/IODEFS \
 	tools/crypto/CRYPTODEFS \
