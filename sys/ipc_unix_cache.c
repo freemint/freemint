f08402b3828a3a0bbe33381f88402e7342b9e170
David Gálvez <dgalvez75@gmail.com>
2024-08-22 16:35:00 +0200

    Remove unused code in ipc_unix_cache.c
    Code for checksum calculation was added in commit
    c675c4ce84865fdd0c74f84e9098b1e4042b5b28 but it broke
    AF_UNIX sockets domain.
    
    Commit c4eb6beb8d8bfae5e8be3b422b77a2d03aca07f8 revert it
    but it left the ckecksum calculation code that it's not
    used for anything anymore.
    
    See GitHub's issue #330.

--- sys/ipc_unix_cache.c
+++ sys/ipc_unix_cache.c
@@ -29,86 +29,10 @@ struct lookup_cache
 	long	inode;
 	long	stamp;
 # define MK_STAMP(time, date)	((((long)(date)) << 16) | (time))
-	long	cksum;
 	long	un_index;
 };
 
 static struct lookup_cache f_cache[CACHE_ENTRIES];
-
-/*
- * Generic checksum computation routine.
- * Returns the one's complement of the 16 bit one's complement sum over
- * the `nwords' words starting at `buf'.
- */
-static long
-chksum (void *buf, short nwords)
-{
-	long sum = 0;
-	
-	__asm__
-	(
-		"clrl	%%d0		\n\t"
-#ifdef __mcoldfire__
-		"mvzw	%2, %%d1		\n\t"
-		"lsrl	#1, %%d1		\n\t"	/* # of longs in buf */
-#else
-		"movew	%2, %%d1		\n\t"
-		"lsrw	#1, %%d1		\n\t"	/* # of longs in buf */
-#endif
-		"bcc	1f		\n\t"	/* multiple of 4 ? */
-#ifdef __mcoldfire__
-		"mvz.w	%1@+, %%d2	\n\t"
-		"addl	%%d2, %0		\n\t"	/* no, add in extra word */
-		"addxl	%%d0, %0		\n"
-#else
-		"addw	%1@+, %0	\n\t"	/* no, add in extra word */
-		"addxw	%%d0, %0		\n"
-#endif
-		"1:			\n\t"
-#ifdef __mcoldfire__
-		"subql	#1, %%d1		\n\t"	/* decrement for dbeq */
-#else
-		"subqw	#1, %%d1		\n\t"	/* decrement for dbeq */
-#endif
-		"bmi	3f		\n"
-		"2:			\n\t"
-		"addl	%1@+, %0	\n\t"
-		"addxl	%%d0, %0		\n\t"
-#ifdef __mcoldfire__
-		"subql	#1, %%d1		\n\t"
-		"bpls	2b		\n"	/* loop over all longs */
-#else
-		"dbra	%%d1, 2b		\n"	/* loop over all longs */
-#endif
-		"3:			\n\t"
-#ifdef __mcoldfire__
-		"swap	%0		\n\t"	/* convert to short */
-		"mvzw	%0, %%d1		\n\t"
-		"clr.w	%0		\n\t"
-		"swap	%0		\n\t"
-		"addl	%%d1, %0		\n\t"
-		"swap	%0		\n\t"
-		"mvzw	%0, %%d1		\n\t"
-		"clr.w	%0		\n\t"
-		"swap	%0		\n\t"
-		"addl	%%d1, %0		\n\t"
-#else
-		"movel	%0, %%d1		\n\t"	/* convert to short */
-		"swap	%%d1		\n\t"
-		"addw	%%d1, %0		\n\t"
-		"addxw	%%d0, %0		\n\t"
-#endif
-		: "=d"(sum), "=a"(buf)
-		: "g"(nwords), "1"(buf), "0"(sum)
-#ifdef __mcoldfire__
-		: "d0", "d1", "d2"
-#else
-		: "d0", "d1"
-#endif
-	);
-	
-	return ~sum;
-}
 
 long
 un_cache_lookup (char *name, long *index)
@@ -116,7 +40,7 @@ un_cache_lookup (char *name, long *index)
 	XATTR attr;
 	short dirty_idx, i;
 	long r, fd, stamp;
-	long cksum;
+
 	static short last_deleted = 0;
 
 	r = sys_f_xattr (1, name, &attr);
@@ -157,8 +81,6 @@ un_cache_lookup (char *name, long *index)
 		}
 	}
 
-	cksum = chksum(name,
-		MIN((strlen(name) + 1), sizeof(struct sockaddr_un) - UN_PATH_OFFSET) >> 1);
 	if (dirty_idx < 0)
 	{
 		dirty_idx = last_deleted++;
@@ -188,7 +110,6 @@ un_cache_lookup (char *name, long *index)
 	f_cache[dirty_idx].dev = attr.dev;
 	f_cache[dirty_idx].inode = attr.index;
 	f_cache[dirty_idx].stamp = stamp;
-	f_cache[dirty_idx].cksum = cksum;
 	f_cache[dirty_idx].un_index = *index;
 	f_cache[dirty_idx].valid = CACHE_VALID;
 
