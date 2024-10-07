freemint/freemint

commit db7646cf80f408ddbd4fd77d19ebf1e818b0e07f

Prefer machine dir over sysdir when searching for mint.cnf

sys/cnf_mint.c
@@ -271,12 +271,10 @@ load_config(void)
 	char cnf_path[128];
 	struct cnfdata mydata;
 
-	strcpy(cnf_path, sysdir);
-	strcat(cnf_path, "mint.cnf");
-	if (parse_cnf(cnf_path, parser_tab, &mydata, 0) != 0 && strlen(mchdir) > 0)
+	if (*mchdir == '\0' || parse_cnf(strcat(strcpy(cnf_path, mchdir), "mint.cnf"), parser_tab, &mydata, 0) != 0)
 	{
-		/* sysdir/mint.cnf not found, try mchdir/mint.cnf */
-		strcpy(cnf_path, mchdir);
+		/* mchdir/mint.cnf not found, try sysdir/mint.cnf */
+		strcpy(cnf_path, sysdir);
 		strcat(cnf_path, "mint.cnf");
 
 		parse_cnf(cnf_path, parser_tab, &mydata, 0);

commit 87a27304924b2bd5fa84da64334199d7160d6de1

Use separate option to cnf_parse to suppress error messages about file being not present.
Fixes spurious error message if mint.cnf is not found in $MCHDIR,
introduced by db7646cf80f408ddbd4fd77d19ebf1e818b0e07f

sys/cnf.c
@@ -840,7 +840,7 @@ long parse_cnf(const char *path, struct parser_item *parser_tab, void *data, uns
 		fp->links = 0;
 		FP_FREE(fp);
 
-		if (!(options & INF_QUIET))
+		if (!(options & INF_OPTIONAL))
 			ALERT(MSG_cnf_cant_open, path);
 	}
 	return ret;

sys/cnf.h
@@ -36,6 +36,7 @@ struct parsinf
 #define INF_VERBOSE   (1ul << ('V' - 'A'))
 #define INF_CONTROL   (1ul << ('C' - 'A'))
 #define INF_UNSET     (1ul << ('U' - 'A'))
+#define INF_OPTIONAL  (1ul << ('O' - 'A'))
 
 /*============================================================================*/
 /* The parser uses callbacks to start actions. Using the same interfaces, both

sys/cnf_mint.c
@@ -713,7 +713,7 @@ void load_config(void)
 	char cnf_path[128];
 	struct cnfdata mydata;
 
-	if (*mchdir == '\0' || parse_cnf(strcat(strcpy(cnf_path, mchdir), "mint.cnf"), parser_tab, &mydata, INF_UNSET) != 0)
+	if (*mchdir == '\0' || parse_cnf(strcat(strcpy(cnf_path, mchdir), "mint.cnf"), parser_tab, &mydata, INF_UNSET | INF_OPTIONAL) != 0)
 	{
 		/* mchdir/mint.cnf not found, try sysdir/mint.cnf */
 		strcpy(cnf_path, sysdir);

xaaes/src.km/cnf_xaaes.c
@@ -1011,6 +1011,7 @@ void
 load_config(void *path )
 {
 	char cpath[FILENAME_MAX];
+	long ret;
 
 	struct cnfdata mydata;
 	if( !path || !*(char*)path )
@@ -1023,8 +1024,26 @@ load_config(void *path )
 
 	DIAGS(("Loading config %s", cpath));
 	BLOG((0,"Loading config %s", cpath));
-	parse_cnf(cpath, parser_tab, &mydata, INF_QUIET);
+	ret = parse_cnf(cpath, parser_tab, &mydata, INF_OPTIONAL);
 
+	if (ret != 0)
+	{
+#if GENERATE_DIAGS
+#undef CNF_NAME
+#define CNF_NAME "xaaes.cnf"
+		if( !path || !*(char*)path )
+		{
+			strncpy(cpath, C.Aes->home_path, sizeof(cpath)-sizeof(CNF_NAME)-1);
+			strcat(cpath, CNF_NAME);
+		}
+		else
+			strncpy(cpath, path, sizeof(cpath) );
+
+		DIAGS(("Loading config %s", cpath));
+		BLOG((0,"Loading config %s", cpath));
+		parse_cnf(cpath, parser_tab, &mydata, INF_OPTIONAL);
+#endif
+	}
 
 #if GENERATE_DIAGS
 	{
@@ -1227,5 +1246,5 @@ void read_inf(void)
 	char buf[256];
 	sprintf( buf, sizeof(buf), "%s%s", C.start_path, inf_fname );
 	BLOG((0,"%s:read_inf:%s", get_curproc()->name, buf));
-	parse_cnf(buf, inf_tab, &mydata, INF_QUIET);
+	parse_cnf(buf, inf_tab, &mydata, INF_OPTIONAL);
 }