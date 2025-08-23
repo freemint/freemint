0924a139b27da04bf0a16d7802419576955e612b
Miro Kropacek <miro.kropacek@gmail.com>
2025-08-23 23:28:03 +0200

    mintloader supports Hatari builds
    - mint000h.prg and mint030h.prg are now recognized if natfeats is
      enabled
    
    - change the order of search: first look for cpu-based names, then fall
      back into DEFAULT which is mint.prg (similar as xaloader and ubsloader)
    
    - whitespace cleanup

--- tools/mintload/mintloader.c
+++ tools/mintload/mintloader.c
@@ -1,9 +1,10 @@
-#include <mint/cookie.h>
+#include <mint/arch/nf_ops.h>
 #include <mint/basepage.h>
+#include <mint/cookie.h>
 #include <mint/mintbind.h>
 #include <mint/ssystem.h>
-#include <sys/fcntl.h>
 #include <string.h>
+#include <sys/fcntl.h>
 
 #include "../../sys/buildinfo/version.h"
 #ifndef str
@@ -21,49 +22,87 @@ typedef struct
 
 #define CJAR    (*(struct cookie *)(0x5A0))
 
-#define MINTDIR        "\\mint\\"MINT_VERS_PATH_STRING
-#define DEFAULT        "mint000.prg"
-#define DEFAULT_68000  "mint000.prg"
-#define DEFAULT_68020  "mint020.prg"
-#define DEFAULT_68030  "mint030.prg"
-#define DEFAULT_68040  "mint040.prg"
-#define DEFAULT_68060  "mint060.prg"
-#define DEFAULT_MILAN  "mintmil.prg"
-#define DEFAULT_ARANYM "mintara.prg"
+#define MINTDIR         "\\mint\\"MINT_VERS_PATH_STRING
+#define DEFAULT         "mint.prg"
+#define DEFAULT_68000   "mint000.prg"
+#define DEFAULT_68000_HATARI "mint000h.prg"
+#define DEFAULT_68020   "mint020.prg"
+#define DEFAULT_68030   "mint030.prg"
+#define DEFAULT_68030_HATARI "mint030h.prg"
+#define DEFAULT_68040   "mint040.prg"
+#define DEFAULT_68060   "mint060.prg"
+#define DEFAULT_MILAN   "mintmil.prg"
+#define DEFAULT_ARANYM  "mintara.prg"
+#define DEFAULT_FIREBEE "mintv4e.prg"
 
 #ifndef __mcoldfire__
 static long
 get_cookie (long id, unsigned long *ret)
 {
-		void *oldssp;
-		COOKIE *cjar;
+	void *oldssp;
+	COOKIE *cjar;
 
-		oldssp = (void*)(Super(1L) ? NULL : (void*)Super(0L));
+	oldssp = (void*)(Super(1L) ? NULL : (void*)Super(0L));
 
-		cjar = *(COOKIE**)(0x5A0);
+	cjar = *(COOKIE**)(0x5A0);
 
-		if(oldssp)
-				Super( oldssp );
+	if (oldssp)
+		Super (oldssp);
 
-		while (cjar->id)
+	while (cjar->id)
+	{
+		if (cjar->id == id)
 		{
-				if (cjar->id == id)
-				{
-						if (ret)
-						{
-								*ret = cjar->value;
-								return 1;
-						}
-						else
-						{
-								return cjar->value;
-						}
-				}
-
-				cjar++;
+			if (ret)
+			{
+				*ret = cjar->value;
+				return 1;
+			}
+			else
+			{
+				return cjar->value;
+			}
 		}
 
-		return 0;
+		cjar++;
+	}
+
+	return 0;
+}
+
+/*static*/ const char *
+nf_get_name(char *buf, size_t bufsize)
+{
+	const char *ret = NULL;
+	struct nf_ops *nf_ops;
+
+	if ((nf_ops = nf_init()) != NULL)
+	{
+		long nfid_name = NF_GET_ID(nf_ops, NF_ID_NAME);
+
+		if (nfid_name)
+		{
+			if (nf_ops->call(nfid_name | 0, (unsigned long)buf, (unsigned long)bufsize) != 0)
+			{
+				ret = buf;
+			}
+		}
+	}
+
+	return ret;
+}
+
+static int
+is_hatari(void)
+{
+	char name[6+1];
+	return nf_get_name(name, sizeof(name)) != NULL
+		&& name[0] == 'H'
+		&& name[1] == 'a'
+		&& name[2] == 't'
+		&& name[3] == 'a'
+		&& name[4] == 'r'
+		&& name[5] == 'i';
 }
 #endif
 
@@ -71,46 +110,44 @@ get_cookie (long id, unsigned long *ret)
 static void
 my_strlcpy(char *dst, const char *src, size_t n)
 {
-		size_t org_n = n;
+	size_t org_n = n;
 
-		if (n && --n)
-		{
-				do {
-						if (!(*dst++ = *src++))
-								break;
-				}
-				while (--n);
-
+	if (n && --n)
+	{
+		do {
+			if (!(*dst++ = *src++))
+				break;
 		}
+		while (--n);
+	}
 
-		if (org_n && n == 0)
-				*dst = '\0';
+	if (org_n && n == 0)
+		*dst = '\0';
 }
 
 static void
 my_strlcat(char *dst, const char *src, size_t n)
 {
-		if (!n)
-				return;
+	if (!n)
+		return;
 
-		/* find end */
-		while (n-- && *dst)
-				dst++;
+	/* find end */
+	while (n-- && *dst)
+		dst++;
 
-		/* copy over */
-		if (n && --n)
-		{
-				do {
-						if (!(*dst++ = *src++))
-								break;
-				}
-				while (--n);
-
+	/* copy over */
+	if (n && --n)
+	{
+		do {
+			if (!(*dst++ = *src++))
+				break;
 		}
+		while (--n);
+	}
 
-		/* terminate */
-		if (n == 0)
-				*dst = '\0';
+	/* terminate */
+	if (n == 0)
+		*dst = '\0';
 }
 
 int loader_init(int argc, char **argv, char **env);
@@ -118,105 +155,120 @@ int loader_init(int argc, char **argv, char **env);
 int
 loader_init(int argc, char **argv, char **env)
 {
-		char path[384];
-		char temp[384];
-		char *name;
+	char path[384];
+	char temp[384];
+	char *name;
 #ifndef __mcoldfire__
-		long r;
-		unsigned long cpu;
-		unsigned long mch;
+	unsigned long cpu;
+	unsigned long mch;
+	int hatari = is_hatari();
 #endif
-		long fh;
+	long fh;
 
-		(void)Cconws("\033vFreeMiNT loader starting...\r\n");
+	(void)Cconws("\033vFreeMiNT loader starting...\r\n");
 
-		my_strlcpy(path, MINTDIR, sizeof(path));
+	my_strlcpy(path, MINTDIR, sizeof(path));
+	my_strlcat(path, "\\", sizeof(path));
 
+	my_strlcpy(temp, path, sizeof(path));
 
-		/* if the system have a 68000 CPU we use the 68000 compiled
-		* kernel by default
-		*/
 #ifdef __mcoldfire__
-		name = "mintv4e.prg";
+	name = DEFAULT_FIREBEE;
 #else
-		r = get_cookie(C__CPU, &cpu);
-		if (r && cpu < 20)
-				name = DEFAULT_68000;
-		else
-				name = DEFAULT;
-
-		my_strlcpy(temp, path, sizeof(path));
-		my_strlcat(temp, "\\", sizeof(temp));
-		my_strlcat(temp, name, sizeof(temp));
-
-		fh = Fopen(temp, O_RDONLY);
-		if (fh < 0)
+	if (get_cookie(C__CPU, &cpu))
+	{
+		switch(cpu)
 		{
-				/* default kernel not available, select kernel based on CPU type */
-				if(get_cookie(C__CPU, &cpu))
-				{
-						switch(cpu)
-						{
-								case 20:        name = DEFAULT_68020;   break;
-								case 30:        name = DEFAULT_68030;   break;
-								case 40:        name = DEFAULT_68040;   break;
-								case 60:        name = DEFAULT_68060;   break;
+			case 20:
+				name = DEFAULT_68020;
+				break;
+			case 30:
+				name = hatari ? DEFAULT_68030_HATARI : DEFAULT_68030;
+				break;
+			case 40:
+				name = DEFAULT_68040;
+				break;
+			case 60:
+				name = DEFAULT_68060;
+				break;
 
-								default:        break;
-						}
-				}
-
-				/* special treatment for MILAN and ARANYM */
-				if(get_cookie(C__MCH, &mch))
-				{
-						switch(mch>>16)
-						{
-								case 4: name = DEFAULT_MILAN;   break;
-								case 5: name = DEFAULT_ARANYM;  break;
-						}
-				}
+			default:
+				name = hatari ? DEFAULT_68000_HATARI : DEFAULT_68000;
+				break;
 		}
+	}
+	else
+	{
+		name = hatari ? DEFAULT_68000_HATARI : DEFAULT_68000;
+	}
 
-		Fclose((int)fh);
+	/* special treatment for MILAN and ARANYM */
+	if (get_cookie(C__MCH, &mch))
+	{
+		switch(mch>>16)
+		{
+			case 4:
+				name = DEFAULT_MILAN;
+				break;
+			case 5:
+				name = DEFAULT_ARANYM;
+				break;
+		}
+	}
 #endif
-		/* append kernel name */
-		my_strlcat(path, "\\", sizeof(path));
-		my_strlcat(path, name, sizeof(path));
 
-		/* check if file exist */
-		fh = Fopen(path, O_RDONLY);
-		if (fh < 0)
-		{
-				(void)Cconws("FreeMiNT loader: No such file: \"");
-				(void)Cconws(path);
-				(void)Cconws("\"\r\n");
-				goto error;
-		}
+	my_strlcat(temp, name, sizeof(temp));
+
+	fh = Fopen(temp, O_RDONLY);
+	if (fh < 0)
+	{
+		/* kernel based on CPU type not available, select default kernel */
+		name = DEFAULT;
+	}
+	else
+	{
 		Fclose((int)fh);
+	}
 
-		(void)Cconws("Load kernel: ");
+	/* append kernel name */
+	my_strlcat(path, name, sizeof(path));
+
+	/* check if file exist */
+	fh = Fopen(path, O_RDONLY);
+	if (fh < 0)
+	{
+		(void)Cconws("FreeMiNT loader: No such file: \"");
 		(void)Cconws(path);
-		(void)Cconws("\r\n");
+		(void)Cconws("\"\r\n");
+		goto error;
+	}
+	Fclose((int)fh);
 
-		/*
-		* Let our DTA point to our parent DTA; the kernel use
-		* information from the DTA when processing the auto folder
-		*/
+	(void)Cconws("Load kernel: ");
+	(void)Cconws(path);
+	(void)Cconws("\r\n");
 
-		if(_base->p_parent)
-				Fsetdta((_DTA*) _base->p_parent->p_dta);
+	/*
+	* Let our DTA point to our parent DTA; the kernel use
+	* information from the DTA when processing the auto folder
+	*/
 
-		temp[0] = 0;
-		temp[1] = '\0';
-		if(Pexec(PE_LOADGO, path, temp, NULL))
-		{
-				(void)Cconws("FreeMiNT loader: Pexec() failed!\r\n");
-				goto error;
-		}
-		return 0;
+	if (_base->p_parent)
+		Fsetdta((_DTA*) _base->p_parent->p_dta);
+
+	temp[0] = 0;
+	temp[1] = '\0';
+	if (Pexec(PE_LOADGO, path, temp, NULL))
+	{
+		(void)Cconws("FreeMiNT loader: Pexec() failed!\r\n");
+		goto error;
+	}
+
+	return 0;
 
 error:
-		(void)Cconws("press any key to continue ...\r\n");
-		Cconin();
-		return 1;
+	(void)Cconws("press any key to continue ...\r\n");
+	Cconin();
+
+	return 1;
 }
