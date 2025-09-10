51c5182e6a5288c5d6fc699a248f793e726fb664
Vincent B. <vincent.barrilliot@laposte.net>
2025-09-10 23:06:32 +0200

    Rename restr_intr to restore_TOS_vectors because it's really what it does

--- sys/arch/halt.c
+++ sys/arch/halt.c
@@ -159,7 +159,7 @@ hw_halt(void)
 	debug_ws(MSG_system_halted);
 	
 	sysq[READY_Q].head = sysq[READY_Q].tail = NULL;	/* prevent conext switches */
-	restr_intr();		/* restore interrupts to normal */
+	restore_TOS_vectors();		/* restore interrupts to normal */
 	
 	for (;;)
 	{
@@ -212,7 +212,7 @@ HALT (void)
 	debug_ws(MSG_fatal_reboot);
 	
 	sysq[READY_Q].head = sysq[READY_Q].tail = NULL; /* prevent context switches */
-	restr_intr ();		/* restore interrupts to normal */
+	restore_TOS_vectors ();		/* restore interrupts to normal */
 	
 	for (;;)
 	{
